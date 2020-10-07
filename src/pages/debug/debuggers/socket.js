1ec9c2c822a22af06f5423afe9d035b5f8ea8596
srgooglo <traveld8@gmail.com>
2020-10-07 18:19:26 +0200

    added events handlers to basics events (" disconnect, connect... etc ")

--- src/pages/debug/debuggers/socket.js
+++ src/pages/debug/debuggers/socket.js
@@ -7,13 +7,12 @@ import * as antd from 'antd'
 import { objectToArray } from 'core'
 import settings from 'core/libs/settings'
 
-const defaultSocketAddress = "85.251.59.39"
+const defaultSocketAddress = "localhost:7000"
 
 @connect(({ app }) => ({ app }))
-export default class SocketDebug extends React.Component{
+export default class SocketDebug extends React.PureComponent{
     state = {
         resolvers: objectToArray(this.props.app.resolvers),
-        connNode: null,
         InputRaw: defaultSocketAddress
     }
 
@@ -26,7 +25,16 @@ export default class SocketDebug extends React.Component{
         })
     }
 
+    handleDisconnectSocket(){
+        const socket = this.props.app.socket_conn
+        if (socket) {
+            console.log("closing")
+            socket.conn.close()
+        }
+    }
+
     render(){
+        const { socket_opt } = this.props.app
         return(
             <div>
                 <antd.Card>
@@ -42,12 +50,28 @@ export default class SocketDebug extends React.Component{
                     />
                 </antd.Card>
 
-                connectedNode: {this.state.connNode}
+                {socket_opt?  
+                     <antd.List 
+                         grid={{
+                           gutter: 5,
+                           xxl: 3,
+                         }}
+                         dataSource={objectToArray(socket_opt)}
+                         renderItem={(e) => {
+                             return(
+                                 <div style={{ border: "0.1px rgb(217, 217, 217) solid" ,backgroundColor: "rgb(250, 250, 250)", borderRadius: "4px", width: "fit-content", paddingRight: "12px" }}>
+                                     <antd.Tag>{e.key}</antd.Tag>{JSON.stringify(e.value)}
+                                 </div>
+                             )
+                         }}
+                     />
+                : null}
                 
                 <antd.Card>
-                    <antd.Button onClick={() => this.setState({ InputRaw: defaultSocketAddress })} > Set default </antd.Button>
-                    <antd.Button onClick={() => this.dispatchSocket(this.state.InputRaw)} > connect </antd.Button>
+                    <antd.Button onClick={() => this.dispatchSocket(this.state.InputRaw)} > Connect </antd.Button>
+                    <antd.Button onClick={() => this.handleDisconnectSocket()} > Disconnect </antd.Button>
                     <antd.Input onChange={(e) => this.setState({ InputRaw: e.target.value })} value={this.state.InputRaw} placeholder="ws:// http:// some.bruh:9090" />
+                    <antd.Button onClick={() => this.setState({ InputRaw: defaultSocketAddress })} > Set default </antd.Button>
                 </antd.Card>
                         
             </div>
