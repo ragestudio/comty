9f7a0f5ec891472f2236d292540831223aa76390
srgooglo <sirgoogla@gmail.com>
2020-10-14 22:32:08 +0200

    updated get_user_data endpoints

--- config/endpoints.js
+++ config/endpoints.js
@@ -5,7 +5,7 @@ export default {
   posts: 'POST /posts',
   post_actions: 'POST /post-actions',
   
-  get_data: 'POST /get-user-data',
+  get_data: 'POST /get_user_data',
   basicData: 'POST /early_user',
   profileData: 'POST /get-user-data-username'
 };
