2ee52fc904a76f14bcc4026fd538adba10acf0e5
srgooglo <sirgoogla@gmail.com>
2020-10-19 16:46:57 +0200

    changed method of import for verbosity

--- src/core/libs/index.js
+++ src/core/libs/index.js
@@ -1,8 +1,8 @@
+import verbosity from './verbosity'
 import * as v3_model from './v3_model'
 import * as appInterface from './appInterface'
 export * from './settings'
 export * from './router'
-export * from './verbosity'
 export * from './dynamicsdcp'
 
-export { v3_model, appInterface }
+export { v3_model, appInterface, verbosity }
