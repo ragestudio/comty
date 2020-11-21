8aa8fd405e459ef28e2b63545ba20c860dbd5909
srgooglo <chippotato39@gmail.com>
2020-11-21 23:33:17 +0100

    changed overlaySwapper method

---
+++ src/core/models/helpers/index.js
@@ -0,0 +1,9 @@
+export function goLive(payload) {
+    window.overlaySwap.open({
+        id: 'search',
+        mode: 'full',
+        element: <div>
+            SomeSomeSomeSomeSomeSomeSomeSomeSomeSome
+        </div>
+    })
+}
