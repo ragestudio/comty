3c10943ba61b86cd49e18da2788da8d2568938e8
srgooglo <chippotato39@gmail.com>
2020-12-14 19:48:49 +0100

    new scaffolding

---
+++ packages/comty/src/.umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from 'F:/Development/comty/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','dva','getInitialState','request',],
+});
+
+export { plugin };
