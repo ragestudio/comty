2c82e6bddb0ea3098b25320c40e31593aedc8a74
srgooglo <chippotato39@gmail.com>
2021-01-05 08:07:16 +0100

    updated packages

--- packages/comty/src/.umi/plugin-helmet/exports.ts
+++ packages/comty/src/.umi/plugin-helmet/exports.ts
@@ -1,3 +1,3 @@
 // @ts-nocheck
 // @ts-ignore
-export { Helmet } from '/Users/srgooglo/repos/comty/packages/comty/node_modules/@umijs/plugin-helmet/node_modules/react-helmet';
+export { Helmet } from '/Users/srgooglo/repos/comty/packages/comty/node_modules/react-helmet';
