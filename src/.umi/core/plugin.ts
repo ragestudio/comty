75570d565f8d52727f8b3064ab89db716ecf0e5e
srgooglo <chippotato39@gmail.com>
2021-01-05 08:11:21 +0100

    clean

--- src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/srgooglo/repos/comty/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','dva','getInitialState','request',],
-});
-
-export { plugin };
