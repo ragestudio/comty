1f323813a2c91749e9c3618aa1669561a0b0b264
srgooglo <chippotato39@gmail.com>
2021-01-05 08:11:12 +0100

    clean

--- packages/comty/src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/srgooglo/repos/comty/packages/comty/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','dva','getInitialState','initialStateConfig','request',],
-});
-
-export { plugin };
--- packages/comty/src/.umi/plugin-dva/exports.ts
+++
@@ -1,4 +0,0 @@
-// @ts-nocheck
-
-export { connect, useDispatch, useStore, useSelector } from 'dva';
-export { getApp as getDvaApp } from './dva';
