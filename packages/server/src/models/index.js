80a762ebf081f0f00490db984a6c68bb1cf950b9
srgooglo <srgooglo@ragestudio.net>
2021-11-17 18:36:56 +0100

    clean

--- packages/server/src/models/index.js
+++ packages/server/src/models/index.js
@@ -14,15 +14,6 @@ function getSchemas() {
 
 const schemas = getSchemas()
 
-export const FabricObject = mongoose.model('FabricObject', schemas.FabricObject, "fabricObjects")
-export const Workload = mongoose.model('Workload', schemas.Workload, "workload")
-export const Workshifts = mongoose.model('Workshifts', schemas.Workshift, "workshifts")
 export const Role = mongoose.model('Role', schemas.Role, 'roles')
-export const Vault = mongoose.model('Vault', schemas.VaultItem, "vault")
-export const GeoRegion = mongoose.model('GeoRegion', schemas.Region, "regions")
-
-export const Contract = mongoose.model('Contract', schemas.Contract, "contracts")
 export const User = mongoose.model('User', schemas.User, "accounts")
 export const Session = mongoose.model('Session', schemas.Session, "sessions")
-export const Workshift = mongoose.model("Workshift", schemas.Workshift, "workshifts")
-export const Report = mongoose.model("Report", schemas.Report, "reports")
