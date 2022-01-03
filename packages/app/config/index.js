1fa3e720e78c0936a42f321cf35b4045bc08c33c
srgooglo <srgooglo@ragestudio.net>
2022-01-03 18:35:35 +0100

    update config

--- packages/app/config/index.js
+++ packages/app/config/index.js
@@ -1,37 +1,43 @@
+import packagejson from "../package.json"
 import defaultTheme from "../constants/defaultTheme.json"
+import defaultSoundPack from "../constants/defaultSoundPack.json"
 
 export default {
-  defaultTheme: defaultTheme,
-
-  logo: {
-    alt: "https://dl.ragestudio.net/branding/comty/alt/SVG/t3t3.svg"
-  },
-  api: {
-    address: process.env.NODE_ENV !== 'production' ? `http://${window.location.hostname}:3000` : "https://api.amimet.es",
-  },
-
-  app: {
-    siteName: 'Comty™',
-    copyright: 'RageStudio©',
-    mainPath: '/main',
-
-    appTheme_desiredContrast: 7,
-    // Contrast level AA = 4.5, Level AAA = 7
-    // Reference: https://www.w3.org/WAI/WCAG21/quickref/?versions=2.0&showtechniques=143#qr-visual-audio-contrast-contrast
-  },
-  i18n: {
-    languages: [
-      {
-        key: 'en',
-        title: 'English',
-      },
-    ],
-    defaultLanguage: 'en',
-  },
-  stricts: {
-    post_maxlenght: '512',
-    // In KB
-    api_maxpayload: '101376',
-    api_maxovertick: 10,
-  }
+    package: packagejson,
+    defaultTheme: defaultTheme,
+    defaultSoundPack: defaultSoundPack,
+    logo: {
+        alt: "/logo_alt.svg"
+    },
+    api: {
+        address: process.env.NODE_ENV !== 'production' ? `http://${window.location.hostname}:3000` : "https://api.amimet.es",
+    },
+    ws: {
+        address: process.env.NODE_ENV !== 'production' ? `ws://${window.location.hostname}:3001` : "https://ws.amimet.es",
+    },
+    app: {
+        title: packagejson.name,
+        siteName: "Comty",
+        mainPath: '/main',
+        storage: {
+            basics: "user",
+            token: "token",
+            session_frame: "session",
+            signkey: "certified",
+            settings: "app_settings"
+        },
+    },
+    i18n: {
+        languages: [
+            {
+                key: 'en',
+                title: 'English',
+            },
+            {
+                key: 'es',
+                title: 'Español',
+            }
+        ],
+        defaultLanguage: 'en',
+    }
 }
