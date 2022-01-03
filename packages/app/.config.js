50f1df1d7e744d0db2b852fcde5763bb83176b95
srgooglo <srgooglo@ragestudio.net>
2022-01-03 18:36:20 +0100

    added vite support

--- packages/app/.config.js
+++ packages/app/.config.js
@@ -1,33 +1,31 @@
 const path = require('path')
 
 const aliases = {
-    "@antd": path.resolve(__dirname, "../../node_modules/antd"),
-    "@": path.resolve(__dirname, 'src'),
+    '~/': `${path.resolve(__dirname, 'src')}/`,
+    "__": __dirname,
+    "@src": path.resolve(__dirname, 'src'),
     schemas: path.resolve(__dirname, 'constants'),
-    controllers: path.resolve(__dirname, 'src/controllers'),
+    config: path.join(__dirname, 'config'),
     extensions: path.resolve(__dirname, 'src/extensions'),
+    pages: path.join(__dirname, 'src/pages'),
     theme: path.join(__dirname, 'src/theme'),
-    locales: path.join(__dirname, 'src/locales'),
-    core: path.join(__dirname, 'src/core'),
-    "@pages": path.join(__dirname, 'src/pages'),
     components: path.join(__dirname, 'src/components'),
     models: path.join(__dirname, 'src/models'),
 }
 
-module.exports = (config) => {
-    if (typeof config.windowContext.process === 'undefined') {
-        config.windowContext.process = Object()
+module.exports = (config = {}) => {
+    if (!config.resolve) {
+        config.resolve = {}
+    }
+    if (!config.server) {
+        config.server = {}
     }
 
-    config.windowContext.process = config.windowContext.__evite
-    config.windowContext.process["versions"] = process.versions
-    config.resolve.alias = {
-        ...config.resolve.alias,
-        ...aliases,
-    }
-    config.aliases = {
-        ...config.resolve.alias,
-        ...aliases,
+    config.resolve.alias = aliases
+    config.server.port = 8000
+    config.server.host = "0.0.0.0"
+    config.server.fs = {
+        allow: [".."]
     }
 
     config.css = {
