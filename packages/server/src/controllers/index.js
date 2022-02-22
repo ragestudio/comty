6e614f77a1bd777d059d10346dacdc1d8eb8d7d4
srgooglo <srgooglo@ragestudio.net>
2022-02-22 20:22:25 +0100

    implement `PostController`

--- packages/server/src/controllers/index.js
+++ packages/server/src/controllers/index.js
@@ -4,8 +4,10 @@ import { default as SessionController } from "./SessionController"
 import { default as UserController } from "./UserController"
 import { default as FilesController } from "./FilesController"
 import { default as PublicController } from "./PublicController"
+import { default as PostsController } from "./PostsController"
 
 export default [
+    PostsController,
     ConfigController,
     PublicController,
     RolesController,
