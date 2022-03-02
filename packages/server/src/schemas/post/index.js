cf16e3343ef71b9900952989e546b2f9544de2f2
srgooglo <srgooglo@ragestudio.net>
2022-03-02 16:08:46 +0100

    update `post` schema

--- packages/server/src/schemas/post/index.js
+++ packages/server/src/schemas/post/index.js
@@ -1,4 +1,7 @@
 export default {
-    user_id: String,
-    message: String,
+    user_id: { type: String, required: true },
+    created_at: { type: Number, required: true },
+    message: { type: String, required: true },
+    likes: { type: Array, default: [] },
+    comments: { type: Array, default: [] },
 }
