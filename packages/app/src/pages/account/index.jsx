a4698663de6c63c7d37fb434b9f6342d14da841e
srgooglo <srgooglo@ragestudio.net>
2022-03-11 22:52:44 +0100

    use `PostsFeed` component

--- packages/app/src/pages/account/index.jsx
+++ packages/app/src/pages/account/index.jsx
@@ -3,67 +3,15 @@ import * as antd from "antd"
 import { Translation } from "react-i18next"
 
 import { Icons } from "components/Icons"
-import { Skeleton, ActionsBar, AdminTools } from "components"
+import { Skeleton, PostsFeed } from "components"
 import { Session, User } from "models"
 
-import { SessionsView, StatisticsView } from "./components"
-
 import "./index.less"
-
-const SelfViewComponents = {
-	sessionsView: SessionsView,
-	statisticsView: StatisticsView,
-}
-
-const SelfViewTabDecorators = {
-	sessionsView: (
-		<div>
-			<Icons.Key /> Sessions
-		</div>
-	),
-	statisticsView: (
-		<div>
-			<Icons.PieChart /> Statistics
-		</div>
-	),
-}
-
-class SelfView extends React.Component {
-	renderComponents = () => {
-		const renderTagDecorator = (key) => {
-			if (typeof this.props.decorators[key] !== "undefined") {
-				return this.props.decorators[key]
-			}
-			return key
-		}
-
-		return Object.keys(this.props.components).map((key, index) => {
-			const Component = this.props.components[key]
-
-			return (
-				<antd.Tabs.TabPane tab={renderTagDecorator(key)} key={index}>
-					<div key={key}>
-						<Component {...this.props.componentProps} />
-					</div>
-				</antd.Tabs.TabPane>
-			)
-		})
-	}
-
-	render() {
-		return (
-			<antd.Tabs defaultActiveKey="0" centered>
-				{this.renderComponents()}
-			</antd.Tabs>
-		)
-	}
-}
 
 export default class Account extends React.Component {
 	static bindApp = ["userController", "sessionController"]
 
 	state = {
-		hasManager: false,
 		isSelf: false,
 		user: null,
 		sessions: null
@@ -88,26 +36,9 @@ export default class Account extends React.Component {
 			state.user = await this.props.contexts.app.userController.getData({ username: requestedUser })
 		}
 
-		state.hasManager = await User.hasRole("manager")
 		state.hasAdmin = await User.hasRole("admin")
 
 		this.setState(state)
-	}
-
-	handleSignOutAll = () => {
-		return this.props.contexts.app.sessionController.destroyAllSessions()
-	}
-
-	openUserEdit = async () => {
-		const result = await AdminTools.open.dataManager(this.state.user)
-
-		if (result) {
-			this.setState({ user: result })
-		}
-	}
-
-	openRolesManager = async () => {
-		await AdminTools.open.rolesManager(this.state.user._id)
 	}
 
 	render() {
@@ -136,47 +67,19 @@ export default class Account extends React.Component {
 						</div>
 					</div>
 					<div className="extension">
-						<div className="rolesList">
-							{user.roles.map((role, index) => {
+						<div className="badgesList">
+							{user.badges.map((role, index) => {
 								return <antd.Tag>{role}</antd.Tag>
 							})}
 						</div>
 					</div>
 				</div>
 
-
-				{(this.state.isSelf || this.state.hasManager) && <ActionsBar spaced padding="8px">
-					<antd.Button
-						icon={<Icons.Edit />}
-						shape="round"
-						onClick={this.openUserEdit}
-					>
-						<Translation>
-							{(t) => <>{t("Edit")}</>}
-						</Translation>
-					</antd.Button>
-					{this.state.hasAdmin && <antd.Button
-						icon={<Icons.Link />}
-						shape="round"
-						onClick={this.openRolesManager}
-					>
-						<Translation>
-							{(t) => <>{t("Manage roles")}</>}
-						</Translation>
-					</antd.Button>}
-				</ActionsBar>}
-
-				{this.state.isSelf && (
-					<SelfView
-						components={SelfViewComponents}
-						decorators={SelfViewTabDecorators}
-						componentProps={{
-							sessions: this.state.sessions,
-							user: this.state.user,
-							handleSignOutAll: this.handleSignOutAll,
-						}}
-					/>
-				)}
+				<div className="posts">
+				  <PostsFeed 
+				  	fromUserId={user._id}
+				  />
+				</div>
 			</div>
 		)
 	}
