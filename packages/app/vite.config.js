56fe4cc9f8730154b9ea5146141d6ea720877698
srgooglo <srgooglo@ragestudio.net>
2022-03-11 01:07:25 +0100

    set `routeStyle` to `next`

--- packages/app/vite.config.js
+++ packages/app/vite.config.js
@@ -1,14 +1,16 @@
 import getConfig from "./.config.js"
 
 import { defineConfig } from "vite"
-import Pages from "vite-plugin-pages"
 import reactRefresh from "@vitejs/plugin-react-refresh"
+
+import Pages from "vite-plugin-pages"
 
 export default defineConfig({
     plugins: [
         reactRefresh(),
         Pages({
             react: true,
+            routeStyle: "next",
             extensions: ["jsx", "tsx"],
         }),
     ],
