098542f270758a7a8252159e96718b5511505de8
srgooglo <srgooglo@ragestudio.net>
2022-03-14 18:45:23 +0100

    added server setup scripts

---
+++ packages/server/src/setup/dbAdmin/index.js
@@ -0,0 +1,24 @@
+import { User } from "../../models"
+import { createUser } from "../../lib"
+
+export default async () => {
+    // check if any user with includes admin role exists
+    const adminUser = await User.find({
+        $or: [
+            { roles: { $in: ['admin'] } },
+        ],
+    })
+
+    // if no user with admin role exists, create one
+    if (adminUser.length === 0) {
+        console.log('Creating admin user...')
+
+        await createUser({
+            username: 'admin',
+            password: 'admin',
+            roles: ['admin'],
+        })
+
+        console.log('Admin user created!')
+    }
+}
