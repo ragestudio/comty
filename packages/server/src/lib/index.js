e956ddb7ec4f36e4dd4f938f8ff45a2ed6f5e427
srgooglo <srgooglo@ragestudio.net>
2022-03-14 18:44:26 +0100

    added `createUser` to lib

--- packages/server/src/lib/index.js
+++ packages/server/src/lib/index.js
@@ -1,3 +1,5 @@
-export * as Token from './token'
-export { default as Schematized } from './schematized'
-export { default as additionsHandler } from './additionsHandler'
+export { default as Schematized } from "./schematized"
+export { default as additionsHandler } from "./additionsHandler"
+export { default as createUser } from "./createUser"
+
+export * as Token from "./token"
