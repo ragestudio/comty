7e057b6df661e60d91e7c6d741c99ab6ce0bcf91
srgooglo <srgooglo@ragestudio.net>
2022-03-15 01:04:20 +0100

    added `Badge` schema

---
+++ packages/server/src/schemas/badge/index.js
@@ -0,0 +1,7 @@
+export default {
+    name: { type: String, required: true },
+    color: { type: String },
+    description: { type: String },
+    label: { type: String },
+    icon: { type: String },
+}
