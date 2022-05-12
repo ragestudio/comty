9980847fe9c1f47751fd0b6f1ffa8ade0086dce2
srgooglo <srgooglo@ragestudio.net>
2022-05-12 15:00:52 +0200

    fetch list of streams

--- packages/app/src/pages/streams/index.jsx
+++ packages/app/src/pages/streams/index.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import axios from "axios"
+import React from "react"
 import * as antd from "antd"
 import { SelectableList, ActionsBar } from "components"
+
+import "./index.less"
 
 export default class Streams extends React.Component {
     state = {
@@ -15,38 +16,69 @@ export default class Streams extends React.Component {
     }
 
     updateStreamsList = async () => {
-        const streams = await this.api.get.streams().catch(error => {
+        let streams = await this.api.get.streams().catch(error => {
             console.error(error)
             antd.message.error(error)
-            
+
             return false
         })
 
+        if (streams && Array.isArray(streams)) {
+            // resolve user_id with user basic data
+            streams = streams.map(async (stream) => {
+                const userData = await this.api.get.user(undefined, { user_id: stream.user_id }).catch((error) => {
+                    console.error(error)
+                    antd.message.error(error)
+
+                    return false
+                })
+
+                if (userData) {
+                    stream.userData = userData
+                }
+
+                return stream
+            })
+
+            streams = await Promise.all(streams)
+        }
+
         this.setState({ list: streams })
     }
-
 
     onClickItem = (item) => {
         window.app.setLocation(`/streams/viewer?key=${item}`)
     }
 
     renderListItem = (stream) => {
-        stream.StreamPath = stream.StreamPath.replace(/^\/live\//, "")
-        
-        return <div key={stream.id} onClick={() => this.onClickItem(stream.StreamPath)}>
-            <h1>@{stream.StreamPath} #{stream.id}</h1>
+        return <div
+            key={stream.id}
+            onClick={() => this.onClickItem(stream.username)}
+            className="streaming-item"
+        >
+            <div className="thumbnail">
+                <img src={stream.userData.avatar} alt={stream.userData.username} />
+            </div>
+            <div className="details">
+                <div className="title">
+                    <h1>@{stream.userData.username}</h1>
+                    <span>
+                        #{stream.id}
+                    </span>
+                </div>
+            </div>
         </div>
     }
 
     render() {
-        return <div>
+        return <div className="streams">
             <ActionsBar mode="float">
                 <div>
                     <antd.Button onClick={this.updateStreamsList}>Refresh</antd.Button>
                 </div>
             </ActionsBar>
             <div>
-                <SelectableList 
+                <SelectableList
                     selectionEnabled={false}
                     renderItem={this.renderListItem}
                     items={this.state.list}
