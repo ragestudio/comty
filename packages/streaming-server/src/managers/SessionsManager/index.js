ff34d2900b4e8a59e7f80cd784161b55d4810a0c
srgooglo <srgooglo@ragestudio.net>
2022-05-12 10:53:30 +0200

    added `publishStream` & `unpublishStream` methods

--- packages/streaming-server/src/managers/SessionsManager/index.js
+++ packages/streaming-server/src/managers/SessionsManager/index.js
@@ -1,6 +1,7 @@
 export default class SessionsManager {
     constructor() {
         this.sessions = {}
+        this.publicStreams = []
     }
 
     newSession = (id, session) => {
@@ -13,7 +14,19 @@ export default class SessionsManager {
 
     removeSession = (id) => {
         this.sessions[id].reject()
-        
+
         delete this.sessions[id]
+    }
+
+    publishStream = (payload) => {
+        if (typeof payload !== "object") {
+            throw new Error("Payload must be an object")
+        }
+
+        this.publicStreams.push(payload)
+    }
+
+    unpublishStream = (id) => {
+        this.publicStreams = this.publicStreams.filter(stream => stream.id !== id)
     }
 }
