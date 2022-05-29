132b4cb00a3c93215a0d41860e9d9e1e44ef7521
srgooglo <srgooglo@ragestudio.net>
2022-05-29 18:24:43 +0200

    added `about` page

---
+++ packages/app/src/pages/about/index.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { AboutApp } from "components"
+
+import "./index.less"
+
+export default (props) => {
+    return <div className="app_about">
+        <AboutApp.Card />
+    </div>
+}
