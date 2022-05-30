dfcd9829e854627b550c454e277424b80229e54f
srgooglo <srgooglo@ragestudio.net>
2022-05-30 16:48:31 +0200

    added `CrashWrapper`

--- packages/app/src/components/Crash/index.jsx
+++ packages/app/src/components/Crash/index.jsx
@@ -1,23 +1,34 @@
 import React from "react"
 import { Result, Button } from "antd"
 
-export default (props) => {
+import "./index.less"
+
+export const CrashComponent = (props) => {
     const { crash } = props
 
-    return <div className="app_crash_wrapper">
-        <Result
-            status="error"
-            title="Crash"
-            subTitle={crash.message}
-            extra={[
-                <Button type="primary" key="reload" onClick={() => window.location.reload()}>
-                    Reload app
-                </Button>
-            ]}
-        >
-            <div>
-                <code>{crash.error}</code>
-            </div>
-        </Result>
+    return <Result
+        status="error"
+        title="Crash"
+        subTitle={crash.message}
+        extra={[
+            <Button type="primary" key="reload" onClick={() => window.location.reload()}>
+                Reload app
+            </Button>
+        ]}
+    >
+        <div>
+            <code>{crash.error}</code>
+        </div>
+    </Result>
+}
+
+export const CrashWrapper = (props) => {
+    const { crash } = props
+
+    return <div className="crashWrapper">
+        <CrashComponent crash={crash} />
     </div>
 }
+
+
+export default CrashComponent
