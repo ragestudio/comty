0065423d47256bcb07f5c84c9a82afafdbf292f8
srgooglo <srgooglo@ragestudio.net>
2022-06-03 19:34:16 +0200

    added cors to `server`

--- packages/app/server.js
+++ packages/app/server.js
@@ -1,9 +1,13 @@
 const express = require("express")
 const path = require("path")
+const cors = require("cors")
+
 const app = express()
 
 const portFromArgs = process.argv[2]
 let portListen = portFromArgs ? portFromArgs : 9000
+
+app.use(cors())
 
 app.use(express.static(path.join(__dirname, "dist")))
 
