67865c99cbe2867b8f7bdda68e766b36d91762d4
srgooglo <srgooglo@ragestudio.net>
2022-06-03 20:56:20 +0200

    added cors options

--- packages/app/server.js
+++ packages/app/server.js
@@ -7,7 +7,12 @@ const app = express()
 const portFromArgs = process.argv[2]
 let portListen = portFromArgs ? portFromArgs : 9000
 
-app.use(cors())
+app.use(cors({
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: true,
+  optionsSuccessStatus: 204
+}))
 
 app.use(express.static(path.join(__dirname, "dist")))
 
