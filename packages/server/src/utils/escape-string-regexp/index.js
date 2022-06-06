23f60b3b58091425c67d6cb33596228d2022fca9
srgooglo <srgooglo@ragestudio.net>
2022-06-06 17:28:21 +0200

    added utils

---
+++ packages/server/src/utils/escape-string-regexp/index.js
@@ -0,0 +1,12 @@
+// Copyright (c) Sindre Sorhus <sindresorhus@gmail.com> (https://sindresorhus.com)
+export default function escapeStringRegexp(string) {
+	if (typeof string !== 'string') {
+		throw new TypeError('Expected a string');
+	}
+
+	// Escape characters with special meaning either inside or outside character sets.
+	// Use a simple backslash escape when it’s always valid, and a `\xnn` escape when the simpler form would be disallowed by Unicode patterns’ stricter grammar.
+	return string
+		.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&')
+		.replace(/-/g, '\\x2d');
+}
