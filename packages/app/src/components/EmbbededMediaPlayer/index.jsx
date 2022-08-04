11b02d890dac973871b42f04f556cfc86fc3bb12
srgooglo <srgooglo@ragestudio.net>
2022-08-04 11:57:45 +0200

    added early `mediaPlayer`

---
+++ packages/app/src/components/EmbbededMediaPlayer/index.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+import "./index.less"
+
+export default (props) => {
+    return <div className="embbededMediaPlayer">
+        <div className="controls">
+            
+        </div>
+        <h1>
+            Media player
+        </h1>
+    </div>
+}
