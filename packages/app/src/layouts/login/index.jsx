d2e3c6008ff609e1c3f3ba1fb82be8a7cc9f8fa8
srgooglo <srgooglo@ragestudio.net>
2022-09-07 15:17:29 +0200

    remove antd.Layout

--- packages/app/src/layouts/login/index.jsx
+++ packages/app/src/layouts/login/index.jsx
@@ -8,12 +8,6 @@ export default (props) => {
     return <antd.Layout className="app_layout" style={{ height: "100%" }}>
         <Drawer />
         <Sidedrawer />
-        <antd.Layout className="content_layout">
-            <antd.Layout.Content className={classnames("layout_page", ...props.layoutPageModesClassnames ?? [])}>
-                <div id="transitionLayer" className="fade-transverse-active">
-                    {React.cloneElement(props.children, props)}
-                </div>
-            </antd.Layout.Content>
-        </antd.Layout>
+        {React.cloneElement(props.children, props)}
     </antd.Layout>
 }
