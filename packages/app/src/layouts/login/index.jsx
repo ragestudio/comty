3476a230ead99c28653ab6396dafdb369c40db60
srgooglo <srgooglo@ragestudio.net>
2022-09-07 13:26:19 +0200

    added `login` layout

--- packages/app/src/layouts/login/index.jsx
+++ packages/app/src/layouts/login/index.jsx
@@ -1,7 +1,19 @@
 import React from "react"
+import classnames from "classnames"
+import * as antd from "antd"
+
+import { Drawer, Sidedrawer } from "components/layout"
 
 export default (props) => {
-    return <div>
-        {props.children}
-    </div>
+    return <antd.Layout className="app_layout" style={{ height: "100%" }}>
+        <Drawer />
+        <Sidedrawer />
+        <antd.Layout className="content_layout">
+            <antd.Layout.Content className={classnames("layout_page", ...props.layoutPageModesClassnames ?? [])}>
+                <div id="transitionLayer" className="fade-transverse-active">
+                    {React.cloneElement(props.children, props)}
+                </div>
+            </antd.Layout.Content>
+        </antd.Layout>
+    </antd.Layout>
 }
