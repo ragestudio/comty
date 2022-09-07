2e74d243f55361d4cc8078fe127b89103a0dba8d
srgooglo <srgooglo@ragestudio.net>
2022-09-07 16:31:34 +0200

    fix style

--- packages/app/src/components/Login/index.jsx
+++ packages/app/src/components/Login/index.jsx
@@ -116,7 +116,11 @@ export default class Login extends React.Component {
                 <div className="session_card">
                     @{this.props.session.username}
                 </div>
-                <antd.Button type="primary" onClick={() => window.app.setLocation(config.app?.mainPath ?? "/home")} >Go to main</antd.Button>
+                <antd.Button
+                    type="primary"
+                    onClick={() => window.app.setLocation(config.app?.mainPath ?? "/home")} >
+                    Go to home
+                </antd.Button>
             </div>}
             <FormGenerator
                 name="normal_login"
