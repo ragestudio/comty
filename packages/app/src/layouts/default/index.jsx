90b126db68d74b7de0f27154066011c35546975c
srgooglo <srgooglo@ragestudio.net>
2022-09-07 15:17:53 +0200

    moved `backgroundDecorator` to layout

--- packages/app/src/layouts/default/index.jsx
+++ packages/app/src/layouts/default/index.jsx
@@ -5,16 +5,19 @@ import * as antd from "antd"
 import { Sidebar, Drawer, Sidedrawer } from "components/layout"
 
 export default (props) => {
-    return <antd.Layout className="app_layout" style={{ height: "100%" }}>
-        <Drawer />
-        <Sidebar user={props.user} />
-        <Sidedrawer />
-        <antd.Layout className="content_layout">
-            <antd.Layout.Content className={classnames("layout_page", ...props.layoutPageModesClassnames ?? [])}>
-                <div id="transitionLayer" className="fade-transverse-active">
-                    {React.cloneElement(props.children, props)}
-                </div>
-            </antd.Layout.Content>
+    return <>
+        <div className="backgroundDecorator" />
+        <antd.Layout className="app_layout" style={{ height: "100%" }}>
+            <Drawer />
+            <Sidebar user={props.user} />
+            <Sidedrawer />
+            <antd.Layout className="content_layout">
+                <antd.Layout.Content className={classnames("layout_page", ...props.layoutPageModesClassnames ?? [])}>
+                    <div id="transitionLayer" className="fade-transverse-active">
+                        {React.cloneElement(props.children, props)}
+                    </div>
+                </antd.Layout.Content>
+            </antd.Layout>
         </antd.Layout>
-    </antd.Layout>
+    </>
 }
