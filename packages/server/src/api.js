71491750d2eb3c86d73fbe4ce8d55318eb0cb228
srgooglo <srgooglo@ragestudio.net>
2022-09-08 16:51:25 +0200

    expose `BadgesController` to api

--- packages/server/src/api.js
+++ packages/server/src/api.js
@@ -31,6 +31,7 @@ export default class Server {
         controllers.PublicController,
         controllers.PostsController,
         controllers.StreamingController,
+        controllers.BadgesController,
     ]
 
     middlewares = middlewares
