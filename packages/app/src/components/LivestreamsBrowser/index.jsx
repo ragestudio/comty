c78141a622ef0dca0e8613c9a416b23a74f34605
srgooglo <srgooglo@ragestudio.net>
2022-09-08 19:03:33 +0200

    added TODO

--- packages/app/src/components/LivestreamsBrowser/index.jsx
+++ packages/app/src/components/LivestreamsBrowser/index.jsx
@@ -3,6 +3,7 @@ import * as antd from "antd"
 
 import "./index.less"
 
+// TODO: Implement this component
 export default (props) => {
     return <div className="livestreamsBrowser">
         <antd.Empty />
