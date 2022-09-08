3d0f07daa976aef86e00e911b1c20185f4f6ef35
srgooglo <srgooglo@ragestudio.net>
2022-09-08 18:41:04 +0200

    added panels to `home`

--- packages/app/src/pages/home/index.jsx
+++ packages/app/src/pages/home/index.jsx
@@ -1,15 +1,110 @@
 import React from "react"
-import { PostCreator, PostsFeed } from "components"
+import * as antd from "antd"
+import classnames from "classnames"
+
+import { Icons, createIconRender } from "components/Icons"
+
+import { PostCreator, PostsFeed, LivestreamsBrowser } from "components"
 
 import "./index.less"
 
-export default class PostsExplorer extends React.Component {
-    render() {
-        return <div className="explore">
-            <div className="header">
+const Tabs = {
+    "feed": {
+        title: "Feed",
+        icon: "Rss",
+        component: (props) => {
+            return <>
                 <PostCreator />
+                <PostsFeed />
+            </>
+        }
+    },
+    "livestrems": {
+        title: "Livestrems",
+        icon: "Tv",
+        component: (props) => {
+            return <>
+                <LivestreamsBrowser />
+            </>
+        }
+    },
+}
+
+export default class Dashboard extends React.Component {
+    state = {
+        activeTab: "feed"
+    }
+
+    primaryPanelRef = React.createRef()
+
+    componentDidMount() {
+        app.eventBus.emit("style.compactMode", false)
+    }
+
+    renderActiveTab() {
+        const tab = Tabs[this.state.activeTab]
+
+        if (!tab) {
+            return null
+        }
+
+        return React.createElement(tab.component)
+    }
+
+    handleTabChange = (key) => {
+        if (this.state.activeTab === key) return
+
+        // set to primary panel fade-opacity-leave class
+        this.primaryPanelRef.current.classList.add("fade-opacity-leave")
+
+
+        setTimeout(() => {
+            this.setState({ activeTab: key })
+        }, 200)
+
+        // remove fade-opacity-leave class after animation
+        setTimeout(() => {
+            this.primaryPanelRef.current.classList.remove("fade-opacity-leave")
+        }, 300)
+    }
+
+    render() {
+        return <div className="dashboard">
+            <div></div>
+
+            <div
+                ref={this.primaryPanelRef}
+                className={classnames("panel", "fade-opacity-active")}
+                style={{ width: "102%" }}
+            >
+                {this.renderActiveTab()}
             </div>
-            <PostsFeed />
+
+            <div className="panel">
+                <div className="card">
+                    <div>
+                        <h2><Icons.Compass /> Browse</h2>
+                        <antd.Menu
+                            mode="inline"
+                            selectedKeys={[this.state.activeTab]}
+                            activeKey={this.state.activeTab}
+                            onClick={({ key }) => this.handleTabChange(key)}
+                        >
+                            {Object.keys(Tabs).map((key) => {
+                                const tab = Tabs[key]
+
+                                return <antd.Menu.Item
+                                    key={key}
+                                    icon={createIconRender(tab.icon)}
+                                >
+                                    {tab.title}
+                                </antd.Menu.Item>
+                            })}
+                        </antd.Menu>
+                    </div>
+
+                </div>
+            </div>
         </div>
     }
 }
