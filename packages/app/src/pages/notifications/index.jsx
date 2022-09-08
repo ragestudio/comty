efdfb8a07689795f8dae364f97df6f4448d94714
srgooglo <srgooglo@ragestudio.net>
2022-09-08 19:05:38 +0200

    added `notifications` page

---
+++ packages/app/src/pages/notifications/index.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+import "./index.less"
+
+// TODO: Implement this component
+export default (props) => {
+    return <div className="noticationsCenter">
+
+    </div>
+}
