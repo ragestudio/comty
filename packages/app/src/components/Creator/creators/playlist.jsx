562ec6561a7f8a48d527f4d67c42ac7cc87e9bcf
srgooglo <srgooglo@ragestudio.net>
2022-09-29 10:10:23 +0200

    move creators to folders

--- packages/app/src/components/Creator/creators/playlist.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-export default (props) => {
-    return <div className="content">
-        <h1>Playlist Creator</h1>
-    </div>
-}
---
+++ packages/app/src/components/Creator/creators/playlist/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import * as antd from "antd"
+
+import { Icons, createIconRender } from "components/Icons"
+
+import "./index.less"
+
+export default (props) => {
+    return <div className="content">
+        
+    </div>
+}
