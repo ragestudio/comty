4427cce386e6f0246060a375f485fd1d8909a992
srgooglo <srgooglo@ragestudio.net>
2022-09-29 22:00:51 +0200

    non logged users navs to login on "/" route

--- packages/app/src/pages/index.jsx
+++ packages/app/src/pages/index.jsx
@@ -2,6 +2,13 @@ import React from "react"
 import config from "config"
 
 export default (props) => {
+    const isLogged = typeof props.user === "object"
+
+    if (!isLogged) {
+        window.app.setLocation(config.app?.authPath ?? "/login")
+        return <div />
+    }
+
     window.app.setLocation(config.app?.mainPath ?? "/home")
     return <div />
 }
