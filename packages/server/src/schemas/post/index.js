5727c4dfefaa40b2f870e6788e2b6a5e50fa7d67
srgooglo <srgooglo@ragestudio.net>
2022-10-06 17:49:49 +0200

    remove `comments` fields from post schema

--- packages/server/src/schemas/post/index.js
+++ packages/server/src/schemas/post/index.js
@@ -3,7 +3,6 @@ export default {
     created_at: { type: Number, required: true },
     message: { type: String, required: true },
     likes: { type: Array, default: [] },
-    comments: { type: Array, default: [] },
     additions: { type: Array, default: [] },
     type: { type: String, default: "text" },
     data: { type: Object, default: {} },
