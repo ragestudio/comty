d57aca1e85ceb1dffaf3d42b646bd2efdc250a36
srgooglo <srgooglo@ragestudio.net>
2022-10-06 17:32:22 +0200

    added `SearchController`

---
+++ packages/server/src/controllers/SearchController/index.js
@@ -0,0 +1,31 @@
+import { Controller } from "linebridge/dist/server"
+import { User, Post } from "../../models"
+
+export default class SearchController extends Controller {
+    get = {
+        "/search": {
+            middlewares: ["withOptionalAuthentication"],
+            fn: async (req, res) => {
+                const { keywords = "" } = req.query
+
+                let suggestions = {}
+
+                // search users by username or name
+                const users = await User.find({
+                    $or: [
+                        { username: { $regex: keywords, $options: "i" } },
+                        { fullName: { $regex: keywords, $options: "i" } },
+                    ],
+                })
+                    .limit(5)
+                    .select("username fullName avatar verified")
+
+                if (users.length > 0) {
+                    suggestions["users"] = users
+                }
+
+                return res.json(suggestions)
+            }
+        }
+    }
+}
