e3edad8a4014646368d042526bf3c8e994525fe2
srgooglo <srgooglo@ragestudio.net>
2022-10-08 23:54:56 +0200

    support publicRoutes & improve no_session handling

--- packages/app/src/pages/index.jsx
+++ packages/app/src/pages/index.jsx
@@ -2,13 +2,12 @@ import React from "react"
 import config from "config"
 
 export default (props) => {
-    const isLogged = typeof props.user === "object"
-
-    if (!isLogged) {
+    if (!props.session) {
         window.app.setLocation(config.app?.authPath ?? "/login")
         return <div />
     }
 
     window.app.setLocation(config.app?.mainPath ?? "/home")
+
     return <div />
 }
