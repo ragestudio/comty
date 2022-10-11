89e336ef7cf6256013e435a69a135b8044b968fd
srgooglo <srgooglo@ragestudio.net>
2022-10-12 00:48:11 +0200

    remove `streaming-server` from repo

--- packages/streaming-server/src/lib/getStreamingKeyFromStreamPath/index.js
+++
@@ -1,3 +0,0 @@
-export default function getStreamingKeyFromStreamPath(StreamPath) {
-    return StreamPath.split("/").pop()
-}
--- packages/streaming-server/src/managers/index.js
+++
@@ -1,2 +0,0 @@
-export { default as DbManager } from "./DbManager"
-export { default as SessionsManager } from "./SessionsManager"
