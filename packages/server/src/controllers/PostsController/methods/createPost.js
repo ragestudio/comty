d3735769ac554097e0c2c757adaa9a26566c7aec
srgooglo <srgooglo@ragestudio.net>
2022-10-12 16:29:58 +0200

    use utc time

--- packages/server/src/controllers/PostsController/methods/createPost.js
+++ packages/server/src/controllers/PostsController/methods/createPost.js
@@ -4,11 +4,14 @@ import getPostData from "./getPostData"
 export default async (payload) => {
     const { user_id, message, additions, type, data } = payload
 
+    // set creation date (Must be in UTC)
+    const created_at = new Date().toISOString()
+
     const post = new Post({
         user_id: typeof user_id === "object" ? user_id.toString() : user_id,
         message: String(message).toString(),
         additions: additions ?? [],
-        created_at: new Date().toISOString(),
+        created_at: created_at,
         type: type,
         data: data,
     })
