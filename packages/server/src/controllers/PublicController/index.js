8d25e7c151e643508ad0e9d4436f1a19d219e943
srgooglo <srgooglo@ragestudio.net>
2022-10-13 21:55:45 +0200

    implement default `posting_policy`

--- packages/server/src/controllers/PublicController/index.js
+++ packages/server/src/controllers/PublicController/index.js
@@ -7,6 +7,13 @@ export default class PublicController extends Controller {
     static refName = "PublicController"
 
     get = {
+        "/posting_policy": {
+            middlewares: ["withOptionalAuthentication"],
+            fn: async (req, res) => {
+                // TODO: Use `PermissionsAPI` to get the user's permissions and return the correct policy, by now it will return the default policy
+                return res.json(global.DEFAULT_POSTING_POLICY)
+            }
+        },
         "/featured_wallpapers": {
             fn: async (req, res) => {
                 const featuredWallpapers = await FeaturedWallpaper.find({})
