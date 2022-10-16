86321e56d1cb6ca18ff014509e45730671ed3475
srgooglo <srgooglo@ragestudio.net>
2022-10-16 18:22:48 +0200

    use `sharp` to transform to jpeg

--- packages/server/src/utils/indecent-prediction/index.js
+++ packages/server/src/utils/indecent-prediction/index.js
@@ -1,5 +1,9 @@
 const tf = require("@tensorflow/tfjs-node")
 import * as nsfwjs from "nsfwjs/dist"
+import sharp from "sharp"
+
+import fs from "fs"
+import path from "path"
 
 import readImage from "../read-image"
 import imageByteArray from "../image-byte-array"
@@ -16,6 +20,22 @@ export default async (payload) => {
     let { image, channels = 3 } = payload
 
     const model = await nsfwjs.load()
+
+    // check if image is not a jpg
+    if (image.indexOf(".jpg") === -1) {
+        // convert image to jpg
+        const converted = await sharp(image)
+            .jpeg()
+            .toBuffer()
+
+        // write converted image to disk (use cache)
+        const destination = path.resolve(global.uploadCachePath, `${Date.now()}.jpg`)
+
+        fs.writeFileSync(destination, converted)
+
+        // set image to the converted image
+        image = destination
+    }
 
     const logo = readImage(image)
     const input = imageToInput(logo, channels)
