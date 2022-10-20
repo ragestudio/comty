fa5ecac7668b877f6fa8149a916201d8e5458296
srgooglo <srgooglo@ragestudio.net>
2022-10-20 17:38:11 +0200

    added change password component

--- packages/app/constants/settings/security/components/changePassword/index.jsx
+++ packages/app/constants/settings/security/components/changePassword/index.jsx
@@ -1,10 +1,26 @@
 import React from "react"
 import * as antd from "antd"
 
-export default (props) => {
-    return <div>
-        <antd.Button>
-            Change password
-        </antd.Button>
+const ChangePasswordComponent = (props) => {
+    const [loading, setLoading] = React.useState(false)
+
+    return <div className="changePasswordPrompt">
+        <div className="title">
+            <h1>Change Password</h1>
+        </div>
+
+        <div className="form">
+            
+        </div>
     </div>
+}
+
+export default (props) => {
+    const onClick = () => {
+        app.SidedrawerController.open("passwordChange", ChangePasswordComponent)
+    }
+
+    return <antd.Button onClick={onClick}>
+        Change password
+    </antd.Button>
 }
---
+++ packages/app/src/pages/security/change-password/index.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default (props) => {
+    return <div>
+        <h1>Change Password</h1>
+    </div>
+}
