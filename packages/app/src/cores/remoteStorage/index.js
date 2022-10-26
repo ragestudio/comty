2b8e9922ac655116c5afb0dd8f7cde6a13346d79
srgooglo <srgooglo@ragestudio.net>
2022-10-26 15:10:16 +0000

    added remoteStorage file

---
+++ packages/app/src/cores/remoteStorage/index.js
@@ -0,0 +1,19 @@
+import Core from "evite/src/core"
+import config from "config"
+import axios from "axios"
+
+export default class RemoteStorage extends Core {
+    static namespace = "RemoteStorage"
+    static public = []
+
+    connection = null
+
+    async initialize() {
+
+    }
+
+    async connect() {
+
+    }
+
+}
