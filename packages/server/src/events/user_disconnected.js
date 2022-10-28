0a38ac1f66235c951ac7645617deba4fbc2a4dc3
srgooglo <srgooglo@ragestudio.net>
2022-10-28 22:06:33 +0000

    implement internal events

---
+++ packages/server/src/events/user_disconnected.js
@@ -0,0 +1,19 @@
+import { UserFollow } from "../models"
+
+export default async (user_id) => {
+    // get followers of the user
+    const followers = await UserFollow.find({
+        to: user_id,
+    })
+
+    // send event to ws clients (if are connected)
+    followers.forEach((follow) => {
+        const connectedClient = global.wsInterface.clients.find((client) => {
+            return client.user_id === follow.user_id
+        })
+
+        if (connectedClient) {
+            connectedClient.socket.emit("friend.disconnected", user_id)
+        }
+    })
+}
