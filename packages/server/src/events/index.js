df17cd73af30c11727f451b48bace1d7a8a1846f
srgooglo <srgooglo@ragestudio.net>
2022-10-28 22:06:33 +0000

    implement internal events

---
+++ packages/server/src/events/index.js
@@ -0,0 +1,7 @@
+import user_connected from "./user_connected"
+import user_disconnected from "./user_disconnected"
+
+export default {
+    "user.connected": user_connected,
+    "user.disconnected": user_disconnected,
+}
