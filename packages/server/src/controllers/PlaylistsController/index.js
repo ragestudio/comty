502585cd654f3e7dfb6bb22a6ff3d897935ab49c
srgooglo <srgooglo@ragestudio.net>
2022-11-12 08:57:11 +0000

    added `PlaylistController`

--- packages/server/src/controllers/PlaylistsController/index.js
+++ packages/server/src/controllers/PlaylistsController/index.js
@@ -1,6 +1,8 @@
 import { Controller } from "linebridge/dist/server"
 import { Schematized } from "../../lib"
 
+import publishPlaylist from "./methods/publishPlaylist"
+
 export default class PlaylistsController extends Controller {
     //static useMiddlewares = ["withAuthentication"]
 
@@ -13,9 +15,35 @@ export default class PlaylistsController extends Controller {
     post = {
         "/playlist/publish": {
             middlewares: ["withAuthentication"],
-            fn: async (req, res) => {
-                
-            }
+            fn: Schematized({
+                required: ["title", "list"],
+                select: ["title", "description", "thumbnail", "list"],
+            },async (req, res) => {
+                if (typeof req.body.list === "undefined") {
+                    return res.status(400).json({
+                        error: "list is required"
+                    })
+                }
+
+                // parse
+                req.selection.list = JSON.parse(req.selection.list)
+
+                const result = await publishPlaylist({
+                    user_id: req.user._id.toString(),
+                    ...req.selection
+                }).catch((err) => {
+                    res.status(500).json({
+                        error: err.message
+                    })
+
+                    return null
+                })
+
+                if (result) {
+                    return res.json(result)
+                }
+            })
+            
         }
     }
 }
