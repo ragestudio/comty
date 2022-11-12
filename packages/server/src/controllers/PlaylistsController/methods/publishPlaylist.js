426eb7b47e39314e0f00ca78d9ed262e4b6e28b8
srgooglo <srgooglo@ragestudio.net>
2022-11-12 08:58:29 +0000

    validate types

--- packages/server/src/controllers/PlaylistsController/methods/publishPlaylist.js
+++ packages/server/src/controllers/PlaylistsController/methods/publishPlaylist.js
@@ -3,6 +3,14 @@ import { Playlist } from "../../../models"
 export default async (payload) => {
     const { user_id, title, description, thumbnail, list } = payload
 
+    if (!title) {
+        throw new Error("Title is required")
+    }
+
+    if (!Array.isArray(list)) {
+        throw new Error("list is not an array")
+    }
+
     const playlist = new Playlist({
         user_id,
         created_at: Date.now(),
