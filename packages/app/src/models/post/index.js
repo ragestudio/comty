28ca8a633b0f253c313edee7396162f7f3b34a7f
srgooglo <srgooglo@ragestudio.net>
2022-11-14 01:29:01 +0000

    implement `getExplorePosts`

--- packages/app/src/models/post/index.js
+++ packages/app/src/models/post/index.js
@@ -51,4 +51,17 @@ export default class Post {
 
         return request
     }
+
+    static async getExplorePosts({ trim, limit }) {
+        if (!Post.bridge) {
+            throw new Error("Bridge is not available")
+        }
+
+        const request = Post.bridge.get.explorePosts(undefined, {
+            trim: trim ?? 0,
+            limit: limit ?? window.app.settings.get("feed_max_fetch"),
+        })
+
+        return request
+    }
 }
