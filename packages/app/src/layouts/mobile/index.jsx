d60ac3e373593d678a64c8434f7c8d53af152fa1
SrGooglo <srgooglo@ragestudio.net>
2022-11-22 00:37:28 +0000

    fix mobile layout

--- packages/app/src/layouts/mobile/index.jsx
+++ packages/app/src/layouts/mobile/index.jsx
@@ -2,18 +2,20 @@ import React from "react"
 import classnames from "classnames"
 import * as antd from "antd"
 
-import { Drawer, BottomBar } from "components/Layout"
+import { BottomBar, Drawer, Sidedrawer, Modal } from "components/Layout"
 
 export default (props) => {
     return <antd.Layout className={classnames("app_layout", ["mobile"])} style={{ height: "100%" }}>
-        <antd.Layout className="content_layout">
-            <antd.Layout.Content className={classnames("layout_page", ...props.layoutPageModesClassnames ?? [])}>
-                <div id="transitionLayer" className="fade-transverse-active">
-                    {React.cloneElement(props.children, props)}
-                </div>
-            </antd.Layout.Content>
-        </antd.Layout>
-        <BottomBar user={props.user} />
+        <Modal />
+
+        <antd.Layout.Content className={classnames("content_layout", ...props.layoutPageModesClassnames ?? [])}>
+            <div id="transitionLayer" className="fade-transverse-active">
+                {React.cloneElement(props.children, props)}
+            </div>
+        </antd.Layout.Content>
+
+        <BottomBar />
+        <Sidedrawer />
         <Drawer />
     </antd.Layout>
 }
