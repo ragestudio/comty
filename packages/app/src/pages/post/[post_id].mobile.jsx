82710e4ee7b8cf16dccd3aaba90120cc97da5e23
SrGooglo <srgooglo@ragestudio.net>
2022-11-22 00:33:28 +0000

    optimize for mobile

---
+++ packages/app/src/pages/post/[post_id].mobile.jsx
@@ -0,0 +1,39 @@
+import React from "react"
+import * as antd from "antd"
+
+import Post from "models/post"
+import { PostCard, CommentsCard } from "components"
+
+import "./index.less"
+
+export default (props) => {
+    const post_id = props.match.params.post_id
+
+    const [data, setData] = React.useState(null)
+
+    const loadData = async () => {
+        setData(null)
+
+        const data = await Post.getPost({ post_id }).catch(() => {
+            antd.message.error("Failed to get post")
+
+            return false
+        })
+
+        if (data) {
+            setData(data)
+        }
+    }
+
+    React.useEffect(() => {
+        loadData()
+    }, [])
+
+    if (!data) {
+        return <antd.Skeleton active />
+    }
+
+    return <div className="postPage">
+        <PostCard data={data} fullmode />
+    </div>
+}
