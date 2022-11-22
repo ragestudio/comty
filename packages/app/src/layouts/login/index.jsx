c9aec03b3221683328a85ace54e1d6b67234d1ce
SrGooglo <srgooglo@ragestudio.net>
2022-11-22 00:33:03 +0000

    check with `mobile` mode for login layour

--- packages/app/src/layouts/login/index.jsx
+++ packages/app/src/layouts/login/index.jsx
@@ -1,10 +1,11 @@
 import React from "react"
 import * as antd from "antd"
+import classnames from "classnames"
 
 import { Drawer, Sidedrawer } from "components/Layout"
 
 export default (props) => {
-    return <antd.Layout className="app_layout" style={{ height: "100%" }}>
+    return <antd.Layout className={classnames("app_layout", { ["mobile"]: window.isMobile })} style={{ height: "100%" }}>
         <Drawer />
         <Sidedrawer />
         <div id="transitionLayer" className="fade-transverse-active">
