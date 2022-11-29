aa9906c8a3fd4292788a788f995df9bf31383cf2
SrGooglo <srgooglo@ragestudio.net>
2022-11-29 07:33:56 +0000

    get tabs from component

--- packages/app/src/pages/home/[type].mobile.jsx
+++ packages/app/src/pages/home/[type].mobile.jsx
@@ -2,35 +2,9 @@ import React from "react"
 import * as antd from "antd"
 import classnames from "classnames"
 
-import FeedTab from "./components/feed"
-import ExploreTab from "./components/explore"
-import TrendingsTab from "./components/trendings"
-import SavedPostsTab from "./components/savedPosts"
+import Tabs from "./tabs"
 
 import "./index.less"
-
-const Tabs = {
-    "feed": {
-        title: "Feed",
-        icon: "Rss",
-        component: FeedTab
-    },
-    "trendings": {
-        title: "Trendings",
-        icon: "TrendingUp",
-        component: TrendingsTab
-    },
-    "explore": {
-        title: "Explore",
-        icon: "Search",
-        component: ExploreTab
-    },
-    "savedPosts": {
-        title: "Saved posts",
-        icon: "Bookmark",
-        component: SavedPostsTab
-    }
-}
 
 export default class Dashboard extends React.Component {
     state = {
