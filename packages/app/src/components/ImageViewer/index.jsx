eb22955faa663a9738b6c9d1b18b1411700d830a
SrGooglo <srgooglo@ragestudio.net>
2022-12-12 13:51:09 +0000

    implement `FullImageViewer`

--- packages/app/src/components/ImageViewer/index.jsx
+++ packages/app/src/components/ImageViewer/index.jsx
@@ -1,54 +1,16 @@
 import React from "react"
-import { Swiper } from "antd-mobile"
 import { LazyLoadImage } from "react-lazy-load-image-component"
-import classnames from "classnames"
 
 import "react-lazy-load-image-component/src/effects/blur.css"
 import "./index.less"
 
-const ImageViewer = (props) => {
-    React.useEffect(() => {
-        if (!Array.isArray(props.src)) {
-            props.src = [props.src]
-        }
-    }, [])
-
-    const openViewer = () => {
-        if (props.extended) {
-            return false
-        }
-
-        window.app.DrawerController.open("ImageViewer", ImageViewer, {
-            componentProps: {
-                src: props.src,
-                extended: true
-            }
-        })
-    }
-
-    return <div className={classnames("ImageViewer", { ["extended"]: props.extended })}>
-        <Swiper>
-            {props.src.map((image) => {
-                return <Swiper.Item
-                    onClick={() => {
-                        openViewer(image)
-                    }}
-                >
-                    <LazyLoadImage
-                        src={image}
-                        effect="blur"
-                        wrapperClassName="image-wrapper"
-                        onClick={() => {
-                            openViewer()
-                        }}
-                        onError={(e) => {
-                            e.target.src = "/broken-image.svg"
-                        }}
-                    />
-                </Swiper.Item>
-            })}
-        </Swiper>
-    </div>
+export default (props) => {
+    return <LazyLoadImage
+        src={props.src}
+        effect="blur"
+        wrapperClassName="image-wrapper"
+        onError={(e) => {
+            e.target.src = "/broken-image.svg"
+        }}
+    />
 }
-
-export default ImageViewer
