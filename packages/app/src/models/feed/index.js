3b7b6fe4fe48d90ea7f9ef5e973384eed002a4d6
SrGooglo <srgooglo@ragestudio.net>
2022-12-13 11:55:30 +0000

    added `getPlaylistsFeed` method

--- packages/app/src/models/feed/index.js
+++ packages/app/src/models/feed/index.js
@@ -1,16 +1,33 @@
-export default class Post {
+export default class FeedModel {
     static get bridge() {
         return window.app?.api.withEndpoints("main")
     }
 
     static async getPostsFeed({ trim, limit }) {
-        if (!Post.bridge) {
+        if (!FeedModel.bridge) {
             throw new Error("Bridge is not available")
         }
 
         const { data } = await app.api.customRequest("main", {
             method: "GET",
             url: `/feed/posts`,
+            params: {
+                trim: trim ?? 0,
+                limit: limit ?? window.app.settings.get("feed_max_fetch"),
+            }
+        })
+
+        return data
+    }
+
+    static async getPlaylistsFeed({ trim, limit }) {
+        if (!FeedModel.bridge) {
+            throw new Error("Bridge is not available")
+        }
+
+        const { data } = await app.api.customRequest("main", {
+            method: "GET",
+            url: `/feed/playlists`,
             params: {
                 trim: trim ?? 0,
                 limit: limit ?? window.app.settings.get("feed_max_fetch"),
