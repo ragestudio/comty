f6a0ab1282e7c5e13174b45316823d31df591b03
SrGooglo <srgooglo@ragestudio.net>
2022-12-13 12:00:34 +0000

    added `type` to playlist schema

--- packages/server/src/schemas/playlist/index.js
+++ packages/server/src/schemas/playlist/index.js
@@ -1,6 +1,7 @@
 export default {
     user_id: { type: String, required: true },
     created_at: { type: Date, default: Date.now, required: true },
+    type: { type: String, default: "track", required: true },
     title: { type: String, required: true },
     description: { type: String },
     thumbnail: { type: String },
