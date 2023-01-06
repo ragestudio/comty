262b3306849e54f97c50b9fe2235ebbbbe923af8
SrGooglo <srgooglo@ragestudio.net>
2023-01-06 18:14:28 +0000

    added `UserCard` component

---
+++ packages/app/src/components/UserCard/index.jsx
@@ -0,0 +1,57 @@
+import React from "react"
+import * as antd from "antd"
+
+import { Icons } from "components/Icons"
+import { Image, UserBadges } from "components"
+
+import "./index.less"
+
+export default React.forwardRef((props, ref) => {
+    const [user, setUser] = React.useState(props.user)
+
+    // TODO: Support API user data fetching 
+
+    return <div
+        className="userCard"
+        ref={ref}
+    >
+        <div className="avatar">
+            <Image
+                src={user.avatar}
+            />
+        </div>
+
+        <div className="username">
+            <div>
+                <h1>
+                    {user.fullName || user.username}
+                    {user.verified && <Icons.verifiedBadge />}
+                </h1>
+                <span>
+                    @{user.username}
+                </span>
+            </div>
+
+            {
+                user.roles.includes("admin") &&
+                <div className="indicators">
+                    <antd.Tooltip title="Administrators Team">
+                        <Icons.MdAdminPanelSettings />
+                    </antd.Tooltip>
+                </div>
+            }
+        </div>
+
+        <div className="description">
+            <h3>
+                {user.description}
+            </h3>
+        </div>
+
+        <div className="badges">
+            <React.Suspense fallback={<antd.Skeleton />}>
+                <UserBadges user_id={user._id} />
+            </React.Suspense>
+        </div>
+    </div>
+})
