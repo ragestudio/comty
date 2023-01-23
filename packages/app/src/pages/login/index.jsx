6970603db030c8958eb945a84b1e8b13e0314870
SrGooglo <srgooglo@ragestudio.net>
2023-01-23 17:34:07 +0000

    update login

--- packages/app/src/pages/login/index.jsx
+++ packages/app/src/pages/login/index.jsx
@@ -7,6 +7,28 @@ import { Footer } from "components"
 
 import "./index.less"
 
+const GradientSVG = () => {
+    return <svg height="100%" width="100%">
+        <defs>
+            <linearGradient id="0" x1="0" y1="0.5" x2="1" y2="0.5">
+                <stop offset="0%" stop-color="rgba(225, 0, 209, 0.1)" />
+                <stop offset="25%" stop-color="rgba(233, 0, 182, 0.08)" />
+                <stop offset="50%" stop-color="rgba(240, 0, 154, 0.05)" />
+                <stop offset="100%" stop-color="rgba(255, 0, 0, 0)" />
+            </linearGradient>
+            <radialGradient id="1" gradientTransform="translate(-0.81 -0.5) scale(2, 1.2)">
+                <stop offset="0%" stop-color="rgba(255, 96, 100, 0.2)" />
+                <stop offset="20%" stop-color="rgba(255, 96, 100, 0.16)" />
+                <stop offset="40%" stop-color="rgba(255, 96, 100, 0.12)" />
+                <stop offset="60%" stop-color="rgba(255, 96, 100, 0.08)" />
+                <stop offset="100%" stop-color="rgba(255, 96, 100, 0)" />
+            </radialGradient>
+        </defs>
+        <rect fill="url(#0)" height="100%" width="100%" />
+        <rect fill="url(#1)" height="100%" width="100%" />
+    </svg>
+}
+
 export default (props) => {
     const [wallpaperData, setWallpaperData] = React.useState(null)
 
@@ -35,61 +57,64 @@ export default (props) => {
     }, [])
 
     return <div className="loginPage">
-        <div
-            style={{
-                backgroundImage: `url(${wallpaperData?.url})`,
-            }}
-            className="wallpaper"
-        >
-            <p>
-                {wallpaperData?.author ? wallpaperData.author : null}
-            </p>
+        <div className="background">
+            <GradientSVG />
         </div>
 
         <div className="wrapper">
-            <img src={window.isMobile ? config.logo.alt : config.logo.full} className="logo" />
+            <div
+                className="wrapper_background"
+                style={{
+                    backgroundImage: wallpaperData ? `url(${wallpaperData.url})` : null,
+                    animation: wallpaperData ? "opacityIn 1s" : null
+                }}
+            />
 
             <div className="content">
-                <div className="title">
-                    <h1>A prototype of a social network</h1>
+                <div className="content_header">
+                    <img src={window.isMobile ? config.logo.alt : config.logo.full} className="logo" />
                 </div>
 
-                {props.session && <div className="session_available">
-                    <h3><Icons.AlertCircle /> You already have a valid session.</h3>
-                    <div className="session_card">
-                        @{props.session.username}
-                    </div>
+                <div className="actions">
                     <antd.Button
+                        onClick={onClickLogin}
+                        size="large"
+                        icon={<Icons.LogIn />}
                         type="primary"
-                        onClick={() => window.app.setLocation(config.app?.mainPath ?? "/home")} >
-                        Go to home
+                    >
+                        Continue with a Comty™ Account
                     </antd.Button>
-                </div>}
-
-                <div className="buttonsBox">
-                    <div className="register">
-                        <antd.Button
-                            type="primary"
-                            size="large"
-                            onClick={onClickRegister}
-                        >
-                            Create a new account
-                        </antd.Button>
-                        <p>Registering a new account accepts the <a>Terms and Conditions</a> and <a>Privacy policy</a> for the services provided by {config.author}</p>
-                    </div>
-
-                    <h3>
-                        You have already an account?
-                    </h3>
 
                     <antd.Button
                         onClick={onClickLogin}
+                        size="large"
+                        icon={<Icons.LogIn />}
+                        type="primary"
+                        disabled
                     >
-                        Login
+                        Continue with a RageStudio© ID™
                     </antd.Button>
                 </div>
+
+                <h4>Or create a new account</h4>
+
+                <div className="actions">
+                    <antd.Button
+                        onClick={onClickRegister}
+                        icon={<Icons.UserPlus />}
+                        type="primary"
+                    >
+                        Create a Comty™ Account
+                    </antd.Button>
+
+                    <p>
+                        <Icons.Info />
+                        Registering a new account accepts the <a>Terms and Conditions</a> and <a>Privacy policy</a> for the services provided by {config.author}
+                    </p>
+                </div>
             </div>
-            <Footer />
         </div>
+
+        <Footer />
     </div>
 }
