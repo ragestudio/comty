0260427383313a8451e01d2ba43d327f86c0cc9c
SrGooglo <srgooglo@ragestudio.net>
2023-01-23 23:16:01 +0000

    added extensions tab

--- packages/app/constants/settings/extensions/index.jsx
+++ packages/app/constants/settings/extensions/index.jsx
@@ -1,5 +1,10 @@
 import React from "react"
+import loadable from "@loadable/component"
 
-export default [
+export default {
+    icon: "MdOutlineCode",
+    label: "Extensions",
+    settings: [
 
-]
+    ]
+}
