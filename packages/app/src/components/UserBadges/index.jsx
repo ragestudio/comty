77b7d75fea6791ba44d13f5d9b1a099e50eea05d
SrGooglo <srgooglo@ragestudio.net>
2023-02-13 13:28:19 +0000

    fix api

--- packages/app/src/components/UserBadges/index.jsx
+++ packages/app/src/components/UserBadges/index.jsx
@@ -3,14 +3,14 @@ import * as antd from "antd"
 import Loadable from "react-loadable"
 
 import { createIconRender } from "components/Icons"
-import { User } from "models"
+import { UserModel } from "models"
 
 export default React.memo((props) => {
     return React.createElement(Loadable({
         loader: async () => {
             let { user_id } = props
 
-            const badgesData = await User.getUserBadges(user_id).catch((err) => {
+            const badgesData = await UserModel.getUserBadges(user_id).catch((err) => {
                 console.error(err)
 
                 app.message.error("Failed to fetch user badges")
