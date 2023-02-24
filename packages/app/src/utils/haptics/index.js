36ed74c84ef20c918150bade5d866f70c766ed80
SrGooglo <srgooglo@ragestudio.net>
2023-02-24 14:38:15 +0000

    use settings core

--- packages/app/src/utils/haptics/index.js
+++ packages/app/src/utils/haptics/index.js
@@ -2,28 +2,28 @@ import { Haptics, ImpactStyle } from "@capacitor/haptics"
 
 export default {
     selectionStart: async () => {
-        const enabled = window.app.settings.get("haptic_feedback")
+        const enabled = window.app.cores.settings.get("haptic_feedback")
 
         if (enabled) {
             await Haptics.selectionStart()
         }
     },
     selectionChanged: async () => {
-        const enabled = window.app.settings.get("haptic_feedback")
+        const enabled = window.app.cores.settings.get("haptic_feedback")
 
         if (enabled) {
             await Haptics.selectionChanged()
         }
     },
     selectionEnd: async () => {
-        const enabled = window.app.settings.get("haptic_feedback")
+        const enabled = window.app.cores.settings.get("haptic_feedback")
 
         if (enabled) {
             await Haptics.selectionEnd()
         }
     },
     impact: async (style = "Medium") => {
-        const enabled = window.app.settings.get("haptic_feedback")
+        const enabled = window.app.cores.settings.get("haptic_feedback")
 
         if (enabled) {
             style = String(style).toTitleCase()
