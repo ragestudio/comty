30c12a9c4025121061f6c038f6268f27bee098fb
SrGooglo <srgooglo@ragestudio.net>
2023-02-27 09:15:29 +0000

    fixes

--- packages/server/src/controllers/PostsController/services/createPost.js
+++ packages/server/src/controllers/PostsController/services/createPost.js
@@ -1,5 +1,5 @@
 import momentTimezone from "moment-timezone"
-import { Post } from "../../../models"
+import { Post } from "@models"
 
 import getPostData from "./getPostData"
 import flagNsfwByAttachments from "./flagNsfwByAttachments"
