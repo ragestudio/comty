c5f5583f0d351d416fd58f6862e4048edfc829ef
SrGooglo <srgooglo@ragestudio.net>
2023-03-03 22:01:23 +0000

    added logic

--- packages/app/src/components/HashtagTrendings/index.jsx
+++ packages/app/src/components/HashtagTrendings/index.jsx
@@ -1,10 +1,29 @@
 import React from "react"
+import { Icons } from "components/Icons"
 
 import "./index.less"
 
+const TrendingItem = (props) => {
+    return <div
+        key={props.index}
+        className="trendingItem"
+    >
+
+    </div>
+}
+
 // TODO: Implement this component
 export default (props) => {
-    return <div className="hashtagTrendings">
+    const [trendings, setTrendings] = React.useState([])
 
+    return <div className="hashtagTrendings">
+        {
+            trendings.map((trending, index) => {
+                return <TrendingItem
+                    index={index}
+                    data={trending}
+                />
+            })
+        }
     </div>
 }
