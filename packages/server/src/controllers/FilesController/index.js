f71a22440ed66ea6669e5e0f61338b33424b5c00
SrGooglo <srgooglo@ragestudio.net>
2023-03-06 02:15:53 +0000

    added `/objects`

--- packages/server/src/controllers/FilesController/index.js
+++ packages/server/src/controllers/FilesController/index.js
@@ -4,8 +4,48 @@ import uploadBodyFiles from "./services/uploadBodyFiles"
 
 export default class FilesController extends Controller {
     static refName = "FilesController"
+    static useRoute = "/files"
 
     httpEndpoints = {
+        get: {
+            "/objects": {
+                middlewares: ["withAuthentication"],
+                fn: async (req, res) => {
+                    const user_id = req.user.id
+
+                    let totalSize = 0
+
+                    const objectsPath = `${user_id}/`
+
+                    const objects = await new Promise((resolve, reject) => {
+                        const objects = []
+
+                        const objectsStream = global.storage.listObjects(global.storage.defaultBucket, objectsPath, true)
+
+                        objectsStream.on("data", (obj) => {
+                            objects.push(obj)
+                        })
+
+                        objectsStream.on("error", (err) => {
+                            return reject(err)
+                        })
+
+                        objectsStream.on("end", () => {
+                            return resolve(objects)
+                        })
+                    })
+
+                    for await (const object of objects) {
+                        totalSize += object.size
+                    }
+
+                    return res.json({
+                        totalSize,
+                        objects,
+                    })
+                }
+            }
+        },
         post: {
             "/upload": {
                 middlewares: ["withAuthentication"],
