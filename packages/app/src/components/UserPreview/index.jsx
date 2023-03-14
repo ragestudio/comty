0a77fd5ea8c4aff8d379db2506ec271596f10910
SrGooglo <srgooglo@ragestudio.net>
2023-03-14 19:49:08 +0000

    use skeleton avatar

--- packages/app/src/components/UserPreview/index.jsx
+++ packages/app/src/components/UserPreview/index.jsx
@@ -49,7 +49,7 @@ export default (props) => {
 
     if (!userData) {
         return <div className="userPreview">
-            <Skeleton active />
+            <Skeleton.Avatar active />
         </div>
     }
 
