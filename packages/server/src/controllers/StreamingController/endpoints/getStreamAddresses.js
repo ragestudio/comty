aa9dbcf7d5d00eb09e64f4b6853db64f7b70f381
SrGooglo <srgooglo@ragestudio.net>
2023-03-30 22:50:49 +0000

    added `aac` address

--- packages/server/src/controllers/StreamingController/endpoints/getStreamAddresses.js
+++ packages/server/src/controllers/StreamingController/endpoints/getStreamAddresses.js
@@ -9,11 +9,15 @@ export default {
         }
 
         if (req.user) {
-            addresses.liveURL = `${addresses.api}/live/${req.user.username}`
             addresses.ingestURL = `${addresses.ingest}/${req.user.username}`
+
+            addresses.liveURL = `${addresses.api}/live/${req.user.username}`
+            addresses.radioURL = `${addresses.api}/radio/${req.user.username}`
 
             addresses.hlsURL = `${addresses.liveURL}/src.m3u8`
             addresses.flvURL = `${addresses.liveURL}/src.flv`
+            
+            addresses.aacURL = `${addresses.radioURL}/src.aac`
         }
 
         return res.json(addresses)
