d2e413f7eb3d3adfdbd77517ed7d71c6fb027b10
SrGooglo <srgooglo@ragestudio.net>
2023-04-03 16:22:51 +0000

    remove unnecesary tabs

--- packages/app/src/pages/tv/tabs.jsx
+++ packages/app/src/pages/tv/tabs.jsx
@@ -1,13 +1,7 @@
-import FeedTab from "./components/feed"
 import ExploreTab from "./components/explore"
 import ControlPanelTab from "./components/controlPanel"
 
 export default {
-    "feed": {
-        label: "Feed",
-        icon: "Rss",
-        component: FeedTab
-    },
     "explore": {
         label: "Explore",
         icon: "Search",
