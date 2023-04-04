f03666e3f25def2e56999f48b6bd2920f0635ec3
SrGooglo <srgooglo@ragestudio.net>
2023-04-04 10:49:45 +0000

    improve page layouts

--- packages/app/src/pages/music/tabs.jsx
+++ packages/app/src/pages/music/tabs.jsx
@@ -1,28 +1,48 @@
 import FeedTab from "./components/feed"
 import SpacesTab from "./components/spaces"
-import DashboardTab from "./components/dashboard"
 
-export default {
-    "feed": {
+import DashboardTab from "./components/dashboard"
+import ReleasesTab from "./components/dashboard/releases"
+
+export default [
+    {
+        key: "feed",
         label: "Feed",
         icon: "Compass",
         component: FeedTab
     },
-    "library": {
+    {
+        key: "radio",
+        label: "Radio",
+        icon: "Radio",
+        disabled: true
+    },
+    {
+        key: "library",
         label: "Library",
         icon: "MdLibraryMusic",
         component: FeedTab,
         disabled: true
     },
-    "spaces": {
+    {
+        key: "spaces",
         label: "Spaces",
         icon: "MdDeck",
         component: SpacesTab,
         disabled: true
     },
-    "artistPanel": {
-        label: "Artist Panel",
-        icon: "MdOutlineDashboard",
+    {
+        key: "artist_panel",
+        label: "Creator Panel",
+        icon: "MdSpaceDashboard",
         component: DashboardTab,
+        children: [
+            {
+                key: "artist_panel.releases",
+                label: "Releases",
+                icon: "MdUpcoming",
+                component: ReleasesTab,
+            }
+        ]
     },
-}
+]
--- packages/app/src/pages/tv/tabs.jsx
+++ packages/app/src/pages/tv/tabs.jsx
@@ -1,15 +1,36 @@
-import ExploreTab from "./components/explore"
+import FeedTab from "./components/feed"
 import ControlPanelTab from "./components/controlPanel"
 
-export default {
-    "explore": {
-        label: "Explore",
-        icon: "Search",
-        component: ExploreTab
+export default [
+    {
+        key: "feed",
+        label: "Feed",
+        icon: "Compass",
+        component: FeedTab
     },
-    "controlPanel": {
-        label: "Control Panel",
-        icon: "Settings",
-        component: ControlPanelTab
+    {
+        key: "controlPanel",
+        label: "Creator Panel",
+        icon: "MdSpaceDashboard",
+        children: [
+            {
+                key: "controlPanel.uploads",
+                label: "Uploads",
+                icon: "Upload",
+                disabled: true
+            },
+            {
+                key: "controlPanel.streaming_settings",
+                label: "Livestreaming",
+                icon: "Settings",
+                component: ControlPanelTab
+            },
+            {
+                key: "controlPanel.dvr_settings",
+                label: "DVR",
+                icon: "MdFiberDvr",
+                disabled: true
+            }
+        ]
     }
-}
+]
