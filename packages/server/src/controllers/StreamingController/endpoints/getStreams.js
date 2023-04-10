df7185d7410e986507dac960a6b0263ed4b22a79
SrGooglo <srgooglo@ragestudio.net>
2023-04-10 16:19:10 +0000

    use `services/fetchRemoteStreams`

--- packages/server/src/controllers/StreamingController/endpoints/getStreams.js
+++ packages/server/src/controllers/StreamingController/endpoints/getStreams.js
@@ -1,11 +1,11 @@
-import fetchStreamsFromAPI from "../services/fetchStreamsFromAPI"
+import fetchRemoteStreams from "@services/fetchRemoteStreams"
 
 export default {
     method: "GET",
     route: "/streams",
     fn: async (req, res) => {
         if (req.query.username) {
-            const stream = await fetchStreamsFromAPI(`live/${req.query.username}${req.query.profile_id ? `:${req.query.profile_id}` : ""}`)
+            const stream = await fetchRemoteStreams(`live/${req.query.username}${req.query.profile_id ? `:${req.query.profile_id}` : ""}`)
 
             if (!stream) {
                 return res.status(404).json({
@@ -15,7 +15,7 @@ export default {
 
             return res.json(stream)
         } else {
-            const streams = await fetchStreamsFromAPI()
+            const streams = await fetchRemoteStreams()
 
             return res.json(streams)
         }
