5a79ecabbf06c710b2d8e6c746c96b007d0193f7
SrGooglo <srgooglo@ragestudio.net>
2023-04-17 21:50:22 +0000

    remove unused method

--- packages/app/src/models/playlists/index.js
+++ packages/app/src/models/playlists/index.js
@@ -1,61 +1,12 @@
 export default class PlaylistsModel {
-    static async uploadTrack(file, payload) {
-        // get the file from the payload
-        if (!file) {
-            throw new Error("File is required")
+    static async putPlaylist(payload) {
+        if (!payload) {
+            throw new Error("Payload is required")
         }
 
-        console.log(file)
-
-        // create a new form data
-        const formData = new FormData()
-
-        // append the file to the form data
-        formData.append("files", file)
-
-        // send the request
-        const uploadRequest = await app.cores.api.customRequest({
-            method: "POST",
-            url: "/upload",
-            data: formData,
-        })
-
-        console.log(uploadRequest.data)
-
-        if (!uploadRequest.data.files[0]) {
-            throw new Error("Upload failed")
-        }
-
-        // get the url
-        const source = uploadRequest.data.files[0].url
-
-        // send the request for the track to be created
-        const trackRequest = await app.cores.api.customRequest({
-            method: "POST",
-            url: "/tracks/publish",
-            data: {
-                ...payload,
-                source,
-            }
-        })
-
-        return trackRequest.data
-    }
-
-    static async publishTrack(payload) {
         const { data } = await app.cores.api.customRequest({
-            method: "POST",
-            url: "/tracks/publish",
-            data: payload,
-        })
-
-        return data
-    }
-
-    static async publish(payload) {
-        const { data } = await app.cores.api.customRequest({
-            method: "POST",
-            url: `/playlist/publish`,
+            method: "PUT",
+            url: `/playlist`,
             data: payload,
         })
 
@@ -75,38 +26,6 @@ export default class PlaylistsModel {
         const { data } = await app.cores.api.customRequest({
             method: "GET",
             url: `/playlist/self`,
-        })
-
-        return data
-    }
-
-    static async updateTrack(payload) {
-        if (!payload) {
-            throw new Error("Payload is required")
-        }
-
-        const { data } = await app.cores.api.customRequest({
-            method: "PUT",
-            url: `/tracks/${payload._id}`,
-            data: {
-                payload
-            },
-        })
-
-        return data
-    }
-
-    static async updatePlaylist(payload) {
-        if (!payload) {
-            throw new Error("Payload is required")
-        }
-
-        const { data } = await app.cores.api.customRequest({
-            method: "PUT",
-            url: `/playlist/${payload._id}`,
-            data: {
-                payload
-            },
         })
 
         return data
