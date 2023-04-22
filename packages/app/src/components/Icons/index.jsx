57f77d6b83438ead26854999025e160b2198e157
SrGooglo <srgooglo@ragestudio.net>
2023-04-22 23:06:45 +0000

    use customIcons

--- packages/app/src/components/Icons/index.jsx
+++ packages/app/src/components/Icons/index.jsx
@@ -1,16 +1,30 @@
 import React from "react"
 
+import VrChatIcon from "./customIcons/vrchat"
+import VerifiedBadge from "./customIcons/verifiedBadge"
+
 // import icons lib
 import * as lib1 from "feather-reactjs"
-import * as lib2 from "react-icons/md"
-import * as lib3 from "@ant-design/icons"
+import * as lib2 from "@ant-design/icons"
+import * as lib3 from "react-icons/md"
 import * as lib4 from "react-icons/io"
 import * as lib5 from "react-icons/si"
+import * as lib6 from "react-icons/fa"
 
-const marginedStyle = { width: "1em", height: "1em", marginRight: "10px", verticalAlign: "-0.125em" }
+const marginedStyle = {
+    width: "1em",
+    height: "1em",
+    marginRight: "10px",
+    verticalAlign: "-0.125em"
+}
 
 const customs = {
-    verifiedBadge: () => <svg style={marginedStyle} xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"> <path d="M23 12l-2.44-2.78.34-3.68-3.61-.82-1.89-3.18L12 3 8.6 1.54 6.71 4.72l-3.61.81.34 3.68L1 12l2.44 2.78-.34 3.69 3.61.82 1.89 3.18L12 21l3.4 1.46 1.89-3.18 3.61-.82-.34-3.68L23 12m-13 5l-4-4 1.41-1.41L10 14.17l6.59-6.59L18 9l-8 8z"></path></svg>,
+    verifiedBadge: () => <VerifiedBadge
+        style={marginedStyle}
+    />,
+    VrChat: () => <VrChatIcon
+        style={marginedStyle}
+    />,
 }
 
 export const Icons = {
@@ -20,6 +34,7 @@ export const Icons = {
     ...lib3,
     ...lib4,
     ...lib5,
+    ...lib6,
 }
 
 export function createIconRender(icon, props) {
