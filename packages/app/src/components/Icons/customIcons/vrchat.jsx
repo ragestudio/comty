3529e90060b7996d0d14f3e912501ba361df1cde
SrGooglo <srgooglo@ragestudio.net>
2023-04-22 23:06:45 +0000

    use customIcons

---
+++ packages/app/src/components/Icons/customIcons/vrchat.jsx
@@ -0,0 +1,21 @@
+export default (props) => <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" fillRule="nonzero" {...props}>
+    <defs>
+        <linearGradient x1="32" y1="8.202" x2="32" y2="59.635" gradientUnits="userSpaceOnUse" id="color-1">
+            <stop offset="0" stopColor="currentColor"></stop><stop offset="1" stopColor="currentColor"></stop>
+        </linearGradient>
+        <linearGradient x1="39.899" y1="16.499" x2="40.598" y2="36.634" gradientUnits="userSpaceOnUse" id="color-2">
+            <stop offset="0" stopColor="currentColor"></stop><stop offset="1" stopColor="currentColor"></stop>
+        </linearGradient>
+        <linearGradient x1="23.858" y1="17.126" x2="24.557" y2="37.261" gradientUnits="userSpaceOnUse" id="color-3">
+            <stop offset="0" stopColor="currentColor"></stop>
+            <stop offset="1" stopColor="currentColor"></stop>
+        </linearGradient>
+    </defs>
+    <g fill="none" fillRule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" style={{ mixBlendMode: "normal" }}>
+        <g>
+            <path d="M47.99,59c-0.928,0 -1.819,-0.438 -2.386,-1.174l-9.096,-11.826h-24.508c-2.757,0 -5,-2.243 -5,-5v-29c0,-2.757 2.243,-5 5,-5h40c2.757,0 5,2.243 5,5v29c0,2.757 -2.243,5 -5,5h-1v9.99c0,1.659 -1.351,3.01 -3.01,3.01zM12,9c-1.654,0 -3,1.346 -3,3v29c0,1.654 1.346,3 3,3h25c0.311,0 0.604,0.145 0.793,0.391l9.396,12.216c0.193,0.249 0.485,0.393 0.801,0.393c0.557,0 1.01,-0.453 1.01,-1.01v-10.99c0,-0.553 0.447,-1 1,-1h2c1.654,0 3,-1.346 3,-3v-29c0,-1.654 -1.346,-3 -3,-3z" fill="url(#color-1)"></path>
+            <path d="M46.6,26.5v-6.3c0,-2.32 -1.88,-4.2 -4.2,-4.2h-4.2c-2.32,0 -4.2,1.88 -4.2,4.2v15.75c0,0.58 0.47,1.05 1.05,1.05h2.1c0.58,0 1.05,-0.47 1.05,-1.05v-5.25h1.207l2.868,5.721c0.179,0.355 0.542,0.579 0.939,0.579h2.334c0.781,0 1.289,-0.822 0.939,-1.52l-2.546,-5.081c1.555,-0.616 2.659,-2.125 2.659,-3.899zM38.2,25.45v-4.2c0,-0.58 0.47,-1.05 1.05,-1.05h2.1c0.58,0 1.05,0.47 1.05,1.05v4.2c0,0.58 -0.47,1.05 -1.05,1.05h-2.1c-0.58,0 -1.05,-0.47 -1.05,-1.05z" fill="url(#color-2)"></path>
+            <path d="M31.872,17.327l-5.16,18.9c-0.124,0.456 -0.539,0.773 -1.012,0.773h-3.688c-0.473,0 -0.888,-0.317 -1.013,-0.773l-5.16,-18.9c-0.183,-0.668 0.32,-1.327 1.012,-1.327h2.17c0.473,0 0.888,0.317 1.013,0.773l3.821,13.99l3.821,-13.99c0.125,-0.456 0.54,-0.773 1.013,-0.773h2.17c0.693,0 1.196,0.659 1.013,1.327z" fill="url(#color-3)"></path>
+        </g>
+    </g>
+</svg>
