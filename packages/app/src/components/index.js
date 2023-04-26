cc543e865f062ac54fe03d8e544ebad020748a16
SrGooglo <srgooglo@ragestudio.net>
2023-04-26 23:09:32 +0000

    remove unused components

--- packages/app/src/components/index.js
+++ packages/app/src/components/index.js
@@ -1,32 +1,33 @@
 import * as Layout from "./Layout"
 export { default as Footer } from "./Footer"
 
+export { default as Clock } from "./Clock"
+
+export { default as RenderError } from "./RenderError"
+export { default as NotFound } from "./NotFound"
+export * as Crash from "./Crash"
+
+export { default as UserRegister } from "./UserRegister"
+export { default as Login } from "./Login"
+
 // COMPLEX COMPONENTS
 export { default as FormGenerator } from "./FormGenerator"
-export { default as NotFound } from "./NotFound"
-export { default as RenderError } from "./RenderError"
 export { default as ActionsBar } from "./ActionsBar"
 export { default as SelectableList } from "./SelectableList"
 export { default as ObjectInspector } from "./ObjectInspector"
 export { default as ModifierTag } from "./ModifierTag"
-export { default as UserSelector } from "./UserSelector"
-export { default as Clock } from "./Clock"
 export { default as StepsForm } from "./StepsForm"
 export { default as DraggableDrawer } from "./DraggableDrawer"
-export * as Crash from "./Crash"
 export { default as SearchButton } from "./SearchButton"
-export { default as UserRegister } from "./UserRegister"
 export { default as Skeleton } from "./Skeleton"
 export { default as Navigation } from "./Navigation"
 export { default as ImageUploader } from "./ImageUploader"
 export { default as ImageViewer } from "./ImageViewer"
-export { default as Login } from "./Login"
 export { default as Image } from "./Image"
 export { default as LoadMore } from "./LoadMore"
 export { default as EmbbededMediaPlayer } from "./EmbbededMediaPlayer"
 export { default as HashtagTrendings } from "./HashtagTrendings"
 export { default as Searcher } from "./Searcher"
-export { default as UserPreview } from "./UserPreview"
 
 export { default as FeaturedEventAnnouncement } from "./FeaturedEventAnnouncement"
 export { default as FeaturedEventsAnnouncements } from "./FeaturedEventsAnnouncements"
@@ -55,9 +56,10 @@ export { default as UserBadges } from "./UserBadges"
 export { default as UserCard } from "./UserCard"
 export { default as FollowersList } from "./FollowersList"
 export { default as ConnectedFriends } from "./ConnectedFriends"
+export { default as UserSelector } from "./UserSelector"
+export { default as UserPreview } from "./UserPreview"
 
 // OTHERS
-export * as AdminTools from "./AdminTools"
 export * as Window from "./RenderWindow"
 
 export { Layout }
