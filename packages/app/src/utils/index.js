c9d4e2df850ff16a01d7ed3b4adea0b745d198f1
SrGooglo <srgooglo@ragestudio.net>
2023-05-09 21:36:11 +0000

    🎨 Improve `useLongPress` hook

--- packages/app/src/utils/index.js
+++ packages/app/src/utils/index.js
@@ -1,4 +1,3 @@
-export { default as useLongPress } from "./useLongPress"
 export { default as findChildById } from "./findChildById"
 export { default as cursorPosition } from "./cursorPosition"
 export { default as getBase64 } from "./getBase64"
