1f465fe9103d1190a63fca85f31a3c3ac92c2d48
SrGooglo <srgooglo@ragestudio.net>
2023-05-16 19:32:15 +0000

    added `StoragedState` utill

---
+++ packages/app/src/utils/storagedState/index.js
@@ -0,0 +1,24 @@
+import Dexie from "dexie"
+
+export default class StoragedState {
+    constructor() {
+        this.db = new Dexie("storaged_states")
+
+        this.db.version(1).stores({
+            states: "id,value",
+        })
+    }
+
+    getState = async (stateKey) => {
+        const data = await this.db.table("states").get(stateKey)
+
+        return data.value
+    }
+
+    setState = async (stateKey, value) => {
+        return await this.db.table("states").put({
+            id: stateKey,
+            value,
+        })
+    }
+}
