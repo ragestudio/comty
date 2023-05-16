c547788028e78008e9487925481289f23de8b71f
SrGooglo <srgooglo@ragestudio.net>
2023-05-16 19:39:36 +0000

    added `widgets` settings

---
+++ packages/app/constants/settings/widgets/index.jsx
@@ -0,0 +1,25 @@
+import loadable from "@loadable/component"
+
+export default {
+    id: "widgets",
+    icon: "List",
+    label: "Widgets",
+    group: "app",
+    settings: [
+        {
+            id: "widgets.urls",
+            title: "Widgets",
+            icon: "List",
+            component: loadable(() => import("../components/widgetsView")),
+            defaultValue: () => {
+                if (typeof app.cores.widgets === "undefined") {
+                    return []
+                }
+
+                return app.cores.widgets.getInstalled()
+            },
+            reloadValueOnUpdateEvent: "widgets:update",
+            storaged: false,
+        }
+    ]
+}
