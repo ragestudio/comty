e91304d92496597b6e822ba8cff4a56d31ce1b0b
SrGooglo <srgooglo@ragestudio.net>
2023-05-17 16:56:04 +0000

    support for especial character

--- packages/marketplace_server/src/utils/resolveUrl/index.js
+++ packages/marketplace_server/src/utils/resolveUrl/index.js
@@ -2,7 +2,16 @@ export default (from, to) => {
     const resolvedUrl = new URL(to, new URL(from, "resolve://"))
 
     if (resolvedUrl.protocol === "resolve:") {
-        const { pathname, search, hash } = resolvedUrl
+        let { pathname, search, hash } = resolvedUrl
+
+        if (to.includes("@")) {
+            const fromUrl = new URL(from)
+            const toUrl = new URL(to, fromUrl.origin)
+
+            pathname = toUrl.pathname
+            search = toUrl.search
+            hash = toUrl.hash
+        }
 
         return pathname + search + hash
     }
