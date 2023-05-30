47f929bde6916903182b4e09534e6162f92fdda4
SrGooglo <srgooglo@ragestudio.net>
2023-05-30 01:09:11 +0000

    remove unnecessary controllers

--- packages/server/src/controllers/FeedController/services/getPlaylistsFromFollowing.js
+++ packages/server/src/controllers/FeedController/services/getPlaylistsFromFollowing.js
@@ -1,5 +1,4 @@
-import { User, Playlist, UserFollow } from "@models"
-import getTrackDataById from "../../TracksController/services/getTrackDataById"
+import { Playlist, UserFollow } from "@models"
 
 export default async (payload) => {
     const {
@@ -30,22 +29,6 @@ export default async (payload) => {
         .sort({ created_at: -1 })
         .limit(limit)
         .skip(skip)
-
-    playlists = await Promise.all(playlists.map(async (playlist) => {
-        // get user data
-        const user = await User.findById(playlist.user_id)
-
-        playlist.list = await Promise.all(playlist.list.map(async (track_id) => {
-            return await getTrackDataById(track_id)
-        })).catch((err) => {
-            return []
-        })
-
-        return {
-            ...playlist.toObject(),
-            user: user.toObject(),
-        }
-    }))
 
     return playlists
 }
