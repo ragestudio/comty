fbfebe1f56b8490a777cdcc3bf2fd34557ec952c
SrGooglo <srgooglo@ragestudio.net>
2023-05-31 19:35:46 +0000

    remove build-in classes

--- packages/file_server/src/classes/ComtyClient/index.js
+++
@@ -1,9 +0,0 @@
-import createClient from "comty.js"
-
-export default (params = {}) => {
-    return createClient({
-        ...params,
-        accessKey: process.env.COMTY_ACCESS_KEY,
-        privateKey: process.env.COMTY_PRIVATE_KEY,
-    })
-}
