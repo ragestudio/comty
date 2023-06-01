e875f7a090cb6604ee66bd4f268aabadefb3f33d
SrGooglo <srgooglo@ragestudio.net>
2023-06-01 18:01:19 +0000

    added `useDebounce` hook

---
+++ packages/app/src/hooks/useDebounce/index.js
@@ -0,0 +1,15 @@
+export default (value, delay) => {
+    const [debouncedValue, setDebouncedValue] = React.useState(value)
+
+    React.useEffect(() => {
+        const handler = setTimeout(() => {
+            setDebouncedValue(value)
+        }, delay)
+
+        return () => {
+            clearTimeout(handler)
+        }
+    }, [value, delay])
+
+    return debouncedValue
+}
