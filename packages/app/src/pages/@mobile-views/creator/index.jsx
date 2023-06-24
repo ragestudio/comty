d1af2d4777d15f1aa8ed332a9633370dd8485d44
SrGooglo <srgooglo@ragestudio.net>
2023-06-24 12:00:39 +0000

    added creator view

---
+++ packages/app/src/pages/@mobile-views/creator/index.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import PostCreator from "components/PostCreator"
+
+import "./index.less"
+
+export default () => {
+    return <div className="_mobile_creator">
+        <PostCreator />
+    </div>
+}
