4a2ed9c1016ba1241d7dad7a7b8b1393143ecbb6
SrGooglo <srgooglo@ragestudio.net>
2023-06-26 23:34:47 +0000

    open searcher on drawer for mobile mode

--- packages/app/src/App.jsx
+++ packages/app/src/App.jsx
@@ -185,9 +185,17 @@ class ComtyApp extends React.Component {
 			openSearcher: (options) => {
 				app.cores.sound.useUIAudio("navigation.search")
 
-				window.app.ModalController.open((props) => <Searcher renderResults {...props} />)
+				if (app.isMobile) {
+					return app.DrawerController.open("searcher", Searcher, {
+						...options,
+						componentProps: {
+							renderResults: true
+						}
+					})
+				}
+
+				return window.app.ModalController.open((props) => <Searcher renderResults {...props} />)
 			},
-			openNavigationMenu: () => window.app.DrawerController.open("navigation", Navigation),
 			openFullImageViewer: (src) => {
 				const win = new DOMWindow({
 					id: "fullImageViewer",
