b3e9f4526e810ab7337181954bf14eb34ac5006b
SrGooglo <srgooglo@ragestudio.net>
2023-06-26 23:31:46 +0000

    improve `bottomBar` interface & style motion

---
+++ packages/app/src/components/Layout/quickNav/index.jsx
@@ -0,0 +1,71 @@
+import React from "react"
+import classnames from "classnames"
+import { createIconRender } from "components/Icons"
+
+const QuickNavMenuItems = [
+    {
+        id: "music",
+        icon: "MdAlbum",
+        label: "Music",
+        location: "/music"
+    },
+    {
+        id: "tv",
+        icon: "Tv",
+        label: "Tv",
+        location: "/tv"
+    },
+    {
+        id: "groups",
+        icon: "MdGroups",
+        label: "Groups",
+        location: "/groups",
+        disabled: true,
+    },
+    {
+        id: "marketplace",
+        icon: "Box",
+        label: "Marketplace",
+        location: "/marketplace",
+        disabled: true
+    },
+]
+
+export default ({
+    visible,
+}) => {
+    return <div
+        className={classnames(
+            "quick-nav",
+            {
+                ["active"]: visible
+            }
+        )}
+    >
+        {
+            QuickNavMenuItems.map((item, index) => {
+                return <div
+                    key={index}
+                    className={classnames(
+                        "quick-nav_item",
+                        {
+                            ["disabled"]: item.disabled
+                        }
+                    )}
+                    quicknav-item={item.id}
+                    disabled={item.disabled}
+                >
+                    {
+                        createIconRender(item.icon)
+                    }
+                    <h1>
+
+                        {
+                            item.label
+                        }
+                    </h1>
+                </div>
+            })
+        }
+    </div>
+}
