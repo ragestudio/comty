26d4f879fe978cab14a399221127956a0008003a
SrGooglo <srgooglo@ragestudio.net>
2023-06-26 18:22:59 +0000

    added id to layout

--- packages/app/src/layouts/mobile/index.jsx
+++ packages/app/src/layouts/mobile/index.jsx
@@ -5,7 +5,7 @@ import * as antd from "antd"
 import { BottomBar, Drawer, Sidedrawer, Modal } from "components/Layout"
 
 export default (props) => {
-    return <antd.Layout className={classnames("app_layout", ["mobile"])} style={{ height: "100%" }}>
+    return <antd.Layout id="app_layout" className={classnames("app_layout", ["mobile"])}>
         <Modal />
 
         <antd.Layout.Content className={classnames("content_layout", ...props.layoutPageModesClassnames ?? [])}>
