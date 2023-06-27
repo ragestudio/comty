a80c3a5651a4a3a123d9f07351128c318c48d475
SrGooglo <srgooglo@ragestudio.net>
2023-06-27 23:48:24 +0000

    support native media control

--- packages/app/src/cores/player/player.core.js
+++ packages/app/src/cores/player/player.core.js
@@ -1,6 +1,8 @@
 import Core from "evite/src/core"
 import { Observable } from "object-observer"
 import { FastAverageColor } from "fast-average-color"
+import { CapacitorMusicControls } from 'capacitor-music-controls-plugin-v3'
+
 //import { LRUCache } from "lru-cache/dist/mjs/index"
 
 import PlaylistModel from "comty.js/models/playlists"
@@ -34,6 +36,125 @@ const defaultAudioProccessors = [
     CompressorProcessorNode,
 ]
 
+class MediaSession {
+    initialize() {
+        CapacitorMusicControls.addListener("controlsNotification", (info) => {
+            console.log(info)
+
+            this.handleControlsEvent(info)
+        })
+
+        // ANDROID (13, see bug above as to why it's necessary)
+        document.addEventListener("controlsNotification", (event) => {
+            console.log(event)
+
+            const info = { message: event.message, position: 0 }
+
+            this.handleControlsEvent(info)
+        })
+    }
+
+    update(manifest) {
+        if ("mediaSession" in navigator) {
+            return navigator.mediaSession.metadata = new MediaMetadata({
+                title: manifest.title,
+                artist: manifest.artist,
+                album: manifest.album,
+                artwork: [
+                    {
+                        src: manifest.cover ?? manifest.thumbnail,
+                        sizes: "512x512",
+                        type: "image/jpeg",
+                    }
+                ],
+            })
+        }
+
+        return CapacitorMusicControls.create({
+            track: manifest.title,
+            artist: manifest.artist,
+            album: manifest.album,
+            cover: manifest.cover,
+
+            hasPrev: false,
+            hasNext: false,
+            hasClose: true,
+
+            isPlaying: true,
+            dismissable: false,
+
+            playIcon: "media_play",
+            pauseIcon: "media_pause",
+            prevIcon: "media_prev",
+            nextIcon: "media_next",
+            closeIcon: "media_close",
+            notificationIcon: "notification"
+        })
+    }
+
+    updateIsPlaying(to, timeElapsed = 0) {
+        if ("mediaSession" in navigator) {
+            return navigator.mediaSession.playbackState = to ? "playing" : "paused"
+        }
+
+        return CapacitorMusicControls.updateIsPlaying({
+            isPlaying: to,
+            elapsed: timeElapsed,
+        })
+    }
+
+    destroy() {
+        if ("mediaSession" in navigator) {
+            navigator.mediaSession.playbackState = "none"
+        }
+
+        this.active = false
+
+        return CapacitorMusicControls.destroy()
+    }
+
+    handleControlsEvent(action) {
+        const message = action.message
+
+        switch (message) {
+            case "music-controls-next": {
+                return app.cores.player.playback.next()
+            }
+            case "music-controls-previous": {
+                return app.cores.player.playback.previous()
+            }
+            case "music-controls-pause": {
+                return app.cores.player.playback.pause()
+            }
+            case "music-controls-play": {
+                return app.cores.player.playback.play()
+            }
+            case "music-controls-destroy": {
+                return app.cores.player.playback.stop()
+            }
+
+            // External controls (iOS only)
+            case "music-controls-toggle-play-pause": {
+                return app.cores.player.playback.toogle()
+            }
+
+            // Headset events (Android only)
+            // All media button events are listed below
+            case "music-controls-media-button": {
+                return app.cores.player.playback.toogle()
+            }
+            case "music-controls-headset-unplugged": {
+                return app.cores.player.playback.pause()
+            }
+            case "music-controls-headset-plugged": {
+                return app.cores.player.playback.play()
+            }
+            default:
+                break;
+        }
+    }
+}
+
 // TODO: Check if source playing is a stream. Also handle if it's a stream resuming after a pause will seek to the last position
 export default class Player extends Core {
     static dependencies = [
@@ -48,6 +169,8 @@ export default class Player extends Core {
     static maxBufferLoadQueue = 2
 
     static defaultSampleRate = 48000
+
+    native_controls = new MediaSession()
 
     currentDomWindow = null
 
@@ -370,6 +493,7 @@ export default class Player extends Core {
     async onInitialize() {
         this.initializeAudioProcessors()
         this.observeStateChanges()
+        this.native_controls.initialize()
     }
 
     async initializeBeforeRuntimeInitialize() {
@@ -646,7 +770,6 @@ export default class Player extends Core {
         // storage media data on browser cache to improve performance
         instanceObj.media.data = instanceObj.audioElement
 
-
         return instanceObj
     }
 
@@ -736,32 +859,11 @@ export default class Player extends Core {
         instance.audioElement.play()
 
         // set navigator metadata
-        if ("mediaSession" in navigator) {
-            navigator.mediaSession.metadata = new MediaMetadata({
-                title: instance.manifest.title,
-                artist: instance.manifest.artist,
-                album: instance.manifest.album,
-                artwork: [
-                    {
-                        src: instance.manifest.cover ?? instance.manifest.thumbnail,
-                        sizes: "512x512",
-                        type: "image/jpeg",
-                    }
-                ],
-            })
-        }
+        this.native_controls.update(instance.manifest)
 
         // check if the audio is a live stream when metadata is loaded
         instance.audioElement.addEventListener("loadedmetadata", () => {
             console.log("loadedmetadata", instance.audioElement.duration)
-
-            // if ("mediaSesion" in navigator) {
-            //     navigator.mediaSession.setPositionState({
-            //         duration: instance.audioElement.duration,
-            //         playbackRate: instance.audioElement.playbackRate,
-            //         position: instance.audioElement.currentTime,
-            //     })
-            // }
 
             if (instance.audioElement.duration === Infinity) {
                 instance.manifest.stream = true
@@ -923,9 +1025,7 @@ export default class Player extends Core {
                 resolve()
             }, gradualFadeMs)
 
-            if ("mediaSession" in navigator) {
-                navigator.mediaSession.playbackState = "paused"
-            }
+            this.native_controls.updateIsPlaying(false)
         })
     }
 
@@ -949,9 +1049,7 @@ export default class Player extends Core {
                 this.audioContext.currentTime + (gradualFadeMs / 1000)
             )
 
-            if ("mediaSession" in navigator) {
-                navigator.mediaSession.playbackState = "playing"
-            }
+            this.native_controls.updateIsPlaying(true)
         })
     }
 
@@ -967,9 +1065,7 @@ export default class Player extends Core {
 
         this.audioQueue = []
 
-        if ("mediaSession" in navigator) {
-            navigator.mediaSession.playbackState = "none"
-        }
+        this.native_controls.destroy()
     }
 
     close() {
