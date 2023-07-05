a2476f82f6bad86780aace2d09ec896075e9d14c
SrGooglo <srgooglo@ragestudio.net>
2023-07-05 19:04:25 +0000

    rename `post-process`

--- packages/file_server/src/services/post-process/index.js
+++ packages/file_server/src/services/post-process/index.js
@@ -3,12 +3,15 @@ import mimetypes from "mime-types"
 
 import processVideo from "./video"
 import processImage from "./image"
+import processAudio from "./audio"
 
 const fileTransformer = {
+    // video
     "video/avi": processVideo,
     "video/quicktime": processVideo,
     "video/mp4": processVideo,
     "video/webm": processVideo,
+    //image
     "image/jpeg": processImage,
     "image/png": processImage,
     "image/gif": processImage,
@@ -16,6 +19,17 @@ const fileTransformer = {
     "image/tiff": processImage,
     "image/webp": processImage,
     "image/jfif": processImage,
+    // audio
+    "audio/flac": processAudio,
+    "audio/x-flac": processAudio,
+    "audio/mp3": processAudio,
+    "audio/x-mp3": processAudio,
+    "audio/mpeg": processAudio,
+    "audio/x-mpeg": processAudio,
+    "audio/ogg": processAudio,
+    "audio/x-ogg": processAudio,
+    "audio/wav": processAudio,
+    "audio/x-wav": processAudio,
 }
 
 export default async (file) => {
@@ -30,7 +44,7 @@ export default async (file) => {
     const fileMimetype = mimetypes.lookup(file.filepath)
 
     if (typeof fileTransformer[fileMimetype] !== "function") {
-        console.warn(`File (${file.filepath}) has mimetype ${fileMimetype} and will not be processed`)
+        console.debug(`File (${file.filepath}) has mimetype ${fileMimetype} and will not be processed`)
 
         return file
     }
