86f6daf32168d391c820e2d5994aad21826acd89
SrGooglo <srgooglo@ragestudio.net>
2023-07-08 18:27:30 +0000

    added `MobileUserCard`

--- packages/app/src/components/UserCard/index.jsx
+++ packages/app/src/components/UserCard/index.jsx
@@ -1,8 +1,9 @@
 import React from "react"
 import * as antd from "antd"
+import classnames from "classnames"
 
 import { Icons, createIconRender } from "components/Icons"
-import { Image, UserBadges } from "components"
+import { Image, UserBadges, FollowButton } from "components"
 
 import linksDecorators from "schemas/userLinksDecorators"
 
@@ -18,6 +19,27 @@ function processValue(value, decorator) {
     return value
 }
 
+const UserLinkViewer = (props) => {
+    const { link, decorator } = props
+
+    return <div className="userLinkViewer">
+        <div className="userLinkViewer_icon">
+            {
+                createIconRender(decorator.icon ?? "MdLink")
+            }
+        </div>
+
+        <div className="userLinkViewer_value">
+            <p>
+                {
+                    link.value
+                }
+            </p>
+        </div>
+    </div>
+
+}
+
 const UserLink = (props) => {
     let { index, link } = props
 
@@ -31,6 +53,14 @@ const UserLink = (props) => {
 
     const handleOnClick = () => {
         if (!hasHref) {
+            if (app.isMobile) {
+                app.DrawerController.open("link_viewer", UserLinkViewer, {
+                    componentProps: {
+                        link: link,
+                        decorator: decorator
+                    }
+                })
+            }
             return false
         }
 
@@ -55,15 +85,17 @@ const UserLink = (props) => {
             createIconRender(decorator.icon ?? "MdLink")
         }
 
-        <p>
-            {
-                renderName()
-            }
-        </p>
+        {
+            !app.isMobile && <p>
+                {
+                    renderName()
+                }
+            </p>
+        }
     </div>
 }
 
-export default React.forwardRef((props, ref) => {
+export const UserCard = React.forwardRef((props, ref) => {
     const [user, setUser] = React.useState(props.user)
 
     // TODO: Support API user data fetching 
@@ -138,3 +170,117 @@ export default React.forwardRef((props, ref) => {
         }
     </div>
 })
+
+export const MobileUserCard = React.forwardRef((props, ref) => {
+    return <div
+        ref={ref}
+        className={classnames(
+            "_mobile_userCard",
+            {
+                ["no-cover"]: !props.user.cover
+            }
+        )}
+    >
+        <div className="_mobile_userCard_top">
+            {
+                props.user.cover && <div className="_mobile_userCard_top_cover">
+                    <div
+                        className="cover"
+                        style={{
+                            backgroundImage: `url("${props.user.cover}")`
+                        }}
+                    />
+
+                    <div className="_mobile_userCard_top_avatar_wrapper">
+                        <div className="_mobile_userCard_top_avatar">
+                            <Image
+                                src={props.user.avatar}
+                            />
+                        </div>
+                    </div>
+                </div>
+            }
+
+            {
+                !props.user.cover && <div className="_mobile_userCard_top_avatar">
+                    <Image
+                        src={props.user.avatar}
+                    />
+                </div>
+            }
+
+            <div className="_mobile_userCard_top_texts">
+                <div className="_mobile_userCard_top_username">
+                    <h1>
+                        {
+                            props.user.fullName ?? `@${props.user.username}`
+                        }
+                        {
+                            props.user.verified && <Icons.verifiedBadge />
+                        }
+                    </h1>
+
+                    {
+                        props.user.fullName && <span>
+                            @{props.user.username}
+                        </span>
+                    }
+                </div>
+
+                <div className="_mobile_userCard_top_description">
+                    <p>
+                        {
+                            props.user.description
+                        }
+                    </p>
+                </div>
+            </div>
+
+            {
+                props.user.links
+                && Array.isArray(props.user.links)
+                && props.user.links.length > 0
+                && <div
+                    className={classnames(
+                        "_mobile_userCard_links",
+                    )}
+                >
+                    {
+                        props.user.links.map((link, index) => {
+                            return <UserLink index={index} link={link} />
+                        })
+                    }
+                </div>
+            }
+        </div>
+
+        <div
+            className={classnames(
+                "_mobile_card",
+                "_mobile_userCard_actions",
+            )}
+        >
+            {
+                props.followers && <FollowButton
+                    count={props.followers.length}
+                    onClick={props.onClickFollow}
+                    followed={props.isFollowed}
+                    self={props.isSelf}
+                />
+            }
+
+            <antd.Button
+                type="primary"
+                icon={<Icons.MdMessage />}
+                disabled
+            />
+
+            <antd.Button
+                type="primary"
+                icon={<Icons.MdShare />}
+            />
+        </div>
+    </div>
+})
+
+export default UserCard
