dea18404390a6b39ff8598d8bcea7e33fe6810ce
SrGooglo <srgooglo@ragestudio.net>
2023-08-15 18:44:39 +0000

    increase base transcoding quality

--- packages/file_server/src/services/post-process/video/index.js
+++ packages/file_server/src/services/post-process/video/index.js
@@ -9,13 +9,13 @@ import videoTranscode from "@services/videoTranscode"
  * @param {string} [options.videoCodec="libx264"] - The video codec to use.
  * @param {string} [options.format="mp4"] - The format to use.
  * @param {number} [options.audioBitrate=128] - The audio bitrate to use.
- * @param {number} [options.videoBitrate=1024] - The video bitrate to use.
+ * @param {number} [options.videoBitrate=2024] - The video bitrate to use.
  * @throws {Error} Throws an error if file parameter is not provided.
  * @return {Object} The processed video file object.
  */
 async function processVideo(
     file,
-    options = {}
+    options = {},
 ) {
     if (!file) {
         throw new Error("file is required")
@@ -26,14 +26,14 @@ async function processVideo(
         videoCodec = "libx264",
         format = "mp4",
         audioBitrate = 128,
-        videoBitrate = 1024,
+        videoBitrate = 2024,
     } = options
 
     const result = await videoTranscode(file.filepath, file.cachePath, {
         videoCodec,
         format,
         audioBitrate,
-        videoBitrate,
+        videoBitrate: [videoBitrate, true],
     })
 
     file.filepath = result.filepath
