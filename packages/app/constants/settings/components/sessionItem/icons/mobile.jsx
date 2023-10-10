eec2f4b1f7879bd5f9079c7273672dedd4ea5f0b
SrGooglo <srgooglo@ragestudio.net>
2023-10-10 12:16:01 +0000

    move `sessions` component to security

---
+++ packages/app/constants/settings/components/sessionItem/icons/mobile.jsx
@@ -0,0 +1,34 @@
+export default () => {
+    return <svg
+    viewBox="0 0 308.1161 511.4894"
+>
+    <rect
+        x="10"
+        y="10"
+        width="288.1161"
+        height="491.4894"
+        rx="34"
+        ry="34"
+        style={{
+            fill: "none",
+            stroke: "currentColor",
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeWidth: "2rem",
+        }}
+    />
+    <line
+        x1="85.8071"
+        y1="449.5339"
+        x2="222.3091"
+        y2="449.5339"
+        style={{
+            fill: "none",
+            stroke: "currentColor",
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeWidth: "2rem",
+        }}
+    />
+</svg>
+}
