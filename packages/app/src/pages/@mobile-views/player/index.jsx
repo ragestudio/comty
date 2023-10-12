091c558991102bfc9133747a4bc4abf00dbf1bfa
SrGooglo <srgooglo@ragestudio.net>
2023-10-12 19:57:18 +0000

    use new audio player for mobile

--- packages/app/src/pages/@mobile-views/player/index.jsx
+++ packages/app/src/pages/@mobile-views/player/index.jsx
@@ -1,12 +1,131 @@
 import React from "react"
-import MediaPlayer from "components/Player/MediaPlayer"
+import { Button } from "antd"
+import { Icons } from "components/Icons"
+import classnames from "classnames"
+import RGBStringToValues from "utils/rgbToValues"
+
+import SeekBar from "components/Player/SeekBar"
+import Controls from "components/Player/Controls"
+import { WithPlayerContext, Context } from "contexts/WithPlayerContext"
 
 import "./index.less"
 
-export default () => {
-    return <div className="__mobile-player-view">
-        <MediaPlayer
-            frame={false}
+const ServiceIndicator = (props) => {
+    if (!props.service) {
+        return null
+    }
+
+    switch (props.service) {
+        case "tidal": {
+            return <div className="service_indicator">
+                <Icons.SiTidal /> Playing from Tidal
+            </div>
+        }
+        default: {
+            return null
+        }
+    }
+}
+
+const AudioPlayer = (props) => {
+    return <WithPlayerContext>
+        <AudioPlayerComponent
+            {...props}
         />
+    </WithPlayerContext>
+}
+
+const AudioPlayerComponent = (props) => {
+    const ctx = React.useContext(Context)
+
+    React.useEffect(() => {
+        if (app.currentDragger) {
+            app.currentDragger.setBackgroundColorValues(RGBStringToValues(ctx.track_manifest?.cover_analysis?.rgb))
+        }
+
+    }, [ctx.track_manifest?.cover_analysis])
+
+    const {
+        title,
+        album,
+        artist,
+        service,
+        lyricsEnabled,
+        cover_analysis,
+        cover,
+    } = ctx.track_manifest ?? {}
+
+    const playing = ctx.playback_status === "playing"
+    const stopped = ctx.playback_status === "stopped"
+
+    const titleText = (!playing && stopped) ? "Stopped" : (title ?? "Untitled")
+    const subtitleText = `${artist} | ${album?.title ?? album}`
+
+    return <div
+        className={classnames(
+            "mobile_media_player_wrapper",
+            {
+                "cover_light": cover_analysis?.isLight,
+            }
+        )}
+        style={{
+            "--cover_isLight": cover_analysis?.isLight,
+        }}
+    >
+        <div className="mobile_media_player">
+            <ServiceIndicator
+                service={service}
+            />
+
+            <div
+                className="cover"
+                style={{
+                    backgroundImage: `url(${cover ?? "/assets/no_song.png"})`,
+                }}
+            />
+
+            <div className="header">
+                <div className="info">
+                    <div className="title">
+                        <h2>
+                            {
+                                titleText
+                            }
+                        </h2>
+                    </div>
+                    <div className="subTitle">
+                        <div className="artist">
+                            <h3>
+                                {subtitleText}
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <Controls />
+
+            <SeekBar
+                stopped={ctx.playback_status === "stopped"}
+                playing={ctx.playback_status === "playing"}
+                streamMode={ctx.livestream_mode}
+                disabled={ctx.control_locked}
+            />
+
+            <div className="extra_actions">
+                <Button
+                    type="ghost"
+                    icon={<Icons.MdLyrics />}
+                    disabled={!lyricsEnabled}
+                />
+
+                <Button
+                    type="ghost"
+                    icon={<Icons.MdQueueMusic />}
+                />
+            </div>
+        </div>
     </div>
 }
+
+export default AudioPlayer
