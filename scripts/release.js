2eb3eb013f2872b7b7ecd7a97b9374df01c15478
SrGooglo <srgooglo@ragestudio.net>
2024-02-19 19:10:45 +0000

    update release script

--- scripts/release.js
+++ scripts/release.js
@@ -149,7 +149,7 @@ async function main() {
         cwd: process.cwd()
     }).toString().trim()
 
-    if (gitStatus.length > 0 && !process.argv.includes("--force")) {
+    if (gitStatus.length > 0 && !process.argv.includes("--ignore-commits")) {
         console.error("🆘 There are pending changes to commit, please commit first.")
         return false
     }
