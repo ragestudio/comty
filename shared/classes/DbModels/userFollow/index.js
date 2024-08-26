5733503bc076fd4c2afe2d4b5ba0ef6c93f523c3
SrGooglo <srgooglo@ragestudio.net>
2024-08-26 13:51:03 +0000

    remove shared

--- shared/classes/DbModels/userFollow/index.js
+++
@@ -1,8 +0,0 @@
-export default {
-    name: "UserFollow",
-    collection: "follows",
-    schema: {
-        user_id: { type: String, required: true },
-        to: { type: String, required: true },
-    }
-}
