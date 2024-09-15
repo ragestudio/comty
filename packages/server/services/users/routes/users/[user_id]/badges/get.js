6d491ec696890a9b31cfaf88537278d333463213
SrGooglo <srgooglo@ragestudio.net>
2024-09-15 03:34:09 +0000

    implement user badges fetch

---
+++ packages/server/services/users/routes/users/[user_id]/badges/get.js
@@ -0,0 +1,21 @@
+import { User, Badge } from "@db_models"
+
+export default {
+    fn: async (req) => {
+        const { user_id } = req.params
+
+        const user = await User.findOne({
+            _id: user_id
+        }).catch((err) => {
+            return false
+        })
+
+        const badges = await Badge.find({
+            name: {
+                $in: user.badges
+            }
+        })
+
+        return badges
+    }
+}
