bf58669117c519b02196db2deba990e513592001
SrGooglo <srgooglo@ragestudio.net>
2024-09-15 17:26:52 +0000

    use search queries

--- packages/app/src/hooks/useUrlQueryActiveKey/index.js
+++ packages/app/src/hooks/useUrlQueryActiveKey/index.js
@@ -4,15 +4,15 @@ export default ({
     defaultKey = "0",
     queryKey = "key",
 }) => {
-    const [activeKey, setActiveKey] = React.useState(new URLSearchParams(window.location.hash.replace("#", "?")).get(queryKey) ?? defaultKey)
+    const [activeKey, setActiveKey] = React.useState(new URLSearchParams(window.location.search).get(queryKey) ?? defaultKey)
 
     const replaceQueryTypeToCurrentTab = (key) => {
         if (!key) {
             // delete query
-            return document.location.hash = null
+            return history.pushState(undefined, "", window.location.pathname)
         }
 
-        return document.location.hash = `${queryKey}=${key}`
+        return history.pushState(undefined, "", `?${queryKey}=${key}`)
     }
 
     const changeActiveKey = (key) => {
