362560440a3ee2b3ec656b447db0de86c84c4018
SrGooglo <srgooglo@ragestudio.net>
2024-09-15 16:18:47 +0000

    move `home` to `timeline`

--- packages/app/src/pages/index.jsx
+++ packages/app/src/pages/index.jsx
@@ -1,33 +1,5 @@
-import React from "react"
-import { Translation } from "react-i18next"
-
-import { PagePanelWithNavMenu } from "@components/PagePanels"
-
-import Tabs from "./home/tabs"
-
-export default class Home extends React.Component {
-    render() {
-        return <PagePanelWithNavMenu
-            tabs={Tabs}
-            extraItems={[
-                {
-                    key: "create",
-                    icon: "PlusCircle",
-                    label: <Translation>{(t) => t("Create")}</Translation>,
-                    props: {
-                        type: "primary",
-                        onClick: app.controls.openPostCreator
-                    }
-                },
-            ]}
-            onTabChange={() => {
-                app.layout.scrollTo({
-                    top: 0,
-                })
-            }}
-            useSetQueryType
-            transition
-            masked
-        />
-    }
+const HomePage = () => {
+    return app.location.push("/timeline")
 }
+
+export default HomePage
--- packages/app/src/pages/timeline/index.mobile.jsx
+++ packages/app/src/pages/timeline/index.mobile.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 import { PagePanelWithNavMenu } from "@components/PagePanels"
 
-import Tabs from "./home/tabs"
+import Tabs from "./tabs"
 
 export default class Home extends React.Component {
     componentDidMount() {
