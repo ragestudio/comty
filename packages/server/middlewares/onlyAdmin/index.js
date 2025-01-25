4b062d00e550cad1286999734902bfb9572bc1db
SrGooglo <srgooglo@ragestudio.net>
2025-01-25 19:43:28 +0000

    fix `onlyAdmin` middleware

--- packages/server/middlewares/onlyAdmin/index.js
+++ packages/server/middlewares/onlyAdmin/index.js
@@ -1,7 +1,9 @@
-export default (req, res, next) => {
+export default async (req, res, next) => {
     if (!req.auth) {
         return res.status(401).json({ error: "No authenticated" })
     }
+
+    req.auth.user = await req.auth.user()
 
     if (!req.auth.user.roles.includes("admin")) {
         return res.status(403).json({ error: "To make this request it is necessary to have administrator permissions" })
