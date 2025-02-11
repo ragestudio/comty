6c6cb2b609303a8bdb685639479ee4ea4e8416bf
SrGooglo <srgooglo@ragestudio.net>
2025-02-11 16:23:24 +0000

    fix argus

--- scripts/utils/uploadAssets.js
+++ scripts/utils/uploadAssets.js
@@ -1,23 +1,23 @@
-async function uploadAssets(octokit, repo, release, assets, { release, bundlePath, changelogFilepath }) {
-    console.log("⚒  Uploading assets...")
+async function uploadAssets(octokit, repo, release, assets) {
+	console.log("⚒  Uploading assets...")
 
-    console.log(`ReleaseID: ${release.id}`)
+	console.log(`ReleaseID: ${release.id}`)
 
-    for await (const asset of assets) {
-        console.log(`Uploading Asset: ${asset.name}`)
+	for await (const asset of assets) {
+		console.log(`Uploading Asset: ${asset.name}`)
 
-        await octokit.repos.uploadReleaseAsset({
-            release_id: release.id,
-            owner: repo.split("/")[0],
-            repo: repo.split("/")[1],
-            name: asset.name,
-            data: asset.data
-        })
-    }
+		await octokit.repos.uploadReleaseAsset({
+			release_id: release.id,
+			owner: repo.split("/")[0],
+			repo: repo.split("/")[1],
+			name: asset.name,
+			data: asset.data,
+		})
+	}
 
-    console.log("⚒  Uploading assets done!")
+	console.log("⚒  Uploading assets done!")
 
-    return true
+	return true
 }
 
 module.exports = uploadAssets
