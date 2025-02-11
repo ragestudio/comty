8b98a60f82dd612eaf74778e2d58b1a2777fe468
SrGooglo <srgooglo@ragestudio.net>
2025-02-11 16:13:52 +0000

    formaat

--- packages/app/src/pages/studio/index.jsx
+++ packages/app/src/pages/studio/index.jsx
@@ -7,49 +7,45 @@ import useClickNavById from "@hooks/useClickNavById"
 import "./index.less"
 
 const SelectorPaths = {
-    "music": "/studio/music",
-    "tv": "/studio/tv",
-    "marketplace": "/studio/marketplace",
+	music: "/studio/music",
+	tv: "/studio/tv",
+	marketplace: "/studio/marketplace",
 }
 
 const StudioPage = () => {
-    const [navigatorRef, navigatorProps] = useClickNavById(SelectorPaths, ".studio-page-selectors-item")
+	const [navigatorRef, navigatorProps] = useClickNavById(
+		SelectorPaths,
+		".studio-page-selectors-item",
+	)
 
-    return <div className="studio-page">
-        <div className="studio-page-header">
-        <h1>Studio</h1>
-        </div>
+	return (
+		<div className="studio-page">
+			<div className="studio-page-header">
+				<h1>Studio</h1>
+			</div>
 
-        <div
-            className="studio-page-selectors"
-            ref={navigatorRef}
-            {...navigatorProps}
-        >
-            <div
-                id="music"
-                className="studio-page-selectors-item"
-            >
-                <Icons.MdLibraryMusic />
-                <span>Music</span>
-            </div>
+			<div
+				className="studio-page-selectors"
+				ref={navigatorRef}
+				{...navigatorProps}
+			>
+				<div id="music" className="studio-page-selectors-item">
+					<Icons.MdLibraryMusic />
+					<span>Music</span>
+				</div>
 
-            <div
-                id="tv"
-                className="studio-page-selectors-item"
-            >
-                <Icons.MdTv />
-                <span>TV</span>
-            </div>
+				<div id="tv" className="studio-page-selectors-item">
+					<Icons.MdTv />
+					<span>TV</span>
+				</div>
 
-            <div
-                id="marketplace"
-                className="studio-page-selectors-item"
-            >
-                <Icons.MdCode />
-                <span>Marketplace</span>
-            </div>
-        </div>
-    </div>
+				<div id="marketplace" className="studio-page-selectors-item">
+					<Icons.MdCode />
+					<span>Marketplace</span>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default StudioPage
