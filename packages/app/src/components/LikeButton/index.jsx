37ab0184c299d0e919299c594a62e59c5d854267
SrGooglo <srgooglo@ragestudio.net>
2025-02-18 04:22:43 +0000

    support for disable element

--- packages/app/src/components/LikeButton/index.jsx
+++ packages/app/src/components/LikeButton/index.jsx
@@ -4,68 +4,73 @@ import classnames from "classnames"
 import "./index.less"
 
 export default (props) => {
-    const [liked, setLiked] = React.useState(props.liked)
-    const [clicked, setClicked] = React.useState(false)
+	const [liked, setLiked] = React.useState(
+		typeof props.liked === "function" ? false : props.liked,
+	)
+	const [clicked, setClicked] = React.useState(false)
 
-    // TODO: Support handle like change on websocket event
-    if (typeof props.watchWs === "object") {
-        // useWsEvents({
-        //     [props.watchWs.event]: (data) => {
-        //         handleUpdateTrackLike(data.track_id, data.action === "liked")
-        //     }
-        // }, {
-        //     socketName: props.watchWs.socket,
-        // })
-    }
+	// TODO: Support handle like change on websocket event
+	if (typeof props.watchWs === "object") {
+		// useWsEvents({
+		//     [props.watchWs.event]: (data) => {
+		//         handleUpdateTrackLike(data.track_id, data.action === "liked")
+		//     }
+		// }, {
+		//     socketName: props.watchWs.socket,
+		// })
+	}
 
-    async function computeLikedState() {
-        if (typeof props.liked === "function") {
-            let result = await props.liked()
+	async function computeLikedState() {
+		if (props.disabled) {
+			return false
+		}
 
-            result = result.liked ?? result
+		if (typeof props.liked === "function") {
+			let result = await props.liked()
 
-            return setLiked(result)
-        }
+			result = result.liked ?? result
 
-        return setLiked(props.liked)
-    }
+			return setLiked(result)
+		}
 
-    const handleClick = () => {
-        setClicked(true)
+		return setLiked(props.liked)
+	}
 
-        setTimeout(() => {
-            setClicked(false)
-        }, 500)
+	const handleClick = () => {
+		if (props.disabled) {
+			return false
+		}
 
-        if (typeof props.onClick === "function") {
-            props.onClick()
-        }
+		setClicked(true)
 
-        setLiked(!liked)
-    }
+		setTimeout(() => {
+			setClicked(false)
+		}, 500)
 
-    React.useEffect(() => {
-        computeLikedState()
-    }, [props.liked])
+		if (typeof props.onClick === "function") {
+			props.onClick()
+		}
 
-    return <button
-        className={classnames(
-            "likeButton",
-            {
-                ["liked"]: liked,
-                ["clicked"]: clicked,
-            }
-        )}
-        onClick={handleClick}
-    >
-        {
-            !!props.ripple && <div className="ripple" />
-        }
-        <svg
-            className="heart"
-            viewBox="0 0 24 24"
-        >
-            <path d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3 10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27 18.6,15.36 13.45,20.03L12,21.35Z"></path>
-        </svg>
-    </button>
+		setLiked(!liked)
+	}
+
+	React.useEffect(() => {
+		computeLikedState()
+	}, [props.liked])
+
+	return (
+		<button
+			className={classnames("likeButton", {
+				["liked"]: liked,
+				["clicked"]: clicked,
+				["disabled"]: props.disabled,
+			})}
+			onClick={handleClick}
+		>
+			{!!props.ripple && <div className="ripple" />}
+			<svg className="heart" viewBox="0 0 24 24">
+				<path d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3 10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27 18.6,15.36 13.45,20.03L12,21.35Z"></path>
+			</svg>
+		</button>
+	)
 }
