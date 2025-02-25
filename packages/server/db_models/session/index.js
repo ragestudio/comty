e9dd362d7b4b449befb857b5c295deb5308566c8
SrGooglo <srgooglo@ragestudio.net>
2025-02-25 23:08:39 +0000

    format

--- packages/server/db_models/session/index.js
+++ packages/server/db_models/session/index.js
@@ -1,13 +1,14 @@
 export default {
-    name: "Session",
-    collection: "sessions",
-    schema: {
-        token: { type: String, required: true },
-        username: { type: String, required: true },
-        user_id: { type: String, required: true },
-        date: { type: Number, default: 0 },
-        sign_location: { type: String, default: "Unknown" },
-        ip_address: { type: String, default: "Unknown" },
-        client: { type: String, default: "Unknown" },
-    }
+	name: "Session",
+	collection: "sessions",
+	schema: {
+		token: { type: String, required: true },
+		username: { type: String, required: true },
+		user_id: { type: String, required: true },
+		date: { type: Number, default: 0 },
+		sign_location: { type: String, default: "Unknown" },
+		ip_address: { type: String, default: "Unknown" },
+		client: { type: String, default: "Unknown" },
+		created_at: { type: Date, default: Date.now },
+	},
 }
