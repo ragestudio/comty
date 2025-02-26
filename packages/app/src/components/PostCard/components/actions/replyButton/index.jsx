c0bb67153b3f0b2f0949b29625e2eb65d80a2dbf
SrGooglo <srgooglo@ragestudio.net>
2025-02-26 01:11:08 +0000

    improve reply button style

--- packages/app/src/components/PostCard/components/actions/replyButton/index.jsx
+++ packages/app/src/components/PostCard/components/actions/replyButton/index.jsx
@@ -1,21 +1,28 @@
 import React from "react"
 import { Button } from "antd"
-import { Icons } from "@components/Icons"
+import { LuReply } from "react-icons/lu"
 
 import "./index.less"
 
 export default (props) => {
-    return <div
-        className="reply_button"
-    >
-        <Button
-            type="ghost"
-            shape="circle"
-            onClick={props.onClick}
-            icon={<Icons.FiRepeat />}
-        />
-        {
-            props.count > 0 && <span className="replies_count">{props.count}</span>
-        }
-    </div>
+	return (
+		<div className="reply_button">
+			<Button
+				type="ghost"
+				shape="circle"
+				onClick={props.onClick}
+				icon={
+					<LuReply
+						style={{
+							fontSize: "1rem",
+							transform: "translateY(-1px)",
+						}}
+					/>
+				}
+			/>
+			{props.count > 0 && (
+				<span className="replies_count">{props.count}</span>
+			)}
+		</div>
+	)
 }
