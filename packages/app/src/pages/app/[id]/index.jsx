111f801b6a7297abd65ff94edf758e1bcaa382aa
SrGooglo <srgooglo@ragestudio.net>
2025-03-13 23:36:22 +0000

    added extension app page

---
+++ packages/app/src/pages/app/[id]/index.jsx
@@ -0,0 +1,77 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import Image from "@components/Image"
+import { Icons } from "@components/Icons"
+import ErrorBoundary from "@components/ErrorBoundary"
+
+import useCenteredContainer from "@hooks/useCenteredContainer"
+import useTotalWindowHeight from "@hooks/useTotalWindowHeight"
+
+import loadable from "@loadable/component"
+
+import "./index.less"
+
+const AppPage = (props) => {
+	const { id } = props.params
+
+	useCenteredContainer(false)
+	useTotalWindowHeight(true)
+
+	const [loading, setLoading] = React.useState(true)
+
+	const [customApp, setCustomApp] = React.useState({})
+	const [extensionRef, setExtensionRef] = React.useState({})
+	const [Render, setRender] = React.useState(() => () => null)
+
+	async function loadApp() {
+		setLoading(true)
+
+		const extension = app.extensions.extensions.get(id)
+
+		if (!extension) {
+			throw new Error(`Extension with id ${id} not found`)
+		}
+
+		if (typeof extension.main.app.onLoad === "function") {
+			await extension.main.app.onLoad()
+		}
+
+		setExtensionRef(extension)
+		setCustomApp(extension.main.app)
+		setRender(extension.main.app.renderComponent)
+
+		setLoading(false)
+	}
+
+	React.useEffect(() => {
+		loadApp()
+	}, [])
+
+	if (loading) {
+		return (
+			<div className="custom-app-page-loading">
+				<div className="custom-app-page-loading-icon">
+					{customApp.icon && (
+						<Image src={customApp.icon} alt={customApp.name} />
+					)}
+				</div>
+
+				<Icons.LoadingOutlined
+					spin
+					className="custom-app-page-loader"
+				/>
+			</div>
+		)
+	}
+
+	return (
+		<div className="custom-app-page-render">
+			<ErrorBoundary>
+				<Render />
+			</ErrorBoundary>
+		</div>
+	)
+}
+
+export default AppPage
