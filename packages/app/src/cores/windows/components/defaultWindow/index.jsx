1c5c213b96f460a6f32ca7078d24a8f4a2cd8da2
SrGooglo <srgooglo@ragestudio.net>
2025-03-13 23:39:01 +0000

    some improvements on component renders

--- packages/app/src/cores/windows/components/defaultWindow/index.jsx
+++ packages/app/src/cores/windows/components/defaultWindow/index.jsx
@@ -6,191 +6,194 @@ import { Icons } from "@components/Icons"
 import WindowContext from "./context"
 
 export default class DefaultWindowRender extends React.Component {
-    static contextType = WindowContext
+	static contextType = WindowContext
 
-    ref = React.createRef()
+	ref = React.createRef()
 
-    state = {
-        renderError: false,
-        title: null,
-        actions: [],
-        dimensions: {
-            height: this.props.height ?? 600,
-            width: this.props.width ?? 400,
-        },
-        position: this.props.defaultPosition,
-        visible: false,
-    }
+	state = {
+		renderError: false,
+		title: null,
+		actions: [],
+		dimensions: {
+			height: this.props.height ?? 600,
+			width: this.props.width ?? 400,
+		},
+		position: this.props.defaultPosition,
+		visible: false,
+	}
 
-    componentDidMount = () => {
-        this.setDefaultActions()
+	componentDidMount = () => {
+		this.setDefaultActions()
 
-        if (typeof this.props.actions !== "undefined") {
-            if (Array.isArray(this.props.actions)) {
-                const actions = this.state.actions ?? []
+		if (typeof this.props.actions !== "undefined") {
+			if (Array.isArray(this.props.actions)) {
+				const actions = this.state.actions ?? []
 
-                this.props.actions.forEach((action) => {
-                    actions.push(action)
-                })
+				this.props.actions.forEach((action) => {
+					actions.push(action)
+				})
 
-                this.setState({ actions })
-            }
-        }
+				this.setState({ actions })
+			}
+		}
 
-        if (!this.state.position) {
-            this.setState({ position: this.getCenterPosition() })
-        }
+		if (!this.state.position) {
+			this.setState({ position: this.getCenterPosition() })
+		}
 
-        this.toggleVisibility(true)
-    }
+		this.toggleVisibility(true)
+	}
 
-    componentDidCatch = (error) => {
-        console.error(error)
+	componentDidCatch = (error) => {
+		console.error(error)
 
-        this.setState({
-            renderError: error,
-        })
-    }
+		this.setState({
+			renderError: error,
+		})
+	}
 
-    updateTitle = (title) => {
-        this.setState({ title })
-    }
+	updateTitle = (title) => {
+		this.setState({ title })
+	}
 
-    updateDimensions = (dimensions = {
-        width: 0,
-        height: 0,
-    }) => {
-        this.ref.current?.updateSize({
-            width: dimensions.width,
-            height: dimensions.height,
-        })
-    }
+	updateDimensions = (
+		dimensions = {
+			width: 0,
+			height: 0,
+		},
+	) => {
+		this.ref.current?.updateSize({
+			width: dimensions.width,
+			height: dimensions.height,
+		})
+	}
 
-    updatePosition = (position = {
-        x: 0,
-        y: 0,
-    }) => {
-        this.ref.current?.updatePosition({
-            x: position.x,
-            y: position.y,
-        })
-    }
+	updatePosition = (
+		position = {
+			x: 0,
+			y: 0,
+		},
+	) => {
+		this.ref.current?.updatePosition({
+			x: position.x,
+			y: position.y,
+		})
+	}
 
-    toggleVisibility = (to) => {
-        this.setState({ visible: to ?? !this.state.visible })
-    }
+	toggleVisibility = (to) => {
+		this.setState({ visible: to ?? !this.state.visible })
+	}
 
-    getCenterPosition = () => {
-        const dimensions = this.state?.dimensions ?? {}
+	getCenterPosition = () => {
+		const dimensions = this.state?.dimensions ?? {}
 
-        const windowHeight = dimensions.height ?? 600
-        const windowWidth = dimensions.width ?? 400
+		const windowHeight = dimensions.height ?? 600
+		const windowWidth = dimensions.width ?? 400
 
-        return {
-            x: window.innerWidth / 2 - windowWidth / 2,
-            y: window.innerHeight / 2 - windowHeight / 2,
-        }
-    }
+		const y = window.innerHeight / 2 - windowHeight / 2
+		const x = window.innerWidth / 2 - windowWidth / 2
 
-    setDefaultActions = () => {
-        const { actions } = this.state
+		return {
+			x: x,
+			y: y,
+		}
+	}
 
-        actions.push({
-            key: "close",
-            render: () => <Icons.FiXCircle style={{ margin: 0, padding: 0 }} />,
-            onClick: () => {
-                this.props.close()
-            },
-        })
+	setDefaultActions = () => {
+		const { actions } = this.state
 
-        this.setState({ actions })
-    }
+		actions.push({
+			key: "close",
+			render: () => <Icons.FiXCircle style={{ margin: 0, padding: 0 }} />,
+			onClick: () => {
+				this.props.close()
+			},
+		})
 
-    renderActions = () => {
-        const actions = this.state.actions
+		this.setState({ actions })
+	}
 
-        if (Array.isArray(actions)) {
-            return actions.map((action) => {
-                return (
-                    <div key={action.key} onClick={action.onClick} {...action.props}>
-                        {React.isValidElement(action.render) ? action.render : React.createElement(action.render)}
-                    </div>
-                )
-            })
-        }
+	renderActions = () => {
+		const actions = this.state.actions
 
-        return null
-    }
+		if (Array.isArray(actions)) {
+			return actions.map((action) => {
+				return (
+					<div
+						key={action.key}
+						onClick={action.onClick}
+						{...action.props}
+					>
+						{React.isValidElement(action.render)
+							? action.render
+							: React.createElement(action.render)}
+					</div>
+				)
+			})
+		}
 
-    getComponentRender = () => {
-        const ctx = {
-            ...this.props,
-            updateTitle: this.updateTitle,
-            updateDimensions: this.updateDimensions,
-            updatePosition: this.updatePosition,
-            close: this.props.close,
-            position: this.state.position,
-            dimensions: this.state.dimensions,
-        }
+		return null
+	}
 
-        return <WindowContext.Provider value={ctx}>
-            {
-                React.isValidElement(this.props.children)
-                    ? React.cloneElement(this.props.children, ctx)
-                    : React.createElement(this.props.children, ctx)
-            }
-        </WindowContext.Provider>
-    }
+	getComponentRender = () => {
+		const ctx = {
+			...this.props,
+			updateTitle: this.updateTitle,
+			updateDimensions: this.updateDimensions,
+			updatePosition: this.updatePosition,
+			close: this.props.close,
+			position: this.state.position,
+			dimensions: this.state.dimensions,
+		}
 
-    render() {
-        const { position, dimensions, visible } = this.state
+		return (
+			<WindowContext.Provider value={ctx}>
+				{React.isValidElement(this.props.children)
+					? React.cloneElement(this.props.children, ctx)
+					: React.createElement(this.props.children, ctx)}
+			</WindowContext.Provider>
+		)
+	}
 
-        if (!visible) {
-            return null
-        }
+	render() {
+		const { position, dimensions, visible } = this.state
 
-        return <Rnd
-            ref={this.ref}
-            default={{
-                ...position,
-                ...dimensions,
-            }}
-            minWidth={
-                this.props.minWidth
-            }
-            minHeight={
-                this.props.minHeight
-            }
-            enableResizing={
-                this.props.enableResizing ?? true
-            }
-            disableDragging={
-                this.props.disableDragging ?? false
-            }
-            dragHandleClassName={
-                this.props.dragHandleClassName ?? "window_topbar"
-            }
-            bounds={
-                this.props.bounds ?? "#root"
-            }
-            className="window_wrapper"
-        >
-            <div className="window_topbar">
-                <div className="title">{this.state.title}</div>
-                <div className="actions">{this.renderActions()}</div>
-            </div>
+		if (!visible) {
+			return null
+		}
 
-            <div className="window_body">
-                {
-                    this.state.renderError && <div className="render_error">
-                        <h1>Render Error</h1>
-                        <code>{this.state.renderError.message}</code>
-                    </div>
-                }
-                {
-                    !this.state.renderError && this.getComponentRender()
-                }
-            </div>
-        </Rnd>
-    }
+		return (
+			<Rnd
+				ref={this.ref}
+				default={{
+					...position,
+					...dimensions,
+				}}
+				minWidth={this.props.minWidth}
+				minHeight={this.props.minHeight}
+				enableResizing={this.props.enableResizing ?? true}
+				disableDragging={this.props.disableDragging ?? false}
+				dragHandleClassName={
+					this.props.dragHandleClassName ?? "window_topbar"
+				}
+				//bounds={this.props.bounds ?? "#root"}
+				className="window_wrapper"
+			>
+				<div className="window_topbar">
+					<div className="title">{this.state.title}</div>
+					<div className="actions">{this.renderActions()}</div>
+				</div>
+
+				<div className="window_body">
+					{this.state.renderError && (
+						<div className="render_error">
+							<h1>Render Error</h1>
+							<code>{this.state.renderError.message}</code>
+						</div>
+					)}
+					{!this.state.renderError && this.getComponentRender()}
+				</div>
+			</Rnd>
+		)
+	}
 }
