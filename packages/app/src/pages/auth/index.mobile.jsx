0c04269cb3e08c5ee85f108a638fe7d5d6a7a6f3
SrGooglo <srgooglo@ragestudio.net>
2025-03-13 23:36:31 +0000

    fix for mobile

--- packages/app/src/pages/auth/index.mobile.jsx
+++ packages/app/src/pages/auth/index.mobile.jsx
@@ -5,28 +5,28 @@ import useRandomFeaturedWallpaperUrl from "@hooks/useRandomFeaturedWallpaperUrl"
 import "./index.mobile.less"
 
 export default (props) => {
-    const randomWallpaperURL = useRandomFeaturedWallpaperUrl()
+	const randomWallpaperURL = useRandomFeaturedWallpaperUrl()
 
-    React.useEffect(() => {
-        if (app.userData) {
-            app.navigation.goMain()
-        } else {
-            app.controls.openLoginForm({
-                defaultLocked: true,
-            })
-        }
-    }, [])
+	React.useEffect(() => {
+		if (app.userData) {
+			app.navigation.goMain()
+		} else {
+			app.auth.login()
+		}
+	}, [])
 
-    return <div className="loginPage">
-        <div
-            style={{
-                backgroundImage: `url(${randomWallpaperURL})`,
-            }}
-            className="wallpaper"
-        >
-            {/* <p>
+	return (
+		<div className="loginPage">
+			<div
+				style={{
+					backgroundImage: `url(${randomWallpaperURL})`,
+				}}
+				className="wallpaper"
+			>
+				{/* <p>
                 {wallpaperData?.author ? wallpaperData.author : null}
             </p> */}
-        </div>
-    </div>
+			</div>
+		</div>
+	)
 }
