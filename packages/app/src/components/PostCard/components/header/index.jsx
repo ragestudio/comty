16952789ecfe6ebdff98f4a6f935637572cbf3c9
SrGooglo <srgooglo@ragestudio.net>
2025-03-21 18:38:44 +0000

    display private post indicator

--- packages/app/src/components/PostCard/components/header/index.jsx
+++ packages/app/src/components/PostCard/components/header/index.jsx
@@ -11,64 +11,66 @@ import PostReplieView from "@components/PostReplieView"
 import "./index.less"
 
 const PostCardHeader = (props) => {
-    const goToProfile = () => {
-        app.navigation.goToAccount(props.postData.user?.username)
-    }
+	const goToProfile = () => {
+		app.navigation.goToAccount(props.postData.user?.username)
+	}
 
-    return <div className="post-header" onDoubleClick={props.onDoubleClick}>
-        {
-            !props.disableReplyTag && props.postData.reply_to && <div
-                className="post-header-replied_to"
-            >
-                <div className="post-header-replied_to-label">
-                    <Icons.FiRepeat />
+	return (
+		<div className="post-header" onDoubleClick={props.onDoubleClick}>
+			{!props.disableReplyTag && props.postData.reply_to && (
+				<div className="post-header-replied_to">
+					<div className="post-header-replied_to-label">
+						<Icons.FiRepeat />
 
-                    <span>
-                        Replied to
-                    </span>
-                </div>
+						<span>Replied to</span>
+					</div>
 
-                <PostReplieView
-                    data={props.postData.reply_to_data}
-                />
-            </div>
-        }
+					<PostReplieView data={props.postData.reply_to_data} />
+				</div>
+			)}
 
-        <div className="post-header-user">
-            <div className="post-header-user-avatar">
-                <Image
-                    alt="Avatar"
-                    src={props.postData.user?.avatar}
-                />
-            </div>
+			<div className="post-header-user">
+				<div className="post-header-user-avatar">
+					<Image alt="Avatar" src={props.postData.user?.avatar} />
+				</div>
 
-            <div className="post-header-user-info">
-                <h1 onClick={goToProfile}>
-                    {
-                        props.postData.user?.public_name ?? `@${props.postData.user?.username}`
-                    }
+				<div className="post-header-user-info">
+					<div className="post-header-user-info-label">
+						<div className="post-header-user-info-label-left">
+							<h1 onClick={goToProfile}>
+								{props.postData.user?.public_name ??
+									`@${props.postData.user?.username}`}
 
-                    {
-                        props.postData.user?.verified && <Icons.verifiedBadge />
-                    }
+								{props.postData.user?.verified && (
+									<Icons.verifiedBadge id="verified-badge" />
+								)}
+							</h1>
+						</div>
 
-                    {
-                        props.postData.flags?.includes("nsfw") && <Tag
-                            color="volcano"
-                        >
-                            NSFW
-                        </Tag>
-                    }
-                </h1>
+						<div className="post-header-user-info-label-right">
+							{props.postData.visibility === "private" && (
+								<span>
+									<Icons.FiEyeOff /> Private
+								</span>
+							)}
+							{props.postData.flags?.includes("nsfw") && (
+								<Tag color="volcano">NSFW</Tag>
+							)}
+						</div>
+					</div>
 
-                <span className="post-header-user-info-timeago">
-                    <TimeAgo
-                        time={props.postData.timestamp ?? props.postData.created_at}
-                    />
-                </span>
-            </div>
-        </div>
-    </div>
+					<span className="post-header-user-info-timeago">
+						<TimeAgo
+							time={
+								props.postData.timestamp ??
+								props.postData.created_at
+							}
+						/>
+					</span>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default PostCardHeader
