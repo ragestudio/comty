31e27f83aa0f6e07754ace74ef7e36a4679d49e9
SrGooglo <srgooglo@ragestudio.net>
2025-03-21 18:20:08 +0000

    added injectedAuth lib

---
+++ packages/server/lib/injectedAuth/index.js
@@ -0,0 +1,29 @@
+import AuthToken from "../../classes/AuthToken"
+import { User } from "../../db_models"
+
+export default async (obj, token) => {
+	const validation = await AuthToken.validate(token)
+
+	if (!validation.valid) {
+		if (validation.error) {
+			throw new Error(`Server error`)
+		}
+
+		throw new Error(`Invalid token`)
+	}
+
+	let userData = await User.findById(validation.data.user_id).lean()
+
+	if (!userData) {
+		throw new Error(`User not found`)
+	}
+
+	userData._id = userData._id.toString()
+
+	obj.user = userData
+	obj.token = token
+	obj.session = validation.data
+	obj.user = userData
+
+	return obj
+}
