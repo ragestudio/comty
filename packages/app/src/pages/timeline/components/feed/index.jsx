8a20633531ee049c4848c27d30334aa62b45b87c
SrGooglo <srgooglo@ragestudio.net>
2025-03-25 23:03:45 +0000

    use realtime mode

--- packages/app/src/pages/timeline/components/feed/index.jsx
+++ packages/app/src/pages/timeline/components/feed/index.jsx
@@ -7,33 +7,33 @@ import FeedModel from "@models/feed"
 import "./index.less"
 
 const emptyListRender = () => {
-    return <div className="emptyFeed">
-        <h2>
-            We don't have any posts to show you.
-        </h2>
+	return (
+		<div className="emptyFeed">
+			<h2>We don't have any posts to show you.</h2>
 
-        <p>
-            Search for new people to follow on <a onClick={() => app.location.push("/?type=global")}>global</a> tab, and start view their posts.
-        </p>
-    </div>
+			<p>
+				Search for new people to follow on{" "}
+				<a onClick={() => app.location.push("/?type=global")}>global</a>{" "}
+				tab, and start view their posts.
+			</p>
+		</div>
+	)
 }
 
 export class Feed extends React.Component {
-    render() {
-        return <PostsList
-            disableHasReplies
-            ref={this.props.innerRef}
-            emptyListRender={emptyListRender}
-            loadFromModel={FeedModel.getTimelineFeed}
-            watchTimeline={[
-                "feed.new",
-                "post.delete",
-            ]}
-            realtime
-        />
-    }
+	render() {
+		return (
+			<PostsList
+				disableHasReplies
+				ref={this.props.innerRef}
+				emptyListRender={emptyListRender}
+				loadFromModel={FeedModel.getTimelineFeed}
+				realtime
+			/>
+		)
+	}
 }
 
 export default React.forwardRef((props, ref) => {
-    return <Feed {...props} innerRef={ref} />
+	return <Feed {...props} innerRef={ref} />
 })
--- packages/app/src/pages/timeline/components/global/index.jsx
+++ packages/app/src/pages/timeline/components/global/index.jsx
@@ -7,16 +7,13 @@ import Feed from "@models/feed"
 import "./index.less"
 
 export default class ExplorePosts extends React.Component {
-    render() {
-        return <PostsList
-            disableHasReplies
-            loadFromModel={Feed.getGlobalTimelineFeed}
-            watchTimeline={[
-                "post.new",
-                "post.delete",
-                "feed.new",
-            ]}
-            realtime
-        />
-    }
+	render() {
+		return (
+			<PostsList
+				disableHasReplies
+				loadFromModel={Feed.getGlobalTimelineFeed}
+				realtime
+			/>
+		)
+	}
 }
