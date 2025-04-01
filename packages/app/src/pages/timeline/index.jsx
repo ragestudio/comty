f3809bf8a7bd7d48432db9a45431d1ad8d1c1eec
SrGooglo <srgooglo@ragestudio.net>
2025-04-01 21:50:47 +0000

    added featured event widget on timeline

--- packages/app/src/pages/timeline/index.jsx
+++ packages/app/src/pages/timeline/index.jsx
@@ -3,41 +3,47 @@ import { Translation } from "react-i18next"
 
 import { PagePanelWithNavMenu } from "@components/PagePanels"
 import TrendingsCard from "@components/TrendingsCard"
+import FeaturedEvents from "@components/FeaturedEventsAnnouncements"
 
 import usePageWidgets from "@hooks/usePageWidgets"
-
 import Tabs from "./tabs"
 
 const TimelinePage = () => {
-    usePageWidgets([
-        {
-            id: "trendings",
-            component: TrendingsCard
-        }
-    ])
+	usePageWidgets([
+		{
+			id: "trendings",
+			component: TrendingsCard,
+		},
+		{
+			id: "featured_events",
+			component: FeaturedEvents,
+		},
+	])
 
-    return <PagePanelWithNavMenu
-        tabs={Tabs}
-        extraItems={[
-            {
-                key: "create",
-                icon: "FiPlusCircle",
-                label: <Translation>{(t) => t("Create")}</Translation>,
-                props: {
-                    type: "primary",
-                    onClick: app.controls.openPostCreator
-                }
-            },
-        ]}
-        onTabChange={() => {
-            app.layout.scrollTo({
-                top: 0,
-            })
-        }}
-        useSetQueryType
-        transition
-        masked
-    />
+	return (
+		<PagePanelWithNavMenu
+			tabs={Tabs}
+			extraItems={[
+				{
+					key: "create",
+					icon: "FiPlusCircle",
+					label: <Translation>{(t) => t("Create")}</Translation>,
+					props: {
+						type: "primary",
+						onClick: app.controls.openPostCreator,
+					},
+				},
+			]}
+			onTabChange={() => {
+				app.layout.scrollTo({
+					top: 0,
+				})
+			}}
+			useSetQueryType
+			transition
+			masked
+		/>
+	)
 }
 
 export default TimelinePage
