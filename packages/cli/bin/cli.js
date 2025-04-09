92d41f40830bbbb76f75d4d297e26ffbd80e21eb
SrGooglo <srgooglo@ragestudio.net>
2025-04-09 15:45:02 +0000

    use bin instead index

---
+++ packages/cli/bin/cli.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+process.env.NODE_ENV = "production"
+
+import "../src/index.js"
--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 import fs from "node:fs"
 import path from "node:path"
 import { Command } from "commander"
