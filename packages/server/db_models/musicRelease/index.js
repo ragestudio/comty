71c201e35196e0507b4f85eff5f08cc9a9e4d5f0
SrGooglo <srgooglo@ragestudio.net>
2025-04-24 06:11:56 +0000

    update release db model

--- packages/server/db_models/musicRelease/index.js
+++ packages/server/db_models/musicRelease/index.js
@@ -1,35 +1,36 @@
 export default {
-    name: "MusicRelease",
-    collection: "music_releases",
-    schema: {
-        user_id: {
-            type: String,
-            required: true
-        },
-        title: {
-            type: String,
-            required: true
-        },
-        type: {
-            type: String,
-            required: true,
-        },
-        list: {
-            type: Object,
-            default: [],
-            required: true
-        },
-        cover: {
-            type: String,
-            default: "https://storage.ragestudio.net/comty-static-assets/default_song.png"
-        },
-        created_at: {
-            type: Date,
-            required: true
-        },
-        public: {
-            type: Boolean,
-            default: true,
-        },
-    }
+	name: "MusicRelease",
+	collection: "music_releases",
+	schema: {
+		user_id: {
+			type: String,
+			required: true,
+		},
+		title: {
+			type: String,
+			required: true,
+		},
+		type: {
+			type: String,
+			required: true,
+		},
+		items: {
+			type: Array,
+			default: [],
+			required: true,
+		},
+		cover: {
+			type: String,
+			default:
+				"https://storage.ragestudio.net/comty-static-assets/default_song.png",
+		},
+		created_at: {
+			type: Date,
+			required: true,
+		},
+		public: {
+			type: Boolean,
+			default: true,
+		},
+	},
 }
