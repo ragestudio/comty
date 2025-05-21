a92de80121bd43edba160298ab43b22c15a4af44
SrGooglo <srgooglo@ragestudio.net>
2025-05-21 18:56:06 +0000

    export ogg icon

--- packages/app/src/components/Icons/index.jsx
+++ packages/app/src/components/Icons/index.jsx
@@ -4,6 +4,7 @@ import VrChatIcon from "./customIcons/vrchat"
 import VerifiedBadge from "./customIcons/verifiedBadge"
 import Crown from "./customIcons/crown"
 import Lossless from "./customIcons/lossless"
+import Ogg from "./customIcons/ogg"
 
 // import icons lib
 import * as lib1 from "react-icons/fi"
@@ -15,52 +16,43 @@ import * as lib6 from "react-icons/fa"
 import * as lib7 from "react-icons/tb"
 
 const marginedStyle = {
-    width: "1em",
-    height: "1em",
-    marginRight: "10px",
-    verticalAlign: "-0.125em"
+	width: "1em",
+	height: "1em",
+	marginRight: "10px",
+	verticalAlign: "-0.125em",
 }
 
 const customs = {
-    Lossless: (props) => <Lossless
-        style={marginedStyle}
-        {...props}
-    />,
-    verifiedBadge: (props) => <VerifiedBadge
-        style={marginedStyle}
-        {...props}
-    />,
-    VrChat: (props) => <VrChatIcon
-        style={marginedStyle}
-        {...props}
-    />,
-    Crown: (props) => <Crown
-        style={marginedStyle}
-        {...props}
-    />
+	Lossless: (props) => <Lossless style={marginedStyle} {...props} />,
+	verifiedBadge: (props) => (
+		<VerifiedBadge style={marginedStyle} {...props} />
+	),
+	VrChat: (props) => <VrChatIcon style={marginedStyle} {...props} />,
+	Crown: (props) => <Crown style={marginedStyle} {...props} />,
+	Ogg: (props) => <Ogg style={marginedStyle} {...props} />,
 }
 
 export const Icons = {
-    ...customs,
-    ...lib1,
-    ...lib2,
-    ...lib3,
-    ...lib4,
-    ...lib5,
-    ...lib6,
-    ...lib7,
+	...customs,
+	...lib1,
+	...lib2,
+	...lib3,
+	...lib4,
+	...lib5,
+	...lib6,
+	...lib7,
 }
 
 export function createIconRender(icon, props) {
-    if (React.isValidElement(icon)) {
-        return icon
-    }
+	if (React.isValidElement(icon)) {
+		return icon
+	}
 
-    if (typeof Icons[icon] !== "undefined") {
-        return React.createElement(Icons[icon], props)
-    }
+	if (typeof Icons[icon] !== "undefined") {
+		return React.createElement(Icons[icon], props)
+	}
 
-    return null
+	return null
 }
 
 export default Icons
