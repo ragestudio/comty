d38d449c2882d27b9b38dd5ec79e077182142242
SrGooglo <srgooglo@ragestudio.net>
2025-05-21 19:07:22 +0000

    removed unused argument

--- packages/server/services/posts/routes/posts/[post_id]/data/get.js
+++ packages/server/services/posts/routes/posts/[post_id]/data/get.js
@@ -2,7 +2,7 @@ import Posts from "@classes/posts"
 
 export default {
 	useMiddlewares: ["withOptionalAuthentication"],
-	fn: async (req, res) => {
+	fn: async (req) => {
 		const result = await Posts.data({
 			post_id: req.params.post_id,
 			for_user_id: req.auth?.session?.user_id,
