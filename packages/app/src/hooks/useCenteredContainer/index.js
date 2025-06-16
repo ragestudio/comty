f711b71ae22f7fff8e84117e511892fb2cec09a9
SrGooglo <srgooglo@ragestudio.net>
2025-06-16 20:47:23 +0000

    Format

--- packages/app/src/hooks/useCenteredContainer/index.js
+++ packages/app/src/hooks/useCenteredContainer/index.js
@@ -1,19 +1,19 @@
 import React from "react"
 
 export default (to) => {
-    React.useEffect(() => {
-        if (typeof to !== "undefined") {
-            app.layout.toggleCenteredContent(to)
+	React.useEffect(() => {
+		if (typeof to !== "undefined") {
+			app.layout.toggleCenteredContent(to)
 
-            return () => {
-                app.layout.toggleCenteredContent(!!to)
-            }
-        }
+			return () => {
+				app.layout.toggleCenteredContent(!!to)
+			}
+		}
 
-        app.layout.toggleCenteredContent(true)
+		app.layout.toggleCenteredContent(true)
 
-        return () => {
-            app.layout.toggleCenteredContent(false)
-        }
-    }, [])
+		return () => {
+			app.layout.toggleCenteredContent(false)
+		}
+	}, [])
 }
