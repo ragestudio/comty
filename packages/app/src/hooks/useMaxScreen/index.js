d26454d974514fefa655b923bb1030cf1137e907
SrGooglo <srgooglo@ragestudio.net>
2025-06-16 20:48:44 +0000

    format

--- packages/app/src/hooks/useMaxScreen/index.js
+++ packages/app/src/hooks/useMaxScreen/index.js
@@ -1,25 +1,19 @@
 import React from "react"
 
 export default () => {
-    const enterPlayerAnimation = () => {
-        app.cores.style.applyTemporalVariant("dark")
-        app.layout.toggleCompactMode(true)
-        app.layout.toggleCenteredContent(false)
-        app.controls.toggleUIVisibility(false)
-    }
+	const enterPlayerAnimation = () => {
+		app.controls.toggleUIVisibility(false)
+	}
 
-    const exitPlayerAnimation = () => {
-        app.cores.style.applyVariant(app.cores.style.getStoragedVariantKey())
-        app.layout.toggleCompactMode(false)
-        app.layout.toggleCenteredContent(true)
-        app.controls.toggleUIVisibility(true)
-    }
+	const exitPlayerAnimation = () => {
+		app.controls.toggleUIVisibility(true)
+	}
 
-    React.useEffect(() => {
-        enterPlayerAnimation()
+	React.useEffect(() => {
+		enterPlayerAnimation()
 
-        return () => {
-            exitPlayerAnimation()
-        }
-    }, [])
+		return () => {
+			exitPlayerAnimation()
+		}
+	}, [])
 }
