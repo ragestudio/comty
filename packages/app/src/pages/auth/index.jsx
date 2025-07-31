c5809e1fdf9a40bf27ff7572af50638686da6c78
srgooglo <srgooglo@ragestudio.net>
2025-07-31 20:55:02 +0200

    Fix property

--- packages/app/src/pages/auth/index.jsx
+++ packages/app/src/pages/auth/index.jsx
@@ -11,27 +11,71 @@ import "./index.less"
 
 const GradientSVG = () => {
 	return (
-		<svg height="100%" width="100%">
+		<svg
+			height="100%"
+			width="100%"
+		>
 			<defs>
-				<linearGradient id="0" x1="0" y1="0.5" x2="1" y2="0.5">
-					<stop offset="0%" stop-color="rgba(225, 0, 209, 0.1)" />
-					<stop offset="25%" stop-color="rgba(233, 0, 182, 0.08)" />
-					<stop offset="50%" stop-color="rgba(240, 0, 154, 0.05)" />
-					<stop offset="100%" stop-color="rgba(255, 0, 0, 0)" />
+				<linearGradient
+					id="0"
+					x1="0"
+					y1="0.5"
+					x2="1"
+					y2="0.5"
+				>
+					<stop
+						offset="0%"
+						stopColor="rgba(225, 0, 209, 0.1)"
+					/>
+					<stop
+						offset="25%"
+						stopColor="rgba(233, 0, 182, 0.08)"
+					/>
+					<stop
+						offset="50%"
+						stopColor="rgba(240, 0, 154, 0.05)"
+					/>
+					<stop
+						offset="100%"
+						stopColor="rgba(255, 0, 0, 0)"
+					/>
 				</linearGradient>
 				<radialGradient
 					id="1"
 					gradientTransform="translate(-0.81 -0.5) scale(2, 1.2)"
 				>
-					<stop offset="0%" stop-color="rgba(255, 96, 100, 0.2)" />
-					<stop offset="20%" stop-color="rgba(255, 96, 100, 0.16)" />
-					<stop offset="40%" stop-color="rgba(255, 96, 100, 0.12)" />
-					<stop offset="60%" stop-color="rgba(255, 96, 100, 0.08)" />
-					<stop offset="100%" stop-color="rgba(255, 96, 100, 0)" />
+					<stop
+						offset="0%"
+						stopColor="rgba(255, 96, 100, 0.2)"
+					/>
+					<stop
+						offset="20%"
+						stopColor="rgba(255, 96, 100, 0.16)"
+					/>
+					<stop
+						offset="40%"
+						stopColor="rgba(255, 96, 100, 0.12)"
+					/>
+					<stop
+						offset="60%"
+						stopColor="rgba(255, 96, 100, 0.08)"
+					/>
+					<stop
+						offset="100%"
+						stopColor="rgba(255, 96, 100, 0)"
+					/>
 				</radialGradient>
 			</defs>
-			<rect fill="url(#0)" height="100%" width="100%" />
-			<rect fill="url(#1)" height="100%" width="100%" />
+			<rect
+				fill="url(#0)"
+				height="100%"
+				width="100%"
+			/>
+			<rect
+				fill="url(#1)"
+				height="100%"
+				width="100%"
+			/>
 		</svg>
 	)
 }
