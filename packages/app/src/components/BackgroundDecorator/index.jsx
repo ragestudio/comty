12a07f647c85e2e5eae9f0cf170b03683635325d
srgooglo <srgooglo@ragestudio.net>
2025-08-22 00:19:47 +0200

    Refactor style core and settings to use unified vars API
    - Replace getVar/mutateTheme with vars/modifyTheme - Update all settings
    and components to use new vars access - Simplify theme variant and style
    update logic - Improve LESS variable organization and add new style
    fixes

--- packages/app/src/components/BackgroundDecorator/index.jsx
+++ packages/app/src/components/BackgroundDecorator/index.jsx
@@ -2,47 +2,44 @@ import React from "react"
 import classnames from "classnames"
 
 export default () => {
-    const [activeColor, setActiveColor] = React.useState(false)
+	const [activeColor, setActiveColor] = React.useState(false)
 
-    const hasBackgroundSVG = (value) => {
-        if (value === "unset" || value === "none" || !value) {
-            return false
-        } else {
-            return true
-        }
-    }
+	const hasBackgroundSVG = (value) => {
+		if (value === "unset" || value === "none" || !value) {
+			return false
+		} else {
+			return true
+		}
+	}
 
-    const handleStyleUpdate = (update) => {
-        if (hasBackgroundSVG(update["backgroundSVG"])) {
-            setActiveColor(true)
-        } else {
-            setActiveColor(false)
-        }
-    }
+	const handleStyleUpdate = (update) => {
+		if (hasBackgroundSVG(update["backgroundSVG"])) {
+			setActiveColor(true)
+		} else {
+			setActiveColor(false)
+		}
+	}
 
-    React.useEffect(() => {
-        app.eventBus.on("style.update", handleStyleUpdate)
+	React.useEffect(() => {
+		app.eventBus.on("style.update", handleStyleUpdate)
 
-        const activeSVG = app.cores.style.getVar("backgroundSVG")
+		const activeSVG = app.cores.style.vars["backgroundSVG"]
 
-        if (hasBackgroundSVG(activeSVG)) {
-            setActiveColor(true)
-        }
+		if (hasBackgroundSVG(activeSVG)) {
+			setActiveColor(true)
+		}
 
-        return () => {
-            app.eventBus.off("style.update", handleStyleUpdate)
-        }
-    }, [])
+		return () => {
+			app.eventBus.off("style.update", handleStyleUpdate)
+		}
+	}, [])
 
-    return <div
-        id="root_background"
-        className={
-            classnames(
-                "root_background",
-                {
-                    ["active"]: activeColor
-                }
-            )
-        }
-    />
+	return (
+		<div
+			id="root_background"
+			className={classnames("root_background", {
+				["active"]: activeColor,
+			})}
+		/>
+	)
 }
--- packages/app/src/settings/components/themeVariantSelector/index.jsx
+++ packages/app/src/settings/components/themeVariantSelector/index.jsx
@@ -5,45 +5,49 @@ import { createIconRender } from "@components/Icons"
 import "./index.less"
 
 const variants = [
-    {
-        id: "light",
-        icon: "MdLightMode",
-    },
-    {
-        id: "dark",
-        icon: "MdDarkMode",
-    },
-    {
-        id: "auto",
-        icon: "MdAutoFixHigh",
-    },
+	{
+		id: "light",
+		icon: "MdLightMode",
+	},
+	{
+		id: "dark",
+		icon: "MdDarkMode",
+	},
+	{
+		id: "auto",
+		icon: "MdAutoFixHigh",
+	},
 ]
 
 const ThemeVariantSelector = (props) => {
-    const [selected, setSelected] = React.useState(app.cores.style.currentVariantKey)
+	const [selected, setSelected] = React.useState(app.cores.style.variantKey)
 
-    React.useEffect(() => {
-        app.cores.style.applyVariant(selected)
-    }, [selected])
+	React.useEffect(() => {
+		app.cores.style.updateVariant(selected)
+	}, [selected])
 
-    return <div className="__setting_theme_variant_selector">
-        {
-            variants.map((variant) => {
-                return <div
-                    className={classnames("__setting_theme_variant_selector-variant", {
-                        "selected": variant.id === selected
-                    })}
-                    onClick={() => {
-                        setSelected(variant.id)
-                    }}
-                >
-                    {
-                        createIconRender(variant.icon)
-                    }
-                </div>
-            })
-        }
-    </div>
+	return (
+		<div className="__setting_theme_variant_selector">
+			{variants.map((variant) => {
+				return (
+					<div
+						key={variant.id}
+						className={classnames(
+							"__setting_theme_variant_selector-variant",
+							{
+								selected: variant.id === selected,
+							},
+						)}
+						onClick={() => {
+							setSelected(variant.id)
+						}}
+					>
+						{createIconRender(variant.icon)}
+					</div>
+				)
+			})}
+		</div>
+	)
 }
 
 export default ThemeVariantSelector
