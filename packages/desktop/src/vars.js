95a22e895a21772d94ae295313935287abd899bb
srgooglo <srgooglo@ragestudio.net>
2025-08-27 23:20:09 +0200

    Use resources dir

--- packages/desktop/src/vars.js
+++ packages/desktop/src/vars.js
@@ -8,5 +8,5 @@ export default {
 	productionUrl: "https://comty.app",
 	indevUrl: "https://indev.comty.app",
 	developmentUrl: "http://localhost:8000",
-	iconPath: path.join(__dirname, "../assets/icon-512.png"),
+	iconPath: path.join(__dirname, "../resources/icon-512.png"),
 }
