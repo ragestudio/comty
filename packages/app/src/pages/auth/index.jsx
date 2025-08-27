94560948f1b94887d3860cc7d2621a02d5c6a081
srgooglo <srgooglo@ragestudio.net>
2025-08-27 20:33:45 +0200

    Added an animated background on auth page

--- packages/app/src/pages/auth/index.jsx
+++ packages/app/src/pages/auth/index.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Background from "./background"
 
 import useRandomFeaturedWallpaperUrl from "@hooks/useRandomFeaturedWallpaperUrl"
 import useUrlQueryActiveKey from "@hooks/useUrlQueryActiveKey"
@@ -9,84 +10,13 @@ import RecoveryForm from "./forms/recovery"
 
 import "./index.less"
 
-const GradientSVG = () => {
-	return (
-		<svg
-			height="100%"
-			width="100%"
-		>
-			<defs>
-				<linearGradient
-					id="0"
-					x1="0"
-					y1="0.5"
-					x2="1"
-					y2="0.5"
-				>
-					<stop
-						offset="0%"
-						stopColor="rgba(225, 0, 209, 0.1)"
-					/>
-					<stop
-						offset="25%"
-						stopColor="rgba(233, 0, 182, 0.08)"
-					/>
-					<stop
-						offset="50%"
-						stopColor="rgba(240, 0, 154, 0.05)"
-					/>
-					<stop
-						offset="100%"
-						stopColor="rgba(255, 0, 0, 0)"
-					/>
-				</linearGradient>
-				<radialGradient
-					id="1"
-					gradientTransform="translate(-0.81 -0.5) scale(2, 1.2)"
-				>
-					<stop
-						offset="0%"
-						stopColor="rgba(255, 96, 100, 0.2)"
-					/>
-					<stop
-						offset="20%"
-						stopColor="rgba(255, 96, 100, 0.16)"
-					/>
-					<stop
-						offset="40%"
-						stopColor="rgba(255, 96, 100, 0.12)"
-					/>
-					<stop
-						offset="60%"
-						stopColor="rgba(255, 96, 100, 0.08)"
-					/>
-					<stop
-						offset="100%"
-						stopColor="rgba(255, 96, 100, 0)"
-					/>
-				</radialGradient>
-			</defs>
-			<rect
-				fill="url(#0)"
-				height="100%"
-				width="100%"
-			/>
-			<rect
-				fill="url(#1)"
-				height="100%"
-				width="100%"
-			/>
-		</svg>
-	)
-}
-
 const keyToComponents = {
 	selector: MainSelector,
 	register: RegisterForm,
 	recovery: RecoveryForm,
 }
 
-const AuthPage = (props) => {
+const AuthPage = () => {
 	const [activeKey, setActiveKey] = useUrlQueryActiveKey({
 		defaultKey: "selector",
 	})
@@ -95,7 +25,10 @@ const AuthPage = (props) => {
 	return (
 		<div className="login-page">
 			<div className="background">
-				<GradientSVG />
+				<Background
+					color={[1, 0.5, 0.3]}
+					speed={0.1}
+				/>
 			</div>
 
 			<div className="login-page-card">
