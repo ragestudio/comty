9ed417affb71d0f3d9070daa05fec1439c713944
srgooglo <srgooglo@ragestudio.net>
2025-08-27 20:25:18 +0200

    Add endpoint to fetch and cache remote URL headers

---
+++ packages/server/services/main/routes/main/query_url/get.js
@@ -0,0 +1,36 @@
+export default {
+	useContexts: ["redis"],
+	fn: async (req, res, ctx) => {
+		const ioredis = ctx.redis.client
+		const url = req.query.url
+
+		const cacheKey = `headers:${Buffer.from(url).toString("base64")}`
+
+		const cachedHeaders = await ioredis.get(cacheKey)
+
+		if (cachedHeaders && !req.query.ignore_cache) {
+			res.header("Cache-Control", "public, max-age=3600")
+
+			return {
+				ok: true,
+				remoteHeaders: JSON.parse(cachedHeaders),
+				fromCache: true,
+			}
+		}
+
+		const remoteHeaders = await fetch(url, {
+			method: "HEAD",
+		}).then((res) => res.headers)
+
+		const headersObj = Object.fromEntries(remoteHeaders.entries())
+
+		await ioredis.setex(cacheKey, 3600, JSON.stringify(headersObj))
+
+		res.header("Cache-Control", "public, max-age=3600")
+
+		return {
+			remoteHeaders: headersObj,
+			fromCache: false,
+		}
+	},
+}
