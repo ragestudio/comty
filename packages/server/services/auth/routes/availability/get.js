779a6cc6d1fbac6dfa04b8f29d938e3d5ebfa2ca
srgooglo <srgooglo@ragestudio.net>
2025-09-17 02:25:01 +0200

    Simplify user availability check query logic

--- packages/server/services/auth/routes/availability/get.js
+++ packages/server/services/auth/routes/availability/get.js
@@ -1,33 +1,35 @@
 import { User } from "@db_models"
 
 export default async (req) => {
-    const { username, email } = req.query
+	const { username, email } = req.query
 
-    if (!username && !email) {
-        throw new OperationError(400, "Missing username or email")
-    }
+	if (!username && !email) {
+		throw new OperationError(400, "Missing username or email")
+	}
 
-    const user = await User
-        .findOne({
-            $or: [
-                { username: username },
-                { email: email },
-            ]
-        })
-        .select("+email")
-        .catch((error) => {
-            return false
-        })
+	let query = {
+		username: username,
+	}
 
-    if (user) {
-        return {
-            message: "User already exists",
-            exists: true,
-        }
-    } else {
-        return {
-            message: "User doesn't exists",
-            exists: false,
-        }
-    }
+	if (email) {
+		query = {
+			email: email,
+		}
+	}
+
+	const user = await User.findOne(query).catch((error) => {
+		return false
+	})
+
+	if (user) {
+		return {
+			message: "User already exists",
+			exists: true,
+		}
+	} else {
+		return {
+			message: "User doesn't exists",
+			exists: false,
+		}
+	}
 }
