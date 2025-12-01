e2ad6f7ee08ba7635c227d76e8ede9c1354da138
srgooglo <srgooglo@ragestudio.net>
2025-12-01 22:10:18 +0100

    Reimplement to support new session model

--- packages/server/classes/AuthToken/index.js
+++ packages/server/classes/AuthToken/index.js
@@ -1,11 +1,15 @@
 import jwt from "jsonwebtoken"
-import { Session, RefreshToken, User } from "@db_models"
+import { User } from "@db_models"
 
 export default class AuthToken {
+	static get SessionModel() {
+		return global.scylla.model("auth_session")
+	}
+
 	static get authStrategy() {
 		return {
 			secret: process.env.JWT_SECRET,
-			expiresIn: process.env.JWT_EXPIRES_IN ?? "24h",
+			expiresIn: process.env.JWT_EXPIRES_IN ?? "1h",
 			algorithm: process.env.JWT_ALGORITHM ?? "HS256",
 		}
 	}
@@ -16,64 +20,6 @@ export default class AuthToken {
 			expiresIn: process.env.JWT_REFRESH_EXPIRES_IN ?? "30d",
 			algorithm: process.env.JWT_ALGORITHM ?? "HS256",
 		}
-	}
-
-	static async signToken(payload, strategy = "authStrategy") {
-		const { secret, expiresIn, algorithm } =
-			AuthToken[strategy] ?? AuthToken.authStrategy
-
-		const token = jwt.sign(payload, secret, {
-			expiresIn: expiresIn,
-			algorithm: algorithm,
-		})
-
-		return token
-	}
-
-	static async createAuthToken(payload) {
-		const session = new Session({
-			username: payload.username,
-			user_id: payload.user_id,
-			sign_location: payload.sign_location,
-			ip_address: payload.ip_address,
-			client: payload.client,
-			date: new Date().getTime(),
-			created_at: new Date().getTime(),
-		})
-
-		await session.save()
-
-		const jwt_token = await this.signToken(
-			{
-				...payload,
-				session_id: session._id.toString(),
-			},
-			"authStrategy",
-		)
-
-		session.token = jwt_token
-
-		await session.save()
-
-		return jwt_token
-	}
-
-	static async createRefreshToken(user_id, authToken) {
-		const jwt_token = await this.signToken(
-			{
-				user_id,
-			},
-			"refreshStrategy",
-		)
-
-		const refreshRegistry = new RefreshToken({
-			authToken: authToken,
-			refreshToken: jwt_token,
-		})
-
-		await refreshRegistry.save()
-
-		return jwt_token
 	}
 
 	static async basicDecode(token) {
@@ -87,6 +33,16 @@ export default class AuthToken {
 
 				resolve(decoded)
 			})
+		})
+	}
+
+	static async signToken(payload, strategy = "authStrategy") {
+		const { secret, expiresIn, algorithm } =
+			AuthToken[strategy] ?? AuthToken.authStrategy
+
+		return jwt.sign(payload, secret, {
+			expiresIn: expiresIn,
+			algorithm: algorithm,
 		})
 	}
 
@@ -118,15 +74,17 @@ export default class AuthToken {
 			return result
 		}
 
-		// FIXME: please find a better way to check sessions
-		const session = await Session.findById(
-			validation.data.session_id,
-		).catch(() => null)
+		const session = await AuthToken.SessionModel.findOneAsync(
+			{
+				_id: validation.data.session_id,
+			},
+			{ raw: true },
+		)
 
 		// if session not found, return invalid
 		if (!session) {
 			result.valid = false
-			result.error = "Session token not found"
+			result.error = "Session not found or not valid"
 			return result
 		}
 
@@ -141,97 +99,29 @@ export default class AuthToken {
 		result.session = session
 		result.data = validation.data
 		result.user = async () => {
-			return await User.findOne({ _id: validation.data.user_id })
+			return await User.findById(validation.data.user_id)
 		}
-
-		return result
-	}
-
-	static async handleRefreshToken(payload) {
-		const { authToken, refreshToken } = payload
-
-		if (!authToken || !refreshToken) {
-			throw new OperationError(
-				400,
-				"Missing refreshToken or/and authToken",
-			)
-		}
-
-		let result = {
-			error: null,
-			token: undefined,
-			refreshToken: undefined,
-		}
-
-		const validation = await AuthToken.jwtVerify(refreshToken)
-
-		if (validation.error) {
-			throw new OperationError(401, validation.error.message)
-		}
-
-		if (!validation.data.user_id) {
-			throw new OperationError(
-				401,
-				"Missing user_id on the refresh token",
-			)
-		}
-
-		let currentSession = await Session.findOne({
-			user_id: validation.data.user_id,
-			token: authToken,
-		})
-
-		if (!currentSession) {
-			throw new OperationError(
-				401,
-				"Session not matching with provided token",
-			)
-		}
-
-		currentSession = currentSession.toObject()
-
-		await Session.findOneAndDelete({
-			_id: currentSession._id.toString(),
-		})
-
-		result.token = await this.createAuthToken({
-			...currentSession,
-			date: new Date().getTime(),
-		})
-
-		result.refreshToken = await this.createRefreshToken(
-			validation.data.user_id,
-			result.token,
-		)
 
 		return result
 	}
 
 	static async jwtVerify(token) {
-		return await new Promise((resolve, reject) => {
-			try {
-				jwt.verify(
-					token,
-					AuthToken.authStrategy.secret,
-					(err, decoded) => {
-						if (err) {
-							return resolve({
-								error: err,
-								data: decoded,
-							})
-						}
+		const { secret } = AuthToken.authStrategy
 
-						resolve({
-							error: err,
-							data: decoded,
-						})
-					},
-				)
-			} catch (error) {
-				resolve({
-					error: error,
+		return await new Promise((resolve) => {
+			jwt.verify(token, secret, (err, decoded) => {
+				if (err) {
+					return resolve({
+						error: err,
+						data: decoded,
+					})
+				}
+
+				return resolve({
+					error: err,
+					data: decoded,
 				})
-			}
+			})
 		})
 	}
 }
