68ad4d509d3e2608c165c87fdbf56be46a3a42d1
srgooglo <srgooglo@ragestudio.net>
2026-01-14 19:57:54 +0100

    Add ShareCameraDialog component for camera sharing options

---
+++ packages/app/src/components/ShareCameraDialog/index.jsx
@@ -0,0 +1,53 @@
+import React from "react"
+import "./index.less"
+
+const mediaDevices = navigator.mediaDevices.enumerateDevices()
+
+const ShareCameraOptionsDialog = ({ close }) => {
+	const devices = React.use(mediaDevices)
+	const videoDevices = React.useMemo(() => {
+		return devices.filter((device) => device.kind === "videoinput")
+	}, [])
+
+	const onClickDevice = React.useCallback(
+		(device) => {
+			console.log(device)
+
+			app.cores.mediartc.handlers().startCameraShare({
+				device: device,
+			})
+
+			close()
+		},
+		[videoDevices],
+	)
+
+	return (
+		<div className="sharecamera-options-dialog">
+			{videoDevices.map((device) => {
+				return (
+					<div
+						className="sharecamera-options-dialog-item"
+						onClick={() => onClickDevice(device)}
+					>
+						<div className="sharecamera-options-dialog-item-name">
+							{device.label === "" ? "No Name" : device.label}
+						</div>
+						<div className="sharecamera-options-dialog-item-id">
+							{device.deviceId === "" ? "No ID" : device.deviceId}
+						</div>
+					</div>
+				)
+			})}
+		</div>
+	)
+}
+
+export function openDialog() {
+	app.layout.modal.open(
+		"sharecamera-options-dialog",
+		ShareCameraOptionsDialog,
+	)
+}
+
+export default ShareCameraOptionsDialog
