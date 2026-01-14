1b7d71ac610f0f05e159f8a9fc90e5efb9ff7dbe
srgooglo <srgooglo@ragestudio.net>
2026-01-14 19:36:02 +0100

    Add encoder capabilities to HLS transformation handler
    Update TransformationPayloadType and FileHandlePayload to include
    capabilities property. Adjust mq-hls.js to use encoder capabilities for
    setting video levels.

--- packages/server/classes/Transformation/handlers/mq-hls.js
+++ packages/server/classes/Transformation/handlers/mq-hls.js
@@ -1,30 +1,88 @@
 import path from "node:path"
 import MultiqualityHLSJob from "@shared-classes/MultiqualityHLSJob"
 
-export default async ({ filePath, workPath, onProgress }) => {
+export default async ({ filePath, workPath, onProgress, capabilities }) => {
 	return new Promise((resolve, reject) => {
 		const outputDir = path.resolve(workPath, "mqhls")
+
+		const levels = [
+			{
+				original: true,
+				codec: "libx264",
+				preset: "ultrafast",
+				crf: 19,
+				maxrate: "15M",
+			},
+			{
+				codec: "libx264",
+				width: "trunc(iw/2/2)*2",
+				preset: "ultrafast",
+				crf: 25,
+				maxrate: "3M",
+			},
+		]
+
+		if (capabilities.encoders.includes("h264_nvenc")) {
+			levels[0] = {
+				...levels[0],
+				codec: "h264_nvenc",
+				preset: "p4",
+			}
+
+			levels[1] = {
+				...levels[1],
+				codec: "h264_nvenc",
+				preset: "p4",
+			}
+		}
+
+		if (capabilities.encoders.includes("h264_vaapi")) {
+			levels[0] = {
+				...levels[0],
+				codec: "h264_vaapi",
+				preset: "medium",
+			}
+
+			levels[1] = {
+				...levels[1],
+				codec: "h264_vaapi",
+				preset: "medium",
+			}
+		}
+
+		if (capabilities.encoders.includes("h265_nvenc")) {
+			levels[0] = {
+				...levels[0],
+				codec: "h265_nvenc",
+				preset: "p4",
+			}
+
+			levels[1] = {
+				...levels[1],
+				codec: "h265_nvenc",
+				preset: "p4",
+			}
+		}
+
+		if (capabilities.encoders.includes("h265_vaapi")) {
+			levels[0] = {
+				...levels[0],
+				codec: "hevc_vaapi",
+				preset: "medium",
+			}
+
+			levels[1] = {
+				...levels[1],
+				codec: "hevc_vaapi",
+				preset: "medium",
+			}
+		}
 
 		const job = new MultiqualityHLSJob({
 			input: filePath,
 			outputDir: outputDir,
-
-			// set default
 			outputMasterName: "master.m3u8",
-			levels: [
-				{
-					original: true,
-					codec: "libx264",
-					bitrate: "10M",
-					preset: "ultrafast",
-				},
-				{
-					codec: "libx264",
-					width: 1280,
-					bitrate: "3M",
-					preset: "ultrafast",
-				},
-			],
+			levels: levels,
 		})
 
 		job.on("end", (data) => {
