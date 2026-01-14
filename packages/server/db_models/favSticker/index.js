b3447ba3575fb1c8f6a68d55677af3ab9e98d5bf
srgooglo <srgooglo@ragestudio.net>
2026-01-14 20:04:20 +0100

    Add database models for StickerSet, Sticker, and FavStickersSet

---
+++ packages/server/db_models/favSticker/index.js
@@ -0,0 +1,8 @@
+export default {
+	name: "FavStickersSet",
+	collection: "fav_stickers_set",
+	schema: {
+		user_id: { type: String, required: false },
+		stickers_set_id: { type: String, required: false },
+	},
+}
