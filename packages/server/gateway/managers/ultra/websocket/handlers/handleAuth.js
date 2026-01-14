6b255761ba21423c06c323af271893145b1e7e0c
srgooglo <srgooglo@ragestudio.net>
2026-01-14 20:05:13 +0100

    Add WebSocket authentication handler function

---
+++ packages/server/gateway/managers/ultra/websocket/handlers/handleAuth.js
@@ -0,0 +1,54 @@
+export default async function (ctx) {
+	let data = {}
+
+	if (!ctx.token || !this.gateway.base.params?.auth) {
+		return data
+	}
+
+	let authParams = this.gateway.base.params.auth
+
+	try {
+		if (authParams.serviceId) {
+			const targetOrigin = this.gateway.targets.get(authParams.serviceId)
+
+			if (!targetOrigin) {
+				throw new Error(
+					`Service [${authParams.serviceId}] not found in targets`,
+				)
+			}
+
+			authParams.url = new URL(
+				authParams.url,
+				targetOrigin.target,
+			).toString()
+		}
+
+		let authResponse = await fetch(authParams.url, {
+			method: authParams.method
+				? authParams.method.toUpperCase()
+				: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: ctx.token,
+			},
+		})
+
+		if (authResponse.ok) {
+			const reponseData = await authResponse.json()
+
+			if (typeof authParams.data === "function") {
+				data = {
+					...authParams.data(reponseData),
+				}
+			} else {
+				data = reponseData
+			}
+		}
+	} catch (e) {
+		this.gateway.console.error("Failed to authenticate user on upgrade:", e)
+
+		return {}
+	}
+
+	return data
+}
