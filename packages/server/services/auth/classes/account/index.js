fe2175f1fb20d8f3ecd92bec056c522fcffa73ce
srgooglo <srgooglo@ragestudio.net>
2026-01-14 20:08:12 +0100

    Reorder account methods for better organization

--- packages/server/services/auth/classes/account/index.js
+++ packages/server/services/auth/classes/account/index.js
@@ -1,12 +1,10 @@
 export default class Account {
-    static usernameMeetPolicy = require("./methods/usernameMeetPolicy").default
-    static passwordMeetPolicy = require("./methods/passwordMeetPolicy").default
-    static loginStrategy = require("./methods/loginStrategy").default
-    static changePassword = require("./methods/changePassword").default
-    static create = require("./methods/create").default
-    static sessions = require("./methods/sessions").default
-    static deleteSession = require("./methods/deleteSession").default
-    static sendActivationCode = require("./methods/sendActivationCode").default
-    static activateAccount = require("./methods/activateAccount").default
-    static disableAccount = require("./methods/disableAccount").default
+	static create = require("./methods/create").default
+	static loginStrategy = require("./methods/loginStrategy").default
+	static changePassword = require("./methods/changePassword").default
+	static usernameMeetPolicy = require("./methods/usernameMeetPolicy").default
+	static passwordMeetPolicy = require("./methods/passwordMeetPolicy").default
+	static sendActivationCode = require("./methods/sendActivationCode").default
+	static activateAccount = require("./methods/activateAccount").default
+	static disableAccount = require("./methods/disableAccount").default
 }
