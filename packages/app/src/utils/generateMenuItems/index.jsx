9a1d6b7ee0dda6cf1912fc63ce5af1a7d30a23dc
srgooglo <srgooglo@ragestudio.net>
2026-02-16 22:46:21 +0100

    Support required flags

--- packages/app/src/utils/generateMenuItems/index.jsx
+++ packages/app/src/utils/generateMenuItems/index.jsx
@@ -1,21 +1,35 @@
-import React from "react"
-
 import { Translation } from "react-i18next"
 import { createIconRender } from "@components/Icons"
 
 export default (items) => {
-    return items.map((item) => {
-        return {
-            id: item.id,
-            key: item.id,
-            path: item.path,
-            icon: createIconRender(item.icon),
-            label: <Translation>
-                {t => t(item.title ?? item.label ?? item.id)}
-            </Translation>,
-            danger: item.danger,
-            disabled: item.disabled,
-            children: item.children,
-        }
-    })
+	items = items.filter((item) => {
+		if (Array.isArray(item.requiredFlags)) {
+			if (!app.userData || !Array.isArray(app.userData.flags)) {
+				return false
+			}
+
+			return item.requiredFlags.every((flag) => {
+				return app.userData.flags.includes(flag)
+			})
+		}
+
+		return true
+	})
+
+	return items.map((item) => {
+		return {
+			id: item.id,
+			key: item.id,
+			path: item.path,
+			icon: createIconRender(item.icon),
+			label: (
+				<Translation>
+					{(t) => t(item.title ?? item.label ?? item.id)}
+				</Translation>
+			),
+			danger: item.danger,
+			disabled: item.disabled,
+			children: item.children,
+		}
+	})
 }
