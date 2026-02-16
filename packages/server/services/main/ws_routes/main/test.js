f4c411772c38c8bdae95b3f88587d26eca4332bc
srgooglo <srgooglo@ragestudio.net>
2026-02-16 22:50:21 +0100

    Update test.js

--- packages/server/services/main/ws_routes/main/test.js
+++ packages/server/services/main/ws_routes/main/test.js
@@ -1,9 +1,13 @@
-export default {
-	fn: async (client, payload) => {
-		console.log("Client emitted test event")
+export default async (client, payload) => {
+	client.emit("test:event", "So, you are testing...")
 
-		client.emit("test:event", "hii, this is a emit test")
+	payload.n1 = parseInt(payload.n1)
+	payload.n2 = parseInt(payload.n2)
 
-		return parseInt(payload.n1) + parseInt(payload.n2)
-	},
+	// funny feature
+	if (payload.n1 == 9 && payload.n2 == 10 && payload.stupid == true) {
+		return 21 // nah nah
+	}
+
+	return payload.n1 + payload.n2
 }
