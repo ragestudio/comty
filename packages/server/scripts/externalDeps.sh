5f022b967839db68e3a90e96c216a522b4421d85
srgooglo <srgooglo@ragestudio.net>
2026-02-17 16:59:39 +0100

    Fix Docker build paths and simplify server Dockerfile

--- packages/server/scripts/externalDeps.sh
+++ packages/server/scripts/externalDeps.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # check if we are in sudo
 if [ "$EUID" -ne 0 ]; then
     exec sudo bash "$0" "$@"
@@ -7,8 +9,17 @@ fi
 # check if ultragateway is installed
 if [ ! -f /usr/local/bin/ultragateway ]; then
     echo "Installing ultragateway"
-    curl -fsSL https://git.ragestudio.net/RageStudio/linebridge-gateway/raw/branch/main/install.sh | sudo sh
-    echo "ultragateway binary installed"
+    curl -fsSL https://git.ragestudio.net/RageStudio/linebridge-gateway/raw/branch/main/install.sh | sh
+    echo "Ultragateway binary installed"
 else
-    echo "ultragateway binary ok"
+    echo "Ultragateway binary ok"
+fi
+
+# check if ffmpeg is installed
+if [ ! -f /usr/local/bin/ffmpeg ]; then
+    echo "Installing FFMPEG"
+    ./scripts/installLatestFfmpeg.sh
+    echo "FFMPEG binary installed"
+else
+    echo "FFMPEG binary ok"
 fi
