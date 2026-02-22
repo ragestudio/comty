7f74859fe2015242e2b7e6d5ed98761f929b1415
srgooglo <srgooglo@ragestudio.net>
2026-02-23 00:24:35 +0100

    use global ipc

--- packages/server/services/auth/classes/account/methods/sendActivationCode.js
+++ packages/server/services/auth/classes/account/methods/sendActivationCode.js
@@ -4,48 +4,54 @@ import { User, ActivationCode } from "@db_models"
 const waitingTime = 60 * 1000
 
 export default async (user_id, event = "account:activation") => {
-    if (!user_id) {
-        throw new OperationError(400, "Missing user_id")
-    }
+	if (!user_id) {
+		throw new OperationError(400, "Missing user_id")
+	}
 
-    const user = await User.findOne({
-        _id: user_id,
-    }).select("+email")
+	const user = await User.findOne({
+		_id: user_id,
+	}).select("+email")
 
-    if (!user) {
-        throw new OperationError(404, "User not found")
-    }
+	if (!user) {
+		throw new OperationError(404, "User not found")
+	}
 
-    if (user.activated) {
-        throw new OperationError(400, "User already activated")
-    }
+	if (user.activated) {
+		throw new OperationError(400, "User already activated")
+	}
 
-    let activationCode = await ActivationCode.findOne({
-        user_id: user._id,
-    })
+	let activationCode = await ActivationCode.findOne({
+		user_id: user._id,
+	})
 
-    if (activationCode) {
-        // check if activation code is too recent
-        if (activationCode.date.getTime() + waitingTime > new Date().getTime()) {
-            throw new OperationError(400, "Activation code timeout, please try again later")
-        }
+	if (activationCode) {
+		// check if activation code is too recent
+		if (
+			activationCode.date.getTime() + waitingTime >
+			new Date().getTime()
+		) {
+			throw new OperationError(
+				400,
+				"Activation code timeout, please try again later",
+			)
+		}
 
-        await ActivationCode.deleteOne({
-            user_id: user._id.toString()
-        })
-    }
+		await ActivationCode.deleteOne({
+			user_id: user._id.toString(),
+		})
+	}
 
-    activationCode = await ActivationCode.create({
-        event: event,
-        user_id: user._id.toString(),
-        code: Math.floor(Math.random() * 900000) + 100000,
-        date: new Date(),
-    })
+	activationCode = await ActivationCode.create({
+		event: event,
+		user_id: user._id.toString(),
+		code: Math.floor(Math.random() * 900000) + 100000,
+		date: new Date(),
+	})
 
-    ipc.invoke("ems", "account:activation:send", {
-        activation_code: activationCode.code,
-        user: user.toObject(),
-    })
+	global.ipc.invoke("ems", "account:activation:send", {
+		activation_code: activationCode.code,
+		user: user.toObject(),
+	})
 
-    return activationCode.toObject()
+	return activationCode.toObject()
 }
