eac07f65906742a7504806a88f0f396efbdadef9
srgooglo <srgooglo@ragestudio.net>
2026-03-10 21:21:57 +0100

    Disable pipewire and pulseaudio modules on Linux

--- packages/desktop/src/patches/linux/index.js
+++ packages/desktop/src/patches/linux/index.js
@@ -1,7 +1,6 @@
 export default async (main) => {
-	// start pipewire
-	await main.loadModule("pipewire", "./modules/pipewire/index.js")
-
-	// start pulseaudio
-	await main.loadModule("pulseaudio", "./modules/pulseaudio/index.js")
+	// // start pipewire
+	// await main.loadModule("pipewire", "./modules/pipewire/index.js")
+	// // start pulseaudio
+	// await main.loadModule("pulseaudio", "./modules/pulseaudio/index.js")
 }
