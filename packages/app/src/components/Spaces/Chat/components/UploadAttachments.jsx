d4c57f1c22e119f9b193617a369de322065e3520
srgooglo <srgooglo@ragestudio.net>
2026-04-06 14:21:11 +0200

    Update CSS class names for chat input attachments

--- packages/app/src/components/Spaces/Chat/components/UploadAttachments.jsx
+++ packages/app/src/components/Spaces/Chat/components/UploadAttachments.jsx
@@ -2,15 +2,20 @@ import classNames from "classnames"
 
 import AttachmentMedia from "./UploadAttachment"
 
+import "./UploadAttachments.less"
+
 const Attachments = ({ items }) => {
 	return (
-		<div className="channel-chat__attachments">
+		<div className="channel-chat__input__attachments">
 			{items.map((item) => (
 				<div
 					key={item.uid}
-					className={classNames("channel-chat__attachments__item", {
-						["pending"]: item.pending,
-					})}
+					className={classNames(
+						"channel-chat__input__attachments__item",
+						{
+							["pending"]: item.pending,
+						},
+					)}
 				>
 					<AttachmentMedia file={item.file} />
 				</div>
