534da6541e0d4a1fd74c6cdc93a6dc99c9ccf8f4
srgooglo <srgooglo@ragestudio.net>
2026-06-13 23:49:18 +0200

    Refactor global type declarations and add less module declaration

--- packages/app/src/global.d.ts
+++ packages/app/src/global.d.ts
@@ -1,27 +1,28 @@
-declare global {
-	interface Window {
-		ipcRenderer: Electron.IpcRenderer
-		app: app
-	}
-
-	export const app: App
-
-	interface App {
-		eventBus: EventBus
-		cores: CoresManager
-		userData: null | Object<string, any>
-		isDesktop: boolean
-	}
-
-	interface CoresManager {
-		[key: string]: any
-	}
+interface CoresManager {
+	[key: string]: any
 }
 
 type EventBus = {
 	emit(event: string, ...args: any[]): void
 	on(event: string, listener: (...args: any[]) => void): void
 	off(event: string, listener: (...args: any[]) => void): void
+}
+
+interface app {
+	eventBus: EventBus
+	cores: CoresManager
+	userData: null | Record<string, any>
+	isDesktop: boolean
+	[key: string]: any
+}
+
+declare global {
+	interface Window {
+		ipcRenderer: Electron.IpcRenderer
+		app: app
+	}
+
+	const app: app
 }
 
 export {}
