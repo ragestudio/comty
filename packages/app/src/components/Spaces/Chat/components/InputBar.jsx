f38fff759cd52659d3a9d4b2d614f183898bb497
srgooglo <srgooglo@ragestudio.net>
2026-07-15 13:43:33 +0200

    Refactor InputBar component and add file upload menu
    Extract InputMenu component from the main bar, add upload progress
    tracking,
    improve attachment state handling, and add a popover menu for file
    uploads
    and future poll creation

--- packages/app/src/components/Spaces/Chat/components/InputBar.jsx
+++ packages/app/src/components/Spaces/Chat/components/InputBar.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 import { Input } from "antd"
 
+import Popover from "@/ui/Popover"
+import Button from "@/ui/Button"
 import { Icons } from "@components/Icons"
 import UploadButton from "@components/UploadButton"
 
@@ -14,16 +16,57 @@ import GifPicker from "./GifPicker"
 
 import "./InputBar.less"
 
-const ChatInputBar = React.memo(({ channel_id, send, typing }) => {
+const InputMenu = ({
+	close,
+	uploaderRef,
+	onUploaderStart,
+	onUploaderSuccess,
+	onUploaderProgress,
+}) => {
+	const handleOnUploaderStart = (...args) => {
+		if (typeof close === "function") close()
+		if (typeof onUploaderStart === "function") onUploaderStart(...args)
+	}
+
+	return (
+		<div className="channel-chat__input__area__menu">
+			<UploadButton
+				multiple
+				ref={uploaderRef}
+				onStart={handleOnUploaderStart}
+				onProgress={onUploaderProgress}
+				onSuccess={onUploaderSuccess}
+				render={() => {
+					return (
+						<Button
+							type="ghost"
+							icon={<Icons.FileUp />}
+						>
+							<span>Upload a File</span>
+						</Button>
+					)
+				}}
+			/>
+
+			<Button
+				type="ghost"
+				icon={<Icons.FormInput />}
+				disabled
+			>
+				<span>Create a Poll</span>
+			</Button>
+		</div>
+	)
+}
+
+export const ChatInputBar = ({ channel_id, send, typing }) => {
 	const [inputValue, setInputValue] = React.useState("")
 	const [attachments, setAttachments] = React.useState([])
 
-	const uploaderRef = React.useRef()
+	const uploaderRef = React.useRef(null)
 
-	// check if the message is empty or if there are attachments
-	// also check if something is uploading
 	const canSubmit = () => {
-		if (uploaderRef.current?.uploading) {
+		if (attachments.some((item) => item.pending)) {
 			return false
 		}
 
@@ -85,26 +128,45 @@ const ChatInputBar = React.memo(({ channel_id, send, typing }) => {
 				uid: uid,
 				file: file,
 				pending: true,
+				progress: 0,
 			},
 		])
 	}, [])
 
+	const onUploaderProgress = React.useCallback((uid, progress) => {
+		setAttachments((prev) =>
+			prev.map((item) => {
+				if (item.uid !== uid) {
+					return item
+				}
+
+				return {
+					...item,
+					progress: progress?.percent ?? 0,
+				}
+			}),
+		)
+	}, [])
+
 	const onUploaderSuccess = React.useCallback((uid, response) => {
 		setAttachments((prev) => {
-			// find the file in the list
-			const index = [...prev].findIndex((file) => file.uid === uid)
+			return prev.map((file) => {
+				if (file.uid !== uid) {
+					return file
+				}
 
-			// update the file
-			prev[index].url = response.url
-
-			if (response.metadata) {
-				prev[index].hash = response.metadata?.["File-Hash"]
-			}
-
-			prev[index].pending = false
-
-			return prev
+				return {
+					...file,
+					url: response.url,
+					hash: response.metadata?.["File-Hash"],
+					pending: false,
+				}
+			})
 		})
+	}, [])
+
+	const removeAttachment = React.useCallback((uid) => {
+		setAttachments((prev) => prev.filter((item) => item.uid !== uid))
 	}, [])
 
 	useOnPaste((event) => {
@@ -131,10 +193,25 @@ const ChatInputBar = React.memo(({ channel_id, send, typing }) => {
 	return (
 		<div className="channel-chat__input">
 			{attachments.length > 0 && (
-				<UploadAttachments items={attachments} />
+				<UploadAttachments
+					items={attachments}
+					onRemove={removeAttachment}
+				/>
 			)}
 
 			<div className="channel-chat__input__area bg-accent">
+				<Popover
+					content={InputMenu}
+					contentProps={{
+						uploaderRef,
+						onUploaderStart,
+						onUploaderSuccess,
+						onUploaderProgress,
+					}}
+				>
+					<Button icon={<Icons.Plus />} />
+				</Popover>
+
 				<Input.TextArea
 					name="message"
 					id="message"
@@ -155,19 +232,12 @@ const ChatInputBar = React.memo(({ channel_id, send, typing }) => {
 							setInputValue(inputValue + emoji)
 						}
 					/>
-					<StickersButton
-						onClickItem={(sticker) => sendSticker(sticker)}
-					/>
-					<GifPicker
-						onClickItem={(gifUrl) => {
-							console.log(gifUrl)
-						}}
-					/>
 
 					<UploadButton
 						multiple
 						ref={uploaderRef}
 						onStart={onUploaderStart}
+						onProgress={onUploaderProgress}
 						onSuccess={onUploaderSuccess}
 						children={null}
 					/>
@@ -183,6 +253,8 @@ const ChatInputBar = React.memo(({ channel_id, send, typing }) => {
 			</div>
 		</div>
 	)
-})
+}
 
-export default ChatInputBar
+export const MemoizedChatInputBar = React.memo(ChatInputBar)
+
+export default MemoizedChatInputBar
