f3280a88da26146a42724c52de46d7da66762221
srgooglo <srgooglo@ragestudio.net>
2026-07-24 12:31:36 +0200

    Add encode and decode helpers for EventData type
    Replace the default export with EventDataEncode and EventDataDecode
    utility functions to serialize and deserialize event data.

--- packages/shared/types/event_data.ts
+++ packages/shared/types/event_data.ts
@@ -5,4 +5,22 @@ export type EventData = {
 	ack?: boolean
 }
 
-export default EventData
+const textDecoder = new TextDecoder()
+const textEncoder = new TextEncoder()
+
+export function EventDataEncode(
+	event: string | symbol,
+	data: any,
+	error?: any,
+	ack?: boolean,
+): Uint8Array {
+	return textEncoder.encode(JSON.stringify({ event, data, error, ack }))
+}
+
+export function EventDataDecode(data: string | Uint8Array): EventData {
+	if (data instanceof Uint8Array) {
+		data = textDecoder.decode(data)
+	}
+
+	return JSON.parse(data)
+}
