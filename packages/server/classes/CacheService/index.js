b52ecc8e25078b82b021935cecbd86a55ce5bdc9
srgooglo <srgooglo@ragestudio.net>
2026-07-28 16:09:58 +0200

    Add getter for cache path in CacheService

--- packages/server/classes/CacheService/index.js
+++ packages/server/classes/CacheService/index.js
@@ -2,71 +2,77 @@ import fs from "fs"
 import path from "path"
 
 export default class CacheService {
-    static deletionInterval = 1000 * 60 * 5
+	static deletionInterval = 1000 * 60 * 5
 
-    static cachePath = path.join(process.cwd(), ".cache")
+	static cachePath = path.join(process.cwd(), ".cache")
 
-    constructor(params = {}) {
-        this.params = params
+	constructor(params = {}) {
+		this.params = params
 
-        if (!fs.existsSync(CacheService.cachePath)) {
-            fs.mkdirSync(CacheService.cachePath, { recursive: true })
-        }
-    }
+		if (!fs.existsSync(CacheService.cachePath)) {
+			fs.mkdirSync(CacheService.cachePath, { recursive: true })
+		}
+	}
 
-    intervalMaps = new Map()
+	get path() {
+		return CacheService.cachePath
+	}
 
-    checkDeletionFilepath(filepath) {
-        try {
-            const stats = fs.statSync(filepath)
+	intervalMaps = new Map()
 
-            stats.atime = new Date(stats.atime)
+	checkDeletionFilepath(filepath) {
+		try {
+			const stats = fs.statSync(filepath)
 
-            if (stats.atime.getTime() + CacheService.deletionInterval < Date.now()) {
-                fs.promises.unlink(filepath)
-            } else {
-                return false
-            }
+			stats.atime = new Date(stats.atime)
 
-            return true
+			if (
+				stats.atime.getTime() + CacheService.deletionInterval <
+				Date.now()
+			) {
+				fs.promises.unlink(filepath)
+			} else {
+				return false
+			}
 
-        } catch (error) {
-            console.error(error)
+			return true
+		} catch (error) {
+			console.error(error)
 
-            fs.promises.unlink(filepath)
+			fs.promises.unlink(filepath)
 
-            return true
-        }
-    }
+			return true
+		}
+	}
 
-    appendToDeletion(filepath) {
-        // create a interval of 5 minutes to delete the file
-        // check the last time the file was accessed and if it was accessed in the last 5 minutes
-        // reset the interval until the file is not accessed for 5 minutes and then delete it
-        try {
-            const createInterval = () => {
-                let interval = setInterval(async () => {
-                    try {
-                        await this.checkDeletionFilepath(filepath)
+	appendToDeletion(filepath) {
+		// create a interval of 5 minutes to delete the file
+		// check the last time the file was accessed and if it was accessed in the last 5 minutes
+		// reset the interval until the file is not accessed for 5 minutes and then delete it
+		try {
+			const createInterval = () => {
+				let interval = setInterval(async () => {
+					try {
+						await this.checkDeletionFilepath(filepath)
 
-                        this.intervalMaps.delete(filepath)
+						this.intervalMaps.delete(filepath)
 
-                        if (!results) {
-                            this.appendToDeletion(filepath)
-                        }
-                    } catch (error) {
-                        return clearInterval(interval)
-                    }
-                })
+						if (!results) {
+							this.appendToDeletion(filepath)
+						}
+					} catch (error) {
+						return clearInterval(interval)
+					}
+				})
 
-                return interval
-            }
+				return interval
+			}
 
-            this.intervalMaps.set(filepath, createInterval())
-        } catch (error) {
-            console.error(error)
+			this.intervalMaps.set(filepath, createInterval())
+		} catch (error) {
+			console.error(error)
 
-            return fs.promises.unlink(filepath)
-        }
-    }
+			return fs.promises.unlink(filepath)
+		}
+	}
 }
