6ad8dd31d809c41fc8b5eb9b0541e51cf44cfcb4
srgooglo <srgooglo@ragestudio.net>
2026-07-29 21:18:17 +0200

    Remove unused ServiceIndicator component and simplify button props

--- packages/app/src/components/Player/index.jsx
+++ packages/app/src/components/Player/index.jsx
@@ -27,23 +27,11 @@ function isOverflown(parent, element) {
 	return elementRect.width > parentRect.width
 }
 
-const ServiceIndicator = (props) => {
-	if (!props.service) {
-		return null
-	}
-
-	switch (props.service) {
-		default: {
-			return null
-		}
-	}
-}
-
 const Player = (props) => {
 	const [playerState] = usePlayerStateContext()
 
-	const contentRef = React.useRef()
-	const titleRef = React.useRef()
+	const contentRef = React.useRef(null)
+	const titleRef = React.useRef(null)
 
 	const [topActionsVisible, setTopActionsVisible] = React.useState(false)
 	const [titleOverflown, setTitleOverflown] = React.useState(false)
@@ -108,21 +96,16 @@ const Player = (props) => {
 		>
 			<div className={classnames("toolbar_player_top_actions")}>
 				{!playerState.control_locked && (
-					<Button
-						icon={<Icons.Cast />}
-						shape="circle"
-					/>
+					<Button icon={<Icons.Cast />} />
 				)}
 
 				<Button
 					icon={<Icons.Fullscreen />}
-					shape="circle"
 					onClick={() => app.location.push("/lyrics")}
 				/>
 
 				<Button
 					icon={<Icons.X />}
-					shape="circle"
 					onClick={() => app.cores.player.close()}
 				/>
 			</div>
@@ -145,8 +128,6 @@ const Player = (props) => {
 								["overflown"]: titleOverflown,
 							})}
 						>
-							<ServiceIndicator service={service} />
-
 							{titleText}
 						</h1>
 
@@ -154,13 +135,11 @@ const Player = (props) => {
 							<Marquee
 								gradientColor={RGBStringToValues(
 									coverAnalysis?.rgb ?? "0,0,0",
-								)}
+								).join("")}
 								gradientWidth={20}
 								play={playerState.playback_status !== "stopped"}
 							>
 								<h1 className="toolbar_player_info_title">
-									<ServiceIndicator service={service} />
-
 									{titleText}
 								</h1>
 							</Marquee>
