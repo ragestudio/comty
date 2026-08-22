5faa10dcb8a9d32faf17b2939340f8a3d6efc780
srgooglo <srgooglo@ragestudio.net>
2026-08-22 23:27:08 +0200

    Update useAckNotifications.js

--- packages/app/src/hooks/useAckNotifications.js
+++ packages/app/src/hooks/useAckNotifications.js
@@ -2,6 +2,7 @@ import React from "react"
 
 const useAckNotifications = () => {
 	const [pending, setPending] = React.useState([])
+	const [acks, setAcks] = React.useState(app.cores.notifications.state.acks || [])
 
 	const load = async () => {
 		setPending(await app.cores.notifications.unread())
@@ -11,21 +12,32 @@ const useAckNotifications = () => {
 		load()
 	}
 
+	const handleAcksUpdated = (newAcks) => {
+		setAcks([...newAcks])
+	}
+
 	React.useEffect(() => {
 		load()
+		if (app.cores.notifications.state.acks) {
+			setAcks([...app.cores.notifications.state.acks])
+		}
 
 		app.eventBus.on("notification:ack:new", handleNewAck)
 		app.eventBus.on("notification:ack:del", handleNewAck)
+		app.eventBus.on("acks:updated", handleAcksUpdated)
 
 		return () => {
 			app.eventBus.off("notification:ack:new", handleNewAck)
 			app.eventBus.off("notification:ack:del", handleNewAck)
+			app.eventBus.off("acks:updated", handleAcksUpdated)
 		}
 	}, [])
 
 	return {
 		pending,
 		setPending,
+		acks,
+		setAcks,
 	}
 }
 
