d2b1e1f86f8994ea7e8eafa5ceff3cf00756ba59
srgooglo <srgooglo@ragestudio.net>
2026-08-22 23:26:34 +0200

    Update index.js

--- packages/app/src/cores/api/baseWsEvents/index.js
+++ packages/app/src/cores/api/baseWsEvents/index.js
@@ -1,7 +1,9 @@
 import newNotification from "./newNotification"
+import ackSyncNotification from "./ackSyncNotification"
 import dmActivityUpdate from "./dmActivityUpdate"
 
 export default {
 	"notification:new": newNotification,
+	"notification:ack:sync": ackSyncNotification,
 	"dm:activity:update": dmActivityUpdate,
 }
