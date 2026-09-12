ecf2e0fef488cc2eb6b4cd4eb1db6a8b57615256
SrGooglo <srgooglo@ragestudio.net>
2026-09-12 14:33:00 +0200

    Refactor expressions_favs schema keys and types

--- packages/shared/db/expressions_favs.ts
+++ packages/shared/db/expressions_favs.ts
@@ -6,33 +6,32 @@ export type ExpressionFavorite = InferDoc<typeof schema>
 export const schema = new Schema(
 	{
 		table_name: "expressions_favs",
-		keys: [["user_id"], "type", "resource_url", "created_at"],
+		keys: [["user_id"], "type", "_id"],
 		clustering_order: {
-			type: "asc",
-			resource_url: "asc",
-			created_at: "asc",
+			type: "desc",
+			_id: "desc",
 		},
 	},
 	{
+		_id: defineColumn<number>()({
+			type: ColumnTypes.Bigint,
+			required: true,
+		}),
 		user_id: defineColumn<string>()({
-			type: ColumnTypes.Varchar,
+			type: ColumnTypes.Text,
 			required: true,
 		}),
 		type: defineColumn<string>()({
-			type: ColumnTypes.Varchar,
+			type: ColumnTypes.Text,
 			required: true,
 		}),
 		resource_url: defineColumn<string>()({
-			type: ColumnTypes.Varchar,
+			type: ColumnTypes.Text,
 			required: true,
 		}),
 		metadata: defineColumn<Map<string, string>>()({
-			type: "map<varchar, varchar>",
+			type: "map<Text, Text>",
 			required: false,
-		}),
-		created_at: defineColumn<Date>()({
-			type: ColumnTypes.Timestamp,
-			required: true,
 		}),
 	},
 )
