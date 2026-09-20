4a17652cace5d7b68b3d9a17870020633ed8608a
SrGooglo <srgooglo@ragestudio.net>
2026-09-20 18:34:17 +0200

    Refactor gif favorites routes for consistency

--- packages/server/services/main/routes/expressions/gif/fav/get.ts
+++ packages/server/services/main/routes/expressions/gif/fav/get.ts
@@ -4,19 +4,13 @@ import ExpressionFavModel from "@db/expressions_favs"
 export default defineRoute<API>()({
 	useMiddlewares: ["withAuthentication"],
 	fn: async (req, res, ctx) => {
-		// @ts-ignore
 		const user_id = req.auth.session.user_id
 		//const { limit = 30, page = 0 } = req.query
 
-		const gifs = await ExpressionFavModel.find(
-			{
-				user_id: user_id,
-				type: "gif",
-			},
-			// {
-			// 	limit: parseInt(limit),
-			// },
-		)
+		const gifs = await ExpressionFavModel.find({
+			user_id: user_id,
+			type: "gif",
+		})
 
 		return gifs
 	},
--- packages/server/services/main/routes/expressions/gif/fav/post.ts
+++ packages/server/services/main/routes/expressions/gif/fav/post.ts
@@ -4,38 +4,34 @@ import ExpressionFavModel from "@db/expressions_favs"
 export default defineRoute<API>()({
 	useMiddlewares: ["withAuthentication"],
 	fn: async (req, res, ctx) => {
-		// @ts-ignore
 		const user_id = req.auth.session.user_id
 
-		const { resource_url, metadata } = req.body
+		const { _id, resource_url, metadata } = req.body
 
-		let favFind = await ExpressionFavModel.find({
-			user_id: user_id,
-			type: "gif",
-			resource_url: resource_url,
-		})
+		let fav = null
 
-		let fav = favFind[0]
+		if (typeof _id === "string") {
+			fav = await ExpressionFavModel.findOne({
+				user_id: user_id,
+				type: "gif",
+				_id: Number(_id),
+			})
+		}
 
 		if (fav) {
-			await ExpressionFavModel.delete({
-				user_id: user_id,
-				type: "gif",
-				resource_url: resource_url,
-				created_at: fav.created_at,
-			})
+			await fav.delete()
 		} else {
 			fav = ExpressionFavModel.obj({
-				user_id: user_id,
+				_id: new Date().getTime(),
 				type: "gif",
+				user_id: user_id,
 				resource_url: resource_url,
 				metadata: metadata,
-				created_at: new Date(),
 			})
 
 			await fav.save()
 		}
 
-		return fav
+		return fav.toRaw()
 	},
 })
