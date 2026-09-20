1730bbce2ce99c0509d80db9d5fce68e19901436
SrGooglo <srgooglo@ragestudio.net>
2026-09-20 18:29:05 +0200

    Remove unused model loading code from db_models

--- packages/server/db_models/index.ts
+++ packages/server/db_models/index.ts
@@ -1,6 +1,4 @@
-import mongoose, { Schema, Model } from "mongoose"
-import fs from "fs"
-import path from "path"
+import mongoose, { Schema } from "mongoose"
 
 export type ModelDefinition = {
 	name: string
@@ -21,25 +19,3 @@ export function defineModel(definition: ModelDefinition) {
 
 	return mongoose.model(name, modelSchema, collection)
 }
-
-function retreiveModels() {
-	let models = {}
-
-	const dirs = fs
-		.readdirSync(__dirname)
-		.filter((file) => file !== "index.js" && file !== "index.ts")
-
-	dirs.forEach((file) => {
-		const mod = require(path.join(__dirname, file)).default
-
-		if (typeof mod === "function" && mod.base) {
-			return (models[mod.modelName] = mod)
-		}
-
-		return (models[mod.name] = defineModel(mod))
-	})
-
-	return models
-}
-
-module.exports = retreiveModels()
