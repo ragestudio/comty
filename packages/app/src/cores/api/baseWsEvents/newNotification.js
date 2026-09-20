1cd349a36b03ec1fc8c553c10bfe28fc7a07cb15
SrGooglo <srgooglo@ragestudio.net>
2026-09-20 18:35:52 +0200

    Update newNotification.js

--- packages/app/src/cores/api/baseWsEvents/newNotification.js
+++ packages/app/src/cores/api/baseWsEvents/newNotification.js
@@ -1,3 +1,25 @@
 export default async function (data) {
+	data.ack = true
+
+	if (typeof data.data === "string") {
+		try {
+			const parsed = JSON.parse(data.data)
+			data.description = parsed.short_message || "New message"
+			if (parsed.group_id) {
+				data.group_id = parsed.group_id
+			}
+		} catch (e) {}
+	}
+	
+	if (!data.title) {
+		if (data.type === "group_mention") {
+			data.title = "You were mentioned!"
+		} else if (data.type === "dm_message") {
+			data.title = "New Direct Message"
+		} else {
+			data.title = "New Notification"
+		}
+	}
+
 	return app.cores.notifications.new(data)
 }
