b0a20db33d36d6cc3d2b01ab1ab0d4d33091bdc5
SrGooglo <srgooglo@ragestudio.net>
2026-09-20 18:24:48 +0200

    Update global.d.ts

--- packages/app/src/global.d.ts
+++ packages/app/src/global.d.ts
@@ -8,21 +8,4 @@ type EventBus = {
 	off(event: string, listener: (...args: any[]) => void): void
 }
 
-interface app {
-	eventBus: EventBus
-	cores: CoresManager
-	userData: null | Record<string, any>
-	isDesktop: boolean
-	[key: string]: any
-}
-
-declare global {
-	interface Window {
-		ipcRenderer: Electron.IpcRenderer
-		app: app
-	}
-
-	const app: app
-}
-
 export {}
